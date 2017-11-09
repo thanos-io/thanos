24e3b23b7ea507b6b8a1444cf62f604911b561c4
Bartek Plotka <bwplotka@gmail.com>
2017-11-09 17:09:14 +0000

    Moved spinup to different location.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- test/e2e/query_test.go
+++ test/e2e/query_test.go
@@ -1,18 +1,14 @@
 package e2e_test
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"sync"
-	"syscall"
 	"testing"
 	"time"
 
@@ -21,12 +17,23 @@ import (
 
 	"reflect"
 
+	"fmt"
+
 	"github.com/improbable-eng/thanos/pkg/runutil"
 	"github.com/improbable-eng/thanos/pkg/testutil"
 )
 
-func selfScrapePromConfig(port int) string {
-	return fmt.Sprintf(`
+// TestQuerySimple runs a setup of Prometheus servers, sidecars, and query nodes and verifies that
+// queries return data merged from all Prometheus servers.
+func TestQuerySimple(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test_query_simple")
+	testutil.Ok(t, err)
+	defer os.RemoveAll(dir)
+
+	closeFn := spinup(t, config{
+		promConfigFn: func(port int) string {
+			// Self scraping config with unique external label.
+			return fmt.Sprintf(`
 global:
   external_labels:
     prometheus: prom-%d
@@ -37,96 +44,7 @@ scrape_configs:
   - targets:
     - "localhost:%d"
 `, port, port)
-}
-
-type spinupConfig struct {
-	promConfigFn func(port int) string
-	workDir      string
-
-	numPrometheus int
-	numQueries    int
-}
-
-// NOTE: It is important to install Thanos before using this function to compile latest changes.
-func spinup(t testing.TB, cfg spinupConfig) (close func()) {
-	var commands []*exec.Cmd
-	var closers []*exec.Cmd
-
-	for i := 1; i <= cfg.numPrometheus; i++ {
-		promDir := fmt.Sprintf("%s/data/prom%d", cfg.workDir, i)
-
-		if err := os.MkdirAll(promDir, 0777); err != nil {
-			return func() {}
-		}
-		f, err := os.Create(promDir + "/prometheus.yml")
-		if err != nil {
-			return func() {}
-		}
-		_, err = f.Write([]byte(cfg.promConfigFn(9090 + i)))
-		f.Close()
-		if err != nil {
-			return func() {}
-		}
-
-		commands = append(commands, exec.Command("prometheus",
-			"--config.file", promDir+"/prometheus.yml",
-			"--storage.tsdb.path", promDir,
-			"--log.level", "info",
-			"--web.listen-address", fmt.Sprintf("0.0.0.0:%d", 9090+i),
-		))
-		commands = append(commands, exec.Command("thanos", "sidecar",
-			"--debug.name", fmt.Sprintf("sidecar-%d", i),
-			"--api-address", fmt.Sprintf("0.0.0.0:%d", 19090+i),
-			"--metrics-address", fmt.Sprintf("0.0.0.0:%d", 19190+i),
-			"--prometheus.url", fmt.Sprintf("http://localhost:%d", 9090+i),
-			"--tsdb.path", promDir,
-			"--cluster.address", fmt.Sprintf("0.0.0.0:%d", 19390+i),
-			"--cluster.advertise-address", fmt.Sprintf("127.0.0.1:%d", 19390+i),
-			"--cluster.peers", "127.0.0.1:19391",
-		))
-	}
-
-	for i := 1; i <= cfg.numQueries; i++ {
-		commands = append(commands, exec.Command("thanos", "query",
-			"--debug.name", fmt.Sprintf("query-%d", i),
-			"--api-address", fmt.Sprintf("0.0.0.0:%d", 19490+i),
-			"--cluster.address", fmt.Sprintf("0.0.0.0:%d", 19590+i),
-			"--cluster.advertise-address", fmt.Sprintf("127.0.0.1:%d", 19590+i),
-			"--cluster.peers", "127.0.0.1:19391",
-		))
-	}
-
-	var stderr bytes.Buffer
-	stderrw := &safeWriter{Writer: &stderr}
-
-	close = func() {
-		for _, c := range closers {
-			c.Process.Signal(syscall.SIGTERM)
-			c.Wait()
-		}
-		t.Logf("STDERR\n %s", stderr.String())
-	}
-	for _, cmd := range commands {
-		cmd.Stderr = stderrw
-
-		if err := cmd.Start(); err != nil {
-			close()
-			return func() {}
-		}
-		closers = append(closers, cmd)
-	}
-	return close
-}
-
-// TestQuerySimple runs a setup of Prometheus servers, sidecars, and query nodes and verifies that
-// queries return data merged from all Prometheus servers.
-func TestQuerySimple(t *testing.T) {
-	dir, err := ioutil.TempDir("", "test_query_simple")
-	testutil.Ok(t, err)
-	defer os.RemoveAll(dir)
-
-	closeFn := spinup(t, spinupConfig{
-		promConfigFn:  selfScrapePromConfig,
+		},
 		workDir:       dir,
 		numPrometheus: 3,
 		numQueries:    2,
