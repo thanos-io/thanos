d054de4e4e369e0e554cd356fce9e4dfd187cf52
Fabian Reinartz <fab.reinartz@gmail.com>
2017-11-27 14:03:45 +0100

    e2e: add rule node to spinup, report errors properly

--- test/e2e/spinup_test.go
+++ test/e2e/spinup_test.go
@@ -3,6 +3,7 @@ package e2e_test
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"syscall"
@@ -11,10 +12,12 @@ import (
 
 type config struct {
 	promConfigFn func(port int) string
+	rules        string
 	workDir      string
 
 	numPrometheus int
 	numQueries    int
+	numRules      int
 }
 
 // NOTE: It is important to install Thanos before using this function to compile latest changes.
@@ -26,15 +29,12 @@ func spinup(t testing.TB, cfg config) (close func()) {
 		promDir := fmt.Sprintf("%s/data/prom%d", cfg.workDir, i)
 
 		if err := os.MkdirAll(promDir, 0777); err != nil {
+			t.Errorf("create dir failed: %s", err)
 			return func() {}
 		}
-		f, err := os.Create(promDir + "/prometheus.yml")
+		err := ioutil.WriteFile(promDir+"/prometheus.yml", []byte(cfg.promConfigFn(9090+i)), 0666)
 		if err != nil {
-			return func() {}
-		}
-		_, err = f.Write([]byte(cfg.promConfigFn(9090 + i)))
-		f.Close()
-		if err != nil {
+			t.Errorf("creating config failed: %s", err)
 			return func() {}
 		}
 
@@ -70,13 +70,43 @@ func spinup(t testing.TB, cfg config) (close func()) {
 		))
 	}
 
+	for i := 1; i <= cfg.numRules; i++ {
+		dbDir := fmt.Sprintf("%s/data/rule%d", cfg.workDir, i)
+
+		if err := os.MkdirAll(dbDir, 0777); err != nil {
+			t.Errorf("creating dir failed: %s", err)
+			return func() {}
+		}
+		err := ioutil.WriteFile(dbDir+"/rules.yaml", []byte(cfg.rules), 0666)
+		if err != nil {
+			t.Errorf("creating rule file failed: %s", err)
+			return func() {}
+		}
+
+		commands = append(commands, exec.Command("thanos", "rule",
+			"--debug.name", fmt.Sprintf("rule-%d", i),
+			"--data-dir", dbDir,
+			"--rule-dir", dbDir,
+			"--eval-interval", "1s",
+			"--grpc-address", fmt.Sprintf("0.0.0.0:%d", 19690+i),
+			"--http-address", fmt.Sprintf("0.0.0.0:%d", 19790+i),
+			"--cluster.address", fmt.Sprintf("0.0.0.0:%d", 19780+i),
+			"--cluster.advertise-address", fmt.Sprintf("127.0.0.1:%d", 19890+i),
+			"--cluster.peers", "127.0.0.1:19391",
+			"--cluster.peers", "127.0.0.1:19591",
+			"--log.level", "debug",
+		))
+	}
+
 	var stderr bytes.Buffer
 	stderrw := &safeWriter{Writer: &stderr}
 
 	close = func() {
 		for _, c := range closers {
 			c.Process.Signal(syscall.SIGTERM)
-			c.Wait()
+			if err := c.Wait(); err != nil {
+				t.Errorf("wait failed: %s", err)
+			}
 		}
 		t.Logf("STDERR\n %s", stderr.String())
 	}
@@ -84,6 +114,7 @@ func spinup(t testing.TB, cfg config) (close func()) {
 		cmd.Stderr = stderrw
 
 		if err := cmd.Start(); err != nil {
+			t.Errorf("start failed: %s", err)
 			close()
 			return func() {}
 		}
