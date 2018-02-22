760681e6d8bc7d918bf1f7f55cc4c4771925b93c
Fabian Reinartz <fab.reinartz@gmail.com>
2018-02-22 11:44:33 +0100

    main: add bucket command (#158)
    * main: add bucket command
    
    * bucket cmd: Rebased and refactored.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- cmd/thanos/bucket.go
+++ cmd/thanos/bucket.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
-
-	"github.com/improbable-eng/thanos/pkg/block"
-
-	"github.com/go-kit/kit/log/level"
-
-	"github.com/oklog/ulid"
+	"text/template"
+	"time"
 
 	"cloud.google.com/go/storage"
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/improbable-eng/thanos/pkg/block"
 	"github.com/improbable-eng/thanos/pkg/objstore"
 	"github.com/improbable-eng/thanos/pkg/objstore/gcs"
 	"github.com/oklog/run"
+	"github.com/oklog/ulid"
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
@@ -33,7 +33,7 @@ func registerBucket(m map[string]setupFunc, app *kingpin.Application, name strin
 
 	check := cmd.Command("check", "verify all blocks in the bucket")
 
-	repair := check.Flag("repair", "attempt to repair blocks for which issues were detected").
+	checkRepair := check.Flag("repair", "attempt to repair blocks for which issues were detected").
 		Short('r').Default("false").Bool()
 
 	m[name+" check"] = func(g *run.Group, logger log.Logger, reg *prometheus.Registry, _ opentracing.Tracer) error {
@@ -48,7 +48,19 @@ func registerBucket(m map[string]setupFunc, app *kingpin.Application, name strin
 
 		bkt := gcs.NewBucket(*gcsBucket, gcsClient.Bucket(*gcsBucket), reg)
 
-		return runBucketCheck(logger, bkt, *repair)
+		return runBucketCheck(logger, bkt, *checkRepair)
+	}
+
+	ls := cmd.Command("ls", "list all blocks in the bucket")
+
+	lsOutput := ls.Flag("ouput", "format in which to print each block's information; may be 'json' or custom template").
+		Short('o').Default("").String()
+
+	m[name+" ls"] = func(g *run.Group, logger log.Logger, _ *prometheus.Registry, _ opentracing.Tracer) error {
+		// Dummy actor to immediately kill the group after the run function returns.
+		g.Add(func() error { return nil }, func(error) {})
+
+		return runBucketList(*gcsBucket, *lsOutput)
 	}
 }
 
@@ -148,4 +160,72 @@ func repairBlock(ctx context.Context, bkt objstore.Bucket, id ulid.ULID) (resid 
 		return resid, errors.Wrapf(err, "deleting old block %s failed", id)
 	}
 	return resid, nil
+}
+
+func parseMeta(ctx context.Context, bkt objstore.Bucket, name string) (block.Meta, error) {
+	rc, err := bkt.Get(ctx, path.Join(name, "meta.json"))
+	if err != nil {
+		return block.Meta{}, errors.Wrap(err, "get reader for meta.json")
+	}
+	defer rc.Close()
+
+	// Do a full decode/encode cycle to ensure we only print valid JSON.
+	var m block.Meta
+
+	if err := json.NewDecoder(rc).Decode(&m); err != nil {
+		return block.Meta{}, errors.Wrap(err, "deocde meta.json")
+	}
+	return m, nil
+}
+
+func runBucketList(gcsBucket, format string) error {
+	gcsClient, err := storage.NewClient(context.Background())
+	if err != nil {
+		return errors.Wrap(err, "create GCS client")
+	}
+
+	var bkt objstore.Bucket
+	bkt = gcs.NewBucket(gcsBucket, gcsClient.Bucket(gcsBucket), nil)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	var printBlock func(name string) error
+
+	switch format {
+	case "":
+		printBlock = func(name string) error {
+			fmt.Fprintln(os.Stdout, name[:len(name)-1])
+			return nil
+		}
+	case "json":
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "\t")
+
+		printBlock = func(name string) error {
+			m, err := parseMeta(ctx, bkt, name)
+			if err != nil {
+				return err
+			}
+			return enc.Encode(&m)
+		}
+	default:
+		tmpl, err := template.New("").Parse(format)
+		if err != nil {
+			return errors.Wrap(err, "invalid template")
+		}
+		printBlock = func(name string) error {
+			m, err := parseMeta(ctx, bkt, name)
+			if err != nil {
+				return err
+			}
+
+			if err := tmpl.Execute(os.Stdout, &m); err != nil {
+				return errors.Wrap(err, "execute template")
+			}
+			fmt.Fprintln(os.Stdout, "")
+			return nil
+		}
+	}
+	return bkt.Iter(ctx, "", printBlock)
 }
