0a41afc99ace81264d96a5f6ea2804b3a8d867bc
Mark DeNeve <markd@Marks-MacBook-Pro-2.local>
2018-08-30 18:51:18 -0400

    rolling back change for go_1_11 fork

--- cmd/thanos/downsample.go
+++ cmd/thanos/downsample.go
@@ -34,7 +34,7 @@ func registerDownsample(m map[string]setupFunc, app *kingpin.Application, name s
 		Default("./data").String()
 
 	gcsBucket := cmd.Flag("gcs.bucket", "Google Cloud Storage bucket name for stored blocks.").
-		PlaceHolder("<bucket>").String()
+		PlaceHolder("<bucket>").Required().String()
 
 	s3Config := s3.RegisterS3Params(cmd)
 
