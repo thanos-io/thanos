aee15beb3396f0c28f741e3174e850dac3df4cce
Mark DeNeve <markd@Marks-MacBook-Pro-2.local>
2018-08-29 20:19:03 -0400

    Fix for upstream bug #494 https://github.com/improbable-eng/thanos/issues/494

--- cmd/thanos/downsample.go
+++ cmd/thanos/downsample.go
@@ -34,7 +34,7 @@ func registerDownsample(m map[string]setupFunc, app *kingpin.Application, name s
 		Default("./data").String()
 
 	gcsBucket := cmd.Flag("gcs.bucket", "Google Cloud Storage bucket name for stored blocks.").
-		PlaceHolder("<bucket>").Required().String()
+		PlaceHolder("<bucket>").String()
 
 	s3Config := s3.RegisterS3Params(cmd)
 
