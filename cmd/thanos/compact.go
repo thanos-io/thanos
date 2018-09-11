8580d0cd8b41c292b0a13bccf7e295ec813b7238
jojohappy <sarahdj0917@gmail.com>
2018-09-11 23:05:09 +0800

    To set bucket flag required for component store and compact
    Signed-off-by: jojohappy <sarahdj0917@gmail.com>

--- cmd/thanos/compact.go
+++ cmd/thanos/compact.go
@@ -32,7 +32,7 @@ func registerCompact(m map[string]setupFunc, app *kingpin.Application, name stri
 		Default("./data").String()
 
 	bucketConf := cmd.Flag("objstore.config", "The object store configuration in yaml format.").
-		PlaceHolder("<bucket.config.yaml>").String()
+		PlaceHolder("<bucket.config.yaml>").Required().String()
 
 	syncDelay := modelDuration(cmd.Flag("sync-delay", "Minimum age of fresh (non-compacted) blocks before they are being processed.").
 		Default("30m"))
