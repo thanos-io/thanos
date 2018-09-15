9485f6b2def256a8e95e96c098f74c5cfe2c5a60
Christian Schlotter <chrischdi@users.noreply.github.com>
2018-09-15 10:15:30 +0200

    fix indention again

--- cmd/thanos/compact.go
+++ cmd/thanos/compact.go
@@ -177,7 +177,7 @@ func runCompact(
 		if err := compact.ApplyRetentionPolicyByResolution(ctx, logger, bkt, retentionByResolution); err != nil {
 			return errors.Wrap(err, fmt.Sprintf("retention failed"))
 		}
-                level.Info(logger).Log("msg", "retention iterations done")
+		level.Info(logger).Log("msg", "retention iterations done")
 		return nil
 	}
 
