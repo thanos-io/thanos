2b64ce08b28141438f1f37f02cba76d10efd9cb6
jojohappy <sarahdj0917@gmail.com>
2018-09-18 00:01:59 +0800

    Update test case for package objstore client
    Signed-off-by: jojohappy <sarahdj0917@gmail.com>

--- pkg/objstore/client/factory_test.go
+++ pkg/objstore/client/factory_test.go
@@ -9,20 +9,14 @@ import (
 )
 
 func TestErrorBucketConfig(t *testing.T) {
-	conf := `
-    type: FAKE-GCS
-    config:
-        bucket: test-bucket
-    `
+	conf := "testconf/fake-gcs.conf.yml"
 	_, err := NewBucket(log.NewNopLogger(), conf, nil, "bkt-client-test")
 	testutil.NotOk(t, err)
 	testutil.Assert(t, err != ErrNotFound, "it should not error with not found")
 }
 
 func TestBlankBucketConfigContent(t *testing.T) {
-	conf := `
-    type: GCS
-    `
+	conf := "testconf/blank-gcs.conf.yml"
 	_, err := NewBucket(log.NewNopLogger(), conf, nil, "bkt-client-test")
 	testutil.NotOk(t, err)
 	testutil.Assert(t, err != ErrNotFound, "it should not error with not found")
