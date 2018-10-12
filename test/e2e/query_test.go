d84dec08ad94b8b3f7d6fab02e054eb861dc798d
Ivan Valkov <ivanvalkov@improbable.io>
2018-10-12 13:57:32 +0100

    fixed a comment

--- test/e2e/query_test.go
+++ test/e2e/query_test.go
@@ -66,7 +66,7 @@ func TestQuery(t *testing.T) {
 	}
 }
 
-// TestQuerySimple runs a setup of Prometheus servers, sidecars, and query nodes and verifies that
+// testQuerySimple runs a setup of Prometheus servers, sidecars, and query nodes and verifies that
 // queries return data merged from all Prometheus servers. Additionally it verifies if deduplication works for query.
 func testQuerySimple(t *testing.T, conf testConfig) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
