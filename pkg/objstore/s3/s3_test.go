e45293faeeb2e894b8dae4252131b6668ee4c2a7
Giedrius Statkevičius <giedrius.statkevicius@adform.com>
2018-10-30 14:20:50 +0200

    objstore/s3: add unit tests for new function ParseConfig

---
+++ pkg/objstore/s3/s3_test.go
@@ -0,0 +1,33 @@
+package s3
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseConfig(t *testing.T) {
+	inputConfig := []byte(`
+	bucket: abcd
+	http_config:
+		idle_conn_timeout: 50s
+		max_idle_conns: 123
+	`)
+	cfg, err := ParseConfig(inputConfig)
+	if err != nil {
+		t.Errorf("failed to parse config: %s", err)
+	}
+	if time.Duration(cfg.HTTPConfig.IdleConnTimeout) != time.Duration(50*time.Second) {
+		t.Errorf("parsing of idle_conn_timeout failed: got %v, expected %v",
+			time.Duration(cfg.HTTPConfig.IdleConnTimeout), time.Duration(50*time.Second))
+	}
+	if time.Duration(cfg.HTTPConfig.TLSHandshakeTimeout) != time.Duration(10*time.Second) {
+		t.Errorf("overwritten defaults of tls_handshake_timeout: got %v, expected %v",
+			time.Duration(cfg.HTTPConfig.TLSHandshakeTimeout), time.Duration(10*time.Second))
+	}
+	if cfg.Bucket != "abcd" {
+		t.Errorf("parsing of bucket failed: got %v, expected %v", cfg.Bucket, "abcd")
+	}
+	if cfg.HTTPConfig.MaxIdleConns != 123 {
+		t.Errorf("parsing of max_idle_conns failed: got %v, expected %v", cfg.HTTPConfig.MaxIdleConns, 123)
+	}
+}
