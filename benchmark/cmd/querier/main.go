204316d2acbdb9127330aaca98c28389a2fd0dec
Dominic Green <dominicgreen1@gmail.com>
2018-11-08 16:59:41 +0000

    updating bench

--- benchmark/cmd/querier/main.go
+++ benchmark/cmd/querier/main.go
@@ -16,29 +16,25 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"strconv"
+	"math/rand"
 )
 
-// Querier is a server which will repeatedly perform a number of queries against a prometheus endpoint, collecting
-// statistics about its performance. Results are printed to stdout. This component is designed to sit inside the same
-// cluster as the prometheus instance to reduce variance from network uncertainties.
+// Querier will repeatedly perform a number of queries at random against a Prometheus endpoint, collecting
+// statistics about its performance.
 
 var (
-	fHost            = flag.String("host", "localhost", "The prometheus host to query.")
-	fPath			 = flag.String("path", "/api/v1/query_range", "Path to append if needed.")
+	fHost            = flag.String("host", "localhost", "The Prometheus host to query.")
+	fPath			 = flag.String("path", "/api/v1/query_range", "Path to append to host.")
 	fUsername	     = flag.String("username", "", "Username for basic auth (if needed).")
 	fPassword	     = flag.String("password", "", "Password for basic auth (if needed).")
-	fQueries         = flag.String("queries", "", "A semicolon separated list of queries to test.")
+	fQueries         = flag.String("queries", "", "A semicolon separated list of queries to use.")
 	fQueryTotalTime  = flag.Duration("query-time", time.Minute, "The length of time that queries will be run. As many queries as possible will be run within this time.")
 	fQueryRangeStart = flag.Duration("range-offset-start", 1*time.Hour, "The offset to the start of the range to use in queries.")
 	fQueryRangeEnd   = flag.Duration("range-offset-end", 0, "The offset to the end of the range to use in queries.")
 	fConcurrentRequests = flag.Int("concurrent-requests", 1, "The maximum amount of concurrent requests to perform. Default is no concurrency.")
 	fLogLevel           = flag.String("log-level", "info", "The logging verbosity. Values are debug, info, warn, error.")
 	fServer			 = flag.Bool("server", false, "Run Prometheus Querier as a server.")
-)
-
-const (
-	// Stop querying if a query has more than `errorThreshold` errors.
-	errorThreshold = 250
+	fErrorThreshold = flag.Int("error-threshold", 250, "The maximum amount of errors per query before killing the run.")
 )
 
 type QueryResult struct {
@@ -47,11 +43,12 @@ type QueryResult struct {
 	Successes        int     `json:"successes"`
 	AverageDuration  float64 `json:"avg_duration"`
 	MinDuration      float64 `json:"min_duration"`
-	MaxDuration      float64 `json:"max_duration"`
-	TotalDuration    float64 `json:"total_duration"`
+	MaxDuration      float64 	`json:"max_duration"`
+	TotalDuration    time.Duration `json:"total_duration"`
 	QueriesPerSecond float64 `json:"queries_per_second"`
 
-	lock sync.Mutex
+	lock     sync.Mutex
+	QueryURL url.URL
 }
 
 func (q *QueryResult) GetErrors() int {
@@ -91,6 +88,11 @@ func main() {
 	}
 	logger = level.NewFilter(logger, lvl)
 
+	if *fQueries == ""{
+		level.Error(logger).Log("msg", "No Queries supplied.")
+		return
+	}
+
 	queries := strings.Split(*fQueries, ";")
 	now := time.Now()
 	start := now.Add(-*fQueryRangeStart)
@@ -98,10 +100,8 @@ func main() {
 	step := int(end.Sub(start).Seconds() / 250)
 
 	// Gather query statistics.
-	// TODO(dom): create map of all queries and randomly pick one to execute
-
-	results := make([]*QueryResult, len(queries))
-	for i, query := range queries {
+	var results []*QueryResult
+	for _, query := range queries {
 		queryURL := url.URL{
 			Scheme: "https",
 			Host:   *fHost,
@@ -118,20 +118,47 @@ func main() {
 			queryURL.User = url.UserPassword(*fUsername, *fPassword)
 		}
 
-		results[i] = runQuery(logger, queryURL, query, *fQueryTotalTime, *fConcurrentRequests)
+		results = append(results, &QueryResult{
+			Query:       query,
+			QueryURL:	 queryURL,
+			MinDuration: math.MaxInt64,
+		})
 	}
 
+	summaryStart := time.Now()
+	var wg sync.WaitGroup
+	for i := 0; i < *fConcurrentRequests; i++ {
+		wg.Add(1)
+		go performQuery(logger, results, *fQueryTotalTime, &wg)
+	}
+	wg.Wait()
+	duration := time.Since(summaryStart)
+
+	// Calculate summary for queries.
+	var totalDuration time.Duration
+	totalSuccess, totalError := 0, 0
+	for _, resultStorage := range results {
+		if resultStorage.Successes > 0 {
+			resultStorage.AverageDuration = resultStorage.TotalDuration.Seconds() / float64(resultStorage.Successes)
+			resultStorage.QueriesPerSecond = float64(resultStorage.Successes) / resultStorage.TotalDuration.Seconds()
+
+			totalDuration += resultStorage.TotalDuration
+			totalSuccess += resultStorage.Successes
+			totalError += resultStorage.Errors
+		}
+	}
+
+	// launch server if needed
 	if *fServer {
-		level.Info(logger).Log("msg", "Starting Server")
 		resultsBytes, err := json.Marshal(results)
 		if err != nil {
-			level.Error(logger).Log("could not marshal results")
+			level.Error(logger).Log("err", err)
 			return
 		}
 
 		http.HandleFunc("/results", func(w http.ResponseWriter, req *http.Request) {
 			if _, err := w.Write(resultsBytes); err != nil {
-				level.Error(logger).Log("msg", "failed to serve results")
+				level.Error(logger).Log("err", err)
 				w.WriteHeader(http.StatusInternalServerError)
 			}
 		})
@@ -142,6 +169,14 @@ func main() {
 		}
 	}
 
+	// Print results to Command Line.
+	avg := totalDuration.Seconds() / float64(totalSuccess)
+	level.Info(logger).Log("query", "ALL")
+	level.Info(logger).Log("-avg_duration_seconds", avg)
+	level.Info(logger).Log("-queries_per_seconds", float64(totalSuccess) / duration.Seconds())
+	level.Info(logger).Log("-query_success_total", totalSuccess)
+	level.Info(logger).Log("-query_errors_total", totalError)
+
 	for _, r := range results{
 		level.Info(logger).Log("query", r.Query)
 		level.Info(logger).Log("-avg_duration_seconds", r.AverageDuration)
@@ -151,62 +186,32 @@ func main() {
 	}
 }
 
-// runQuery runs a query a number of times. We aim for the query to run for about `queryTotalTime` before returning.
-func runQuery(logger log.Logger, queryURL url.URL, query string, timeout time.Duration, concurrentReqs int) *QueryResult {
-	res := &QueryResult{
-		Query:       query,
-		MinDuration: math.MaxInt64,
-	}
-
-	// Run queries concurrently.
-	var wg sync.WaitGroup
-	for i := 0; i < concurrentReqs; i++ {
-		wg.Add(1)
-		go performQuery(logger, res, queryURL, timeout, &wg)
-	}
-
-	wg.Wait()
-
-	level.Info(logger).Log("msg", fmt.Sprintf("Finished testing query %s", query))
-	return res
-}
-
-func performQuery(logger log.Logger, resultStorage *QueryResult, query url.URL, timeout time.Duration, wg *sync.WaitGroup) {
+func performQuery(logger log.Logger, results []*QueryResult, timeout time.Duration, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	var totalDuration time.Duration
-
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
 	timeoutC := time.After(timeout)
 	for {
 		// Perform as many queries as possible within this time.
 		select {
 		case <-timeoutC:
-			// Calculate averages on completion.
-			resultStorage.lock.Lock()
-			resultStorage.TotalDuration = totalDuration.Seconds()
-			if resultStorage.Successes > 0 {
-				resultStorage.AverageDuration = totalDuration.Seconds() / float64(resultStorage.Successes)
-				resultStorage.QueriesPerSecond = 1 / resultStorage.AverageDuration
-			}
-			resultStorage.lock.Unlock()
-
 			return
 		default:
 		}
 
+		n := r.Intn(len(results))
+		resultStorage := results[n]
 		// Drop out if we have too many errors.
-		if resultStorage.GetErrors() >= errorThreshold {
-			level.Error(logger).Log("msg", fmt.Sprintf("too many errors, skipping the remainder %s", resultStorage.Query))
+		if resultStorage.GetErrors() >= *fErrorThreshold {
+			level.Info(logger).Log("msg", fmt.Sprintf("too many errors, skipping the remainder %s", resultStorage.Query))
 			return
 		}
 
-		// Start timer.
 		queryStart := time.Now()
-
-		// Run query.
-		resp, err := http.Get(query.String())
-		// End timer.
+		resp, err := http.Get(resultStorage.QueryURL.String())
 		duration := time.Since(queryStart)
+
 		if err != nil || resp.StatusCode != 200 {
 			resultStorage.AddError()
 
@@ -223,26 +228,26 @@ func performQuery(logger log.Logger, resultStorage *QueryResult, query url.URL, 
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			resultStorage.AddError()
-			level.Error(logger).Log("msg", "failed to read query response body")
+			level.Error(logger).Log("err",  err)
 			continue
 		}
 
 		var promResult PromResult
 		if err := json.Unmarshal(body, &promResult); err != nil {
 			resultStorage.AddError()
-			level.Error(logger).Log("msg", "failed to unmarshal prometheus result")
+			level.Error(logger).Log("err", err)
 			continue
 		}
 
 		if promResult.Status != "success" {
 			resultStorage.AddError()
-			level.Error(logger).Log("msg", fmt.Sprintf("prometheus query reported failure: %s", resp.Body))
+			level.Error(logger).Log("err", fmt.Sprintf("prometheus query reported failure: %s", resp.Body))
 			continue
 		}
 
-
-		totalDuration += duration
 		resultStorage.lock.Lock()
+		resultStorage.TotalDuration += duration
+
 		if duration.Seconds() > resultStorage.MaxDuration {
 			resultStorage.MaxDuration = duration.Seconds()
 		}
