d714848795dada5c9e1aab2341a14cd485684e6c
jojohappy <sarahdj0917@gmail.com>
2019-01-18 11:25:57 +0800

    Address review comments
    Signed-off-by: jojohappy <sarahdj0917@gmail.com>

--- pkg/runutil/example_test.go
+++ pkg/runutil/example_test.go
@@ -1,86 +1,41 @@
 package runutil_test
 
 import (
+	"context"
 	"errors"
 	"fmt"
-	"os"
+	"log"
 	"time"
 
 	"github.com/improbable-eng/thanos/pkg/runutil"
-
-	"github.com/go-kit/kit/log"
 )
 
 func ExampleRepeat() {
-	stopc := make(chan struct{})
-
-	// It will stop Repeat 11 seconds later.
-	go func() {
-		select {
-		case <-time.After(11 * time.Second):
-			close(stopc)
-		}
-	}()
+	// It will stop Repeat 10 seconds later.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	// It will print out "Repeat" every 5 seconds.
-	err := runutil.Repeat(5*time.Second, stopc, func() error {
+	err := runutil.Repeat(5*time.Second, ctx.Done(), func() error {
 		fmt.Println("Repeat")
 		return nil
 	})
-
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 }
 
 func ExampleRetry() {
-	stopc := make(chan struct{})
+	// It will stop Retry 10 seconds later.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	// It will stop Retry 11 seconds later.
-	go func() {
-		select {
-		case <-time.After(11 * time.Second):
-			close(stopc)
-		}
-	}()
 	// It will print out "Retry" every 5 seconds.
-	err := runutil.Retry(5*time.Second, stopc, func() error {
+	err := runutil.Retry(5*time.Second, ctx.Done(), func() error {
 		fmt.Println("Retry")
-		return errors.New("Try to retry.")
+		return errors.New("Try to retry")
 	})
-
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-}
-
-func ExampleRetryWithLog() {
-	logger := log.NewLogfmtLogger(os.Stdout)
-	stopc := make(chan struct{})
-
-	// It will stop RetryWithLog 11 seconds later.
-	go func() {
-		select {
-		case <-time.After(11 * time.Second):
-			close(stopc)
-		}
-	}()
-
-	// It will print out "RetryWithLog" every 5 seconds.
-	err := runutil.RetryWithLog(logger, 5*time.Second, stopc, func() error {
-		fmt.Println("RetryWithLog")
-		return errors.New("Try to retry.")
-	})
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Output:
-	// RetryWithLog
-	// level=error msg="function failed. Retrying in next tick" err="Try to retry."
-	// RetryWithLog
-	// level=error msg="function failed. Retrying in next tick" err="Try to retry."
-	// RetryWithLog
-	// level=error msg="function failed. Retrying in next tick" err="Try to retry."
-	// Try to retry.
 }
