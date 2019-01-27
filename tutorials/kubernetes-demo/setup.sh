f4aa202e6ed33ce0f1b6dbc28cb3720bd5b73555
Bartek Plotka <bwplotka@gmail.com>
2019-01-27 21:46:44 +0000

    Extending basic prometheus case.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- tutorials/kubernetes-demo/setup.sh
+++ tutorials/kubernetes-demo/setup.sh
@@ -19,7 +19,7 @@ minikube start --cache-images --vm-driver=kvm2 -p eu1 --kubernetes-version="v1.1
 kubectl config use-context eu1
 kubectl apply -f manifests/prometheus.yaml
 kubectl apply -f manifests/prometheus-rules.yaml
-
+kubectl apply -f manifests/kube-state-metrics.yaml
 
 
 # Issues.
@@ -27,3 +27,41 @@ kubectl apply -f manifests/prometheus-rules.yaml
 # - loadbalancer type not possible. It works with minikube tunnel, but it works only for single cluster.
 #  NodePort needs to be used.
 # - mounting config at prometheus.yml does not work - default one overwrites.
+# - kubectl exec -it prometheus-0 -- /bin/kill -SIGHUP 1
+# kube sched and controller metrics - descoping: # Hacks to make kube-scheduler and kube-controller-manager metrics available:
+#---
+#apiVersion: v1
+#kind: Service
+#metadata:
+#  namespace: kube-system
+#  name: kube-scheduler-monitoring
+#  labels:
+#    app: kube-scheduler
+#spec:
+#  selector:
+#    component: kube-scheduler
+#  type: ClusterIP
+#  clusterIP: None
+#  ports:
+#    - name: http
+#      port: 10251
+#      targetPort: 10251
+#      protocol: TCP
+#---
+#apiVersion: v1
+#kind: Service
+#metadata:
+#  namespace: kube-system
+#  name: kube-controller-manager-monitoring
+#  labels:
+#    app: kube-controller-manager
+#spec:
+#  selector:
+#    app: kube-controller-manager
+#  type: ClusterIP
+#  clusterIP: None
+#  ports:
+#    - name: http
+#      port: 10252
+#      targetPort: 10252
+#      protocol: TCP
