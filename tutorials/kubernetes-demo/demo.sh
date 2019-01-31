82a8beb728252a5bf4e3ca3f0303b3ce674ed316
Bartek Plotka <bwplotka@gmail.com>
2019-01-31 20:34:18 +0000

    Finished 3 steps.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- tutorials/kubernetes-demo/demo.sh
+++ tutorials/kubernetes-demo/demo.sh
@@ -41,11 +41,23 @@ ro "kubectl --context=us1 apply -f manifests/prometheus-ha-sidecar.yaml" "cat ma
 r "kubectl --context=eu1 get po"
 r "kubectl --context=us1 get po"
 
-# Second step: Query
-r "cat manifests/thanos-query.yaml | less"
-ro "kubectl --context=eu1 apply -f manifests/thanos-query.yaml" "cat manifests/thanos-query.yaml | sed \"s#%%SIDECAR_US1_0_URL%%#\$(minikube -p us1 service sidecar --format=\"{{.IP}}:{{.Port}}\")#g\" |  sed \"s#%%SIDECAR_US1_1_URL%%#\$(minikube -p us1 service sidecar-1 --format=\"{{.IP}}:{{.Port}}\")#g\" | kubectl --context=eu1 apply -f -"
+# Second step: querier
+r "cat manifests/thanos-querier.yaml | less"
+ro "kubectl --context=eu1 apply -f manifests/thanos-querier.yaml" "cat manifests/thanos-querier.yaml | sed \"s#%%SIDECAR_US1_0_URL%%#\$(minikube -p us1 service sidecar --format=\"{{.IP}}:{{.Port}}\")#g\" |  sed \"s#%%SIDECAR_US1_1_URL%%#\$(minikube -p us1 service sidecar-1 --format=\"{{.IP}}:{{.Port}}\")#g\" | kubectl --context=eu1 apply -f -"
 r "kubectl --context=eu1 get po"
-ro "open \$(minikube -p eu1 service thanos-query --url)/graph" "google-chrome --app=\"\$(minikube -p eu1 service thanos-query --url)/graph?g0.range_input=2h&g0.expr=avg(container_memory_usage_bytes)%20by%20(cluster,replica)&g0.tab=0\" > /dev/null"
+# Show on artifical data!
+ro "open \$(minikube -p eu1 service thanos-querier --url)/graph" "google-chrome --app=\"\$(minikube -p eu1 service thanos-querier --url)/graph?g0.range_input=2h&g0.expr=avg(container_memory_usage_bytes)%20by%20(cluster,replica)&g0.tab=0\" > /dev/null"
+
+# Third: Connect grafana to querier
+r "colordiff -y manifests/grafana-datasources.yaml manifests/grafana-datasources-querier.yaml | less"
+r "kubectl --context=eu1 apply -f manifests/grafana-datasources-querier.yaml"
+r "kubectl --context=eu1 delete po \$(kubectl --context=eu1 get po -l app=grafana -o jsonpath={.items..metadata.name})"
+r "kubectl --context=eu1 get po"
+ro "open \$(minikube -p eu1 service grafana --url)" "google-chrome --app=\"`minikube -p eu1 service grafana --url`/d/pods_memory/pods-memory?orgId=1\" > /dev/null"
+
+# Show result
+rc "open slides/4-initial-setup.svg"
+
 
 rc "open slides/10-the-end.svg"
 
