988196a210725bf95a3960958f3d93d5261a51dc
mreichardt95 <mreichardt@vwd.com>
2019-02-06 14:03:51 +0100

    rule: adjusted parseFlagLabels test to use bool instead of err

--- cmd/thanos/rule_test.go
+++ cmd/thanos/rule_test.go
@@ -12,41 +12,41 @@ import (
 
 func Test_parseFlagLabels(t *testing.T) {
 	var tData = []struct {
-		s   []string
-		err error
+		s         []string
+		expectErr bool
 	}{
 		{
-			s:   []string{`labelName="LabelVal"`, `_label_Name="LabelVal"`, `label_name="LabelVal"`, `LAb_el_Name="LabelValue"`, `lab3l_Nam3="LabelValue"`}, // Valid
-			err: nil,
+			s:         []string{`labelName="LabelVal"`, `_label_Name="LabelVal"`, `label_name="LabelVal"`, `LAb_el_Name="LabelValue"`, `lab3l_Nam3="LabelValue"`},
+			expectErr: false,
 		},
 		{
-			s:   []string{`label-Name="LabelVal"`}, //Unsupported labelname
-			err: errors.New(""),
+			s:         []string{`label-Name="LabelVal"`}, //Unsupported labelname
+			expectErr: true,
 		},
 		{
-			s:   []string{`label:Name="LabelVal"`}, //Unsupported labelname
-			err: errors.New(""),
+			s:         []string{`label:Name="LabelVal"`}, //Unsupported labelname
+			expectErr: true,
 		},
 		{
-			s:   []string{`1abelName="LabelVal"`}, //Unsupported labelname
-			err: errors.New(""),
+			s:         []string{`1abelName="LabelVal"`}, //Unsupported labelname
+			expectErr: true,
 		},
 		{
-			s:   []string{`label_Name"LabelVal"`}, //Missing "=" seprator
-			err: errors.New(""),
+			s:         []string{`label_Name"LabelVal"`}, //Missing "=" seprator
+			expectErr: true,
 		},
 		{
-			s:   []string{`label_Name= "LabelVal"`}, //whitespace invalid syntax
-			err: errors.New(""),
+			s:         []string{`label_Name= "LabelVal"`}, //Whitespace invalid syntax
+			expectErr: true,
 		},
 		{
-			s:   []string{`label_name=LabelVal`}, //Missing quotes invalid syntax
-			err: errors.New(""),
+			s:         []string{`label_name=LabelVal`}, //Missing quotes invalid syntax
+			expectErr: true,
 		},
 	}
 	for _, td := range tData {
 		_, err := parseFlagLabels(td.s)
-		testutil.Equals(t, err == nil, td.err == nil)
+		testutil.Equals(t, err != nil, td.expectErr)
 	}
 }
 
