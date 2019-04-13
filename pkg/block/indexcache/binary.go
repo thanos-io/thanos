b3375e1efe8e112f946a6187ad7506bdf60e8610
Giedrius Statkevičius <giedriuswork@gmail.com>
2019-04-13 12:00:28 +0300

    indexcache: add initial BinaryCache skeleton

--- pkg/block/indexcache/binary.go
+++ pkg/block/indexcache/binary.go
@@ -2,6 +2,9 @@ package indexcache
 
 import (
 	"github.com/go-kit/kit/log"
+	"github.com/improbable-eng/thanos/pkg/runutil"
+	"github.com/pkg/errors"
+	"github.com/prometheus/tsdb/fileutil"
 	"github.com/prometheus/tsdb/index"
 	"github.com/prometheus/tsdb/labels"
 )
@@ -15,6 +18,37 @@ type BinaryCache struct {
 
 // WriteIndexCache writes an index cache into the specified filename.
 func (c *BinaryCache) WriteIndexCache(indexFn string, fn string) error {
+	indexFile, err := fileutil.OpenMmapFile(indexFn)
+	if err != nil {
+		return errors.Wrapf(err, "open mmap index file %s", indexFn)
+	}
+	defer runutil.CloseWithLogOnErr(c.logger, indexFile, "close index cache mmap file from %s", indexFn)
+
+	b := realByteSlice(indexFile.Bytes())
+	indexr, err := index.NewReader(b)
+	if err != nil {
+		return errors.Wrap(err, "open index reader")
+	}
+	defer runutil.CloseWithLogOnErr(c.logger, indexr, "load index cache reader")
+
+	// We assume reader verified index already.
+	symbols, err := getSymbolTable(b)
+	if err != nil {
+		return err
+	}
+
+	// Now it is time to write it.
+	w, err := index.NewWriter(fn)
+	if err != nil {
+		return err
+	}
+	defer runutil.CloseWithLogOnErr(c.logger, w, "index writer")
+
+	err = w.AddSymbols(symbols)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- pkg/block/indexcache/common.go
+++ pkg/block/indexcache/common.go
@@ -1,6 +1,7 @@
 package indexcache
 
 import (
+	"github.com/pkg/errors"
 	"github.com/prometheus/tsdb/index"
 	"github.com/prometheus/tsdb/labels"
 )
@@ -13,4 +14,46 @@ type IndexCache interface {
 		lvals map[string][]string,
 		postings map[labels.Label]index.Range,
 		err error)
+}
+
+type realByteSlice []byte
+
+func (b realByteSlice) Len() int {
+	return len(b)
+}
+
+func (b realByteSlice) Range(start, end int) []byte {
+	return b[start:end]
+}
+
+func (b realByteSlice) Sub(start, end int) index.ByteSlice {
+	return b[start:end]
+}
+
+func getSymbolTable(b index.ByteSlice) (map[string]struct{}, error) {
+	version := int(b.Range(4, 5)[0])
+
+	if version != 1 && version != 2 {
+		return nil, errors.Errorf("unknown index file version %d", version)
+	}
+
+	toc, err := index.NewTOCFromByteSlice(b)
+	if err != nil {
+		return nil, errors.Wrap(err, "read TOC")
+	}
+
+	symbolsV2, symbolsV1, err := index.ReadSymbols(b, version, int(toc.Symbols))
+	if err != nil {
+		return nil, errors.Wrap(err, "read symbols")
+	}
+
+	symbolsTable := make(map[string]struct{}, len(symbolsV1)+len(symbolsV2))
+	for _, s := range symbolsV1 {
+		symbolsTable[s] = struct{}{}
+	}
+	for _, s := range symbolsV2 {
+		symbolsTable[s] = struct{}{}
+	}
+
+	return symbolsTable, nil
 }
