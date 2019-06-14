ecaa38b8d4dbbb624ea2c264ac0076180a705342
Xiaobo.Zhao <shaulboozhiao@gmail.com>
2019-06-14 23:15:00 +0800

    Update proxy.go
    delete space

--- pkg/store/proxy.go
+++ pkg/store/proxy.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/improbable-eng/thanos/pkg/tracing"
-
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
