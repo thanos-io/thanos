c05d89afa24e2d8d77b3ac02e2a70aa1b6be333f
Giedrius Statkevičius <giedriuswork@gmail.com>
2019-07-03 20:29:13 +0300

    runutil: explicitly ignore return values

--- pkg/runutil/runutil.go
+++ pkg/runutil/runutil.go
@@ -117,7 +117,7 @@ func CloseWithLogOnErr(logger log.Logger, closer io.Closer, format string, a ...
 
 // ExhaustCloseWithLogOnErr closes the io.ReadCloser with a log message on error but exhausts the reader before.
 func ExhaustCloseWithLogOnErr(logger log.Logger, readcloser io.ReadCloser, format string, a ...interface{}) {
-	io.Copy(ioutil.Discard, readcloser)
+	var _, _ = io.Copy(ioutil.Discard, readcloser) // best effort
 	CloseWithLogOnErr(logger, readcloser, format, a...)
 }
 
@@ -134,6 +134,6 @@ func CloseWithErrCapture(err *error, closer io.Closer, format string, a ...inter
 
 // ExhaustCloseWithErrCapture closes the io.ReadCloser with error capture but exhausts the reader before.
 func ExhaustCloseWithErrCapture(err *error, readcloser io.ReadCloser, format string, a ...interface{}) {
-	io.Copy(ioutil.Discard, readcloser)
+	var _, _ = io.Copy(ioutil.Discard, readcloser) // best effort
 	CloseWithErrCapture(err, readcloser, format, a...)
 }
