71d38f714960a9a6a5fd937435f7cc313d1cc330
Will Hegedus <wbhegedus@liberty.edu>
2019-08-28 02:35:52 -0400

    prevent EOF errors in Azure objstore
    Signed-off-by: Will Hegedus <wbhegedus@liberty.edu>

--- pkg/objstore/azure/azure.go
+++ pkg/objstore/azure/azure.go
@@ -190,15 +190,19 @@ func (b *Bucket) getBlobReader(ctx context.Context, name string, offset, length 
 	}
 
 	var size int64
-	if length > 0 {
+	// If a length is specified and it won't go past the end of the file,
+	// then set it as the size.
+	if length > 0 && length <= props.ContentLength()-offset {
 		size = length
+		level.Debug(b.logger).Log("msg", "set size to length", "size", size, "length", length, "offset", offset, "name", name)
 	} else {
 		size = props.ContentLength() - offset
+		level.Debug(b.logger).Log("msg", "set size to go to EOF", "contentlength", props.ContentLength(), "size", size, "length", length, "offset", offset, "name", name)
 	}
 
 	destBuffer := make([]byte, size)
 
-	if err := blob.DownloadBlobToBuffer(context.Background(), blobURL.BlobURL, offset, length,
+	if err := blob.DownloadBlobToBuffer(context.Background(), blobURL.BlobURL, offset, size,
 		destBuffer, blob.DownloadFromBlobOptions{
 			BlockSize:   blob.BlobDefaultDownloadBlockSize,
 			Parallelism: uint16(3),
