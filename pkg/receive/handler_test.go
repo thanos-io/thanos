thanos-io/thanos

commit 19e59ef5369c2134ae3295bd29b86b05b82cb980

Fixes out of order samples handling (#1563)
* Fixes out of order samples handling

Signed-off-by: Devin Trejo <dtrejo@palantir.com>

* Ignore errcheck for rollback

Signed-off-by: Devin Trejo <dtrejo@palantir.com>

* Process all valid samples in remote-write call

Signed-off-by: Devin Trejo <dtrejo@palantir.com>

* pkg/receive: surface conflict error

This commit adds support to the receive component to identify if a
failed request is due to an ErrOutOfOrderSample error. If a request is
determined to have failed due to this reason, the handler responds with
a 409 Conflict status.

Fixes: #1509
Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

* Remove rollback from errcheck exclude

Signed-off-by: Devin Trejo <dtrejo@palantir.com>

* Return conflict for duplicate samples

Signed-off-by: Devin Trejo <dtrejo@palantir.com>

* Summarize any fast-add errors rather than logging every occurrence of the error.

Signed-off-by: Devin Trejo <dtrejo@palantir.com>

pkg/receive/handler.go
@@ -22,6 +22,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/route"
 	"github.com/prometheus/prometheus/prompb"
+	"github.com/prometheus/prometheus/storage"
 	terrors "github.com/prometheus/prometheus/tsdb/errors"
 	extpromhttp "github.com/thanos-io/thanos/pkg/extprom/http"
 	"github.com/thanos-io/thanos/pkg/runutil"
@@ -231,6 +232,10 @@ func (h *Handler) receive(w http.ResponseWriter, r *http.Request) {
 	// destined for the local node will be written to the receiver.
 	// Time series will be replicated as necessary.
 	if err := h.forward(r.Context(), tenant, rep, &wreq); err != nil {
+		if hasCause(err, isConflict) {
+			http.Error(w, err.Error(), http.StatusConflict)
+			return
+		}
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -368,7 +373,7 @@ func (h *Handler) parallelizeRequests(ctx context.Context, tenant string, replic
 				return
 			}
 			if res.StatusCode != http.StatusOK {
-				err = errors.New(res.Status)
+				err = errors.New(strconv.Itoa(res.StatusCode))
 				level.Error(h.logger).Log("msg", "forwarding returned non-200 status", "err", err, "endpoint", endpoint)
 				ec <- err
 				return
@@ -423,9 +428,33 @@ func (h *Handler) replicate(ctx context.Context, tenant string, wreq *prompb.Wri
 	err := h.parallelizeRequests(ctx, tenant, replicas, wreqs)
 	if errs, ok := err.(terrors.MultiError); ok {
 		if uint64(len(errs)) >= (h.options.ReplicationFactor+1)/2 {
-			return errors.New("did not meet replication threshold")
+			return errors.Wrap(err, "did not meet replication threshold")
 		}
 		return nil
 	}
 	return errors.Wrap(err, "could not replicate write request")
+}
+
+// hasCause performs a depth-first search of the causes of a nested MultiError
+// to determine if it contains an error that satisfies the given cause.
+func hasCause(err error, f func(error) bool) bool {
+	if err == nil {
+		return false
+	}
+	err = errors.Cause(err)
+	errs, ok := err.(terrors.MultiError)
+	if !ok {
+		return f(err)
+	}
+	for i := range errs {
+		if hasCause(errs[i], f) {
+			return true
+		}
+	}
+	return false
+}
+
+// isConflict returns whether or not the given error represents a conflict.
+func isConflict(err error) bool {
+	return err == storage.ErrDuplicateSampleForTimestamp || err == storage.ErrOutOfOrderSample || err == storage.ErrOutOfBounds || err.Error() == strconv.Itoa(http.StatusConflict)
 }

pkg/receive/writer.go
@@ -2,11 +2,13 @@ package receive
 
 import (
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/pkg/errors"
 	"github.com/prometheus/prometheus/prompb"
 
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/storage"
+	terrors "github.com/prometheus/prometheus/tsdb/errors"
 )
 
 // Appendable returns an Appender.
@@ -27,11 +29,19 @@ func NewWriter(logger log.Logger, app Appendable) *Writer {
 }
 
 func (r *Writer) Write(wreq *prompb.WriteRequest) error {
+	var (
+		numOutOfOrder  = 0
+		numDuplicates  = 0
+		numOutOfBounds = 0
+	)
+
 	app, err := r.append.Appender()
+
 	if err != nil {
 		return errors.Wrap(err, "failed to get appender")
 	}
 
+	var errs terrors.MultiError
 	for _, t := range wreq.Timeseries {
 		lset := make(labels.Labels, len(t.Labels))
 		for j := range t.Labels {
@@ -41,17 +51,41 @@ func (r *Writer) Write(wreq *prompb.WriteRequest) error {
 			}
 		}
 
+		// Append as many valid samples as possible, but keep track of the errors
 		for _, s := range t.Samples {
 			_, err = app.Add(lset, s.Timestamp, s.Value)
-			if err != nil {
-				return errors.Wrap(err, "failed to non-fast add")
+			switch err {
+			case nil:
+				continue
+			case storage.ErrOutOfOrderSample:
+				numOutOfOrder++
+				level.Debug(r.logger).Log("msg", "Out of order sample", "lset", lset.String(), "sample", s.String())
+			case storage.ErrDuplicateSampleForTimestamp:
+				numDuplicates++
+				level.Debug(r.logger).Log("msg", "Duplicate sample for timestamp", "lset", lset.String(), "sample", s.String())
+			case storage.ErrOutOfBounds:
+				numOutOfBounds++
+				level.Debug(r.logger).Log("msg", "Out of bounds metric", "lset", lset.String(), "sample", s.String())
 			}
 		}
 	}
 
-	if err := app.Commit(); err != nil {
-		return errors.Wrap(err, "failed to commit")
+	if numOutOfOrder > 0 {
+		level.Warn(r.logger).Log("msg", "Error on ingesting out-of-order samples", "num_dropped", numOutOfOrder)
+		errs.Add(errors.Wrapf(storage.ErrOutOfOrderSample, "failed to non-fast add %d samples", numOutOfOrder))
+	}
+	if numDuplicates > 0 {
+		level.Warn(r.logger).Log("msg", "Error on ingesting samples with different value but same timestamp", "num_dropped", numDuplicates)
+		errs.Add(errors.Wrapf(storage.ErrDuplicateSampleForTimestamp, "failed to non-fast add %d samples", numDuplicates))
+	}
+	if numOutOfBounds > 0 {
+		level.Warn(r.logger).Log("msg", "Error on ingesting samples that are too old or are too far into the future", "num_dropped", numOutOfBounds)
+		errs.Add(errors.Wrapf(storage.ErrOutOfBounds, "failed to non-fast add %d samples", numOutOfBounds))
 	}
 
-	return nil
+	if err := app.Commit(); err != nil {
+		errs.Add(errors.Wrap(err, "failed to commit"))
+	}
+
+	return errs.Err()
 }

pkg/receive/handler_test.go
@@ -1,0 +1,99 @@
+package receive
+
+import (
+	"net/http"
+	"strconv"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/storage"
+	terrors "github.com/prometheus/prometheus/tsdb/errors"
+)
+
+func TestHasCause(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		err  error
+		f    func(error) bool
+		out  bool
+	}{
+		{
+			name: "nil",
+			f:    isConflict,
+			out:  false,
+		},
+		{
+			name: "nil multierror",
+			err:  terrors.MultiError([]error{}),
+			f:    isConflict,
+			out:  false,
+		},
+		{
+			name: "non-matching multierror",
+			err: terrors.MultiError([]error{
+				errors.New("foo"),
+				errors.New("bar"),
+			}),
+			f:   isConflict,
+			out: false,
+		},
+		{
+			name: "nested non-matching multierror",
+			err: errors.Wrap(terrors.MultiError([]error{
+				errors.New("foo"),
+				errors.New("bar"),
+			}), "baz"),
+			f:   isConflict,
+			out: false,
+		},
+		{
+			name: "deep nested non-matching multierror",
+			err: errors.Wrap(terrors.MultiError([]error{
+				errors.New("foo"),
+				terrors.MultiError([]error{
+					errors.New("bar"),
+					errors.New("qux"),
+				}),
+			}), "baz"),
+			f:   isConflict,
+			out: false,
+		},
+		{
+			name: "matching multierror",
+			err: terrors.MultiError([]error{
+				storage.ErrOutOfOrderSample,
+				errors.New("foo"),
+				errors.New("bar"),
+			}),
+			f:   isConflict,
+			out: true,
+		},
+		{
+			name: "nested matching multierror",
+			err: errors.Wrap(terrors.MultiError([]error{
+				storage.ErrOutOfOrderSample,
+				errors.New("foo"),
+				errors.New("bar"),
+			}), "baz"),
+			f:   isConflict,
+			out: true,
+		},
+		{
+			name: "deep nested matching multierror",
+			err: errors.Wrap(terrors.MultiError([]error{
+				terrors.MultiError([]error{
+					errors.New("qux"),
+					errors.New(strconv.Itoa(http.StatusConflict)),
+				}),
+				errors.New("foo"),
+				errors.New("bar"),
+			}), "baz"),
+			f:   isConflict,
+			out: true,
+		},
+	} {
+		if hasCause(tc.err, tc.f) != tc.out {
+			t.Errorf("test case %s: expected %t, got %t", tc.name, tc.out, !tc.out)
+		}
+	}
+}

commit 612fe85a7a5a5c8de4c05530e964bdbdf57d9940

pkg/receive: fix regression + test external API
This PR begins by fixing a regression introduced in
19e59ef5369c2134ae3295bd29b86b05b82cb980, where returning a
MultiError threw off the error counting logic in the request
parallelization func, which could cause the wrong HTTP status code to be
returned. In order to protext against these kinds of regressions in the
future, we need to thoroughly test the external API of the receive
component. This commit now fixes the regression as well as adds tests
that verify the HTTP status codes returned by a hashring of receive
nodes and the time series in their DBs when they are in various
configurations and error states.

Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

cmd/thanos/receive.go
@@ -58,9 +58,9 @@ func registerReceive(m map[string]setupFunc, app *kingpin.Application) {
 
 	local := cmd.Flag("receive.local-endpoint", "Endpoint of local receive node. Used to identify the local node in the hashring configuration.").String()
 
-	tenantHeader := cmd.Flag("receive.tenant-header", "HTTP header to determine tenant for write requests.").Default("THANOS-TENANT").String()
+	tenantHeader := cmd.Flag("receive.tenant-header", "HTTP header to determine tenant for write requests.").Default(receive.DefaultTenantHeader).String()
 
-	replicaHeader := cmd.Flag("receive.replica-header", "HTTP header specifying the replica number of a write request.").Default("THANOS-REPLICA").String()
+	replicaHeader := cmd.Flag("receive.replica-header", "HTTP header specifying the replica number of a write request.").Default(receive.DefaultReplicaHeader).String()
 
 	replicationFactor := cmd.Flag("receive.replication-factor", "How many times to replicate incoming write requests.").Default("1").Uint64()
 

pkg/receive/handler.go
@@ -29,6 +29,13 @@ import (
 	"github.com/thanos-io/thanos/pkg/tracing"
 )
 
+const (
+	// DefaultTenantHeader is the default header used to designate the tenant making a write request.
+	DefaultTenantHeader = "THANOS-TENANT"
+	// DefaultReplicaHeader is the default header used to designate the replica count of a write request.
+	DefaultReplicaHeader = "THANOS-REPLICA"
+)
+
 // conflictErr is returned whenever an operation fails due to any conflict-type error.
 var conflictErr = errors.New("conflict")
 
@@ -326,6 +333,15 @@ func (h *Handler) parallelizeRequests(ctx context.Context, tenant string, replic
 					err = errors.New("storage is not ready")
 				} else {
 					err = h.writer.Write(wreqs[endpoint])
+					// When a MultiError is added to another MultiError, the error slices are concatenated, not nested.
+					// To avoid breaking the counting logic, we need to flatten the error.
+					if errs, ok := err.(terrors.MultiError); ok {
+						if countCause(errs, isConflict) > 0 {
+							err = errors.Wrap(conflictErr, errs.Error())
+						} else {
+							err = errors.New(errs.Error())
+						}
+					}
 				}
 				h.mtx.RUnlock()
 				if err != nil {

pkg/receive/writer.go
@@ -1,6 +1,8 @@
 package receive
 
 import (
+	"sync"
+
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/pkg/errors"
@@ -36,9 +38,8 @@ func (r *Writer) Write(wreq *prompb.WriteRequest) error {
 	)
 
 	app, err := r.append.Appender()
-
 	if err != nil {
-		return errors.Wrap(err, "failed to get appender")
+		return errors.Wrap(err, "get appender")
 	}
 
 	var errs terrors.MultiError
@@ -84,8 +85,82 @@ func (r *Writer) Write(wreq *prompb.WriteRequest) error {
 	}
 
 	if err := app.Commit(); err != nil {
-		errs.Add(errors.Wrap(err, "failed to commit"))
+		errs.Add(errors.Wrap(err, "commit samples"))
 	}
 
 	return errs.Err()
+}
+
+type fakeAppendable struct {
+	appender    storage.Appender
+	appenderErr func() error
+}
+
+var _ Appendable = &fakeAppendable{}
+
+func nilErrFn() error {
+	return nil
+}
+
+func (f *fakeAppendable) Appender() (storage.Appender, error) {
+	errf := f.appenderErr
+	if errf == nil {
+		errf = nilErrFn
+	}
+	return f.appender, errf()
+}
+
+type fakeAppender struct {
+	sync.Mutex
+	samples     map[string][]prompb.Sample
+	addErr      func() error
+	addFastErr  func() error
+	commitErr   func() error
+	rollbackErr func() error
+}
+
+var _ storage.Appender = &fakeAppender{}
+
+func newFakeAppender(addErr, addFastErr, commitErr, rollbackErr func() error) *fakeAppender {
+	if addErr == nil {
+		addErr = nilErrFn
+	}
+	if addFastErr == nil {
+		addFastErr = nilErrFn
+	}
+	if commitErr == nil {
+		commitErr = nilErrFn
+	}
+	if rollbackErr == nil {
+		rollbackErr = nilErrFn
+	}
+	return &fakeAppender{
+		samples:     make(map[string][]prompb.Sample),
+		addErr:      addErr,
+		addFastErr:  addFastErr,
+		commitErr:   commitErr,
+		rollbackErr: rollbackErr,
+	}
+}
+
+func (f *fakeAppender) Add(l labels.Labels, t int64, v float64) (uint64, error) {
+	f.Lock()
+	defer f.Unlock()
+	f.samples[l.String()] = append(f.samples[l.String()], prompb.Sample{Value: v, Timestamp: t})
+	return 0, f.addErr()
+}
+
+func (f *fakeAppender) AddFast(l labels.Labels, ref uint64, t int64, v float64) error {
+	f.Lock()
+	defer f.Unlock()
+	f.samples[l.String()] = append(f.samples[l.String()], prompb.Sample{Value: v, Timestamp: t})
+	return f.addFastErr()
+}
+
+func (f *fakeAppender) Commit() error {
+	return f.commitErr()
+}
+
+func (f *fakeAppender) Rollback() error {
+	return f.rollbackErr()
 }

pkg/receive/handler_test.go
@@ -1,11 +1,19 @@
 package receive
 
 import (
+	"bytes"
 	"net/http"
+	"net/http/httptest"
 	"strconv"
+	"sync"
 	"testing"
 
+	"github.com/go-kit/kit/log"
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
 	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/prompb"
 	"github.com/prometheus/prometheus/storage"
 	terrors "github.com/prometheus/prometheus/tsdb/errors"
 )
@@ -119,4 +127,421 @@ func TestCountCause(t *testing.T) {
 			t.Errorf("test case %s: expected %d, got %d", tc.name, tc.out, n)
 		}
 	}
+}
+
+func newHandlerHashring(appendables []*fakeAppendable, replicationFactor uint64) ([]*Handler, Hashring, func()) {
+	cfg := []HashringConfig{
+		{
+			Hashring: "test",
+		},
+	}
+	var closers []func()
+	var handlers []*Handler
+	for i := range appendables {
+		h := NewHandler(nil, &Options{
+			TenantHeader:      DefaultTenantHeader,
+			ReplicaHeader:     DefaultReplicaHeader,
+			ReplicationFactor: replicationFactor,
+			Writer:            NewWriter(log.NewNopLogger(), appendables[i]),
+		})
+		handlers = append(handlers, h)
+		ts := httptest.NewServer(h.router)
+		closers = append(closers, ts.Close)
+		h.options.Endpoint = ts.URL + "/api/v1/receive"
+		cfg[0].Endpoints = append(cfg[0].Endpoints, h.options.Endpoint)
+	}
+	hashring := newMultiHashring(cfg)
+	for _, h := range handlers {
+		h.Hashring(hashring)
+	}
+	close := func() {
+		for _, closer := range closers {
+			closer()
+		}
+	}
+	return handlers, hashring, close
+}
+
+func TestReceive(t *testing.T) {
+	appenderErrFn := func() error { return errors.New("failed to get appender") }
+	conflictErrFn := func() error { return storage.ErrOutOfBounds }
+	commitErrFn := func() error { return errors.New("failed to commit") }
+	wreq1 := &prompb.WriteRequest{
+		Timeseries: []prompb.TimeSeries{
+			{
+				Labels: []prompb.Label{
+					{
+						Name:  "foo",
+						Value: "bar",
+					},
+				},
+				Samples: []prompb.Sample{
+					{
+						Value:     1,
+						Timestamp: 1,
+					},
+					{
+						Value:     2,
+						Timestamp: 2,
+					},
+					{
+						Value:     3,
+						Timestamp: 3,
+					},
+				},
+			},
+		},
+	}
+	for _, tc := range []struct {
+		name              string
+		status            int
+		replicationFactor uint64
+		wreq              *prompb.WriteRequest
+		appendables       []*fakeAppendable
+	}{
+		{
+			name:              "size 1 success",
+			status:            http.StatusOK,
+			replicationFactor: 1,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 1 commit error",
+			status:            http.StatusInternalServerError,
+			replicationFactor: 1,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+			},
+		},
+		{
+			name:              "size 1 conflict",
+			status:            http.StatusConflict,
+			replicationFactor: 1,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 2 success",
+			status:            http.StatusOK,
+			replicationFactor: 1,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 success",
+			status:            http.StatusOK,
+			replicationFactor: 1,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 success with replication",
+			status:            http.StatusOK,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 commit error",
+			status:            http.StatusInternalServerError,
+			replicationFactor: 1,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 commit error with replication",
+			status:            http.StatusInternalServerError,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 appender error with replication",
+			status:            http.StatusInternalServerError,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender:    newFakeAppender(nil, nil, nil, nil),
+					appenderErr: appenderErrFn,
+				},
+				&fakeAppendable{
+					appender:    newFakeAppender(nil, nil, nil, nil),
+					appenderErr: appenderErrFn,
+				},
+				&fakeAppendable{
+					appender:    newFakeAppender(nil, nil, nil, nil),
+					appenderErr: appenderErrFn,
+				},
+			},
+		},
+		{
+			name:              "size 3 conflict with replication",
+			status:            http.StatusConflict,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 conflict and commit error with replication",
+			status:            http.StatusConflict,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, commitErrFn, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 with replication and one faulty",
+			status:            http.StatusOK,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(cycleErrors([]error{storage.ErrOutOfBounds, storage.ErrOutOfOrderSample, storage.ErrDuplicateSampleForTimestamp}), nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 with replication and one commit error",
+			status:            http.StatusOK,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 with replication and two conflicts",
+			status:            http.StatusConflict,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(cycleErrors([]error{storage.ErrOutOfBounds, storage.ErrOutOfOrderSample, storage.ErrDuplicateSampleForTimestamp}), nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(conflictErrFn, nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 with replication one conflict and one commit error",
+			status:            http.StatusInternalServerError,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(cycleErrors([]error{storage.ErrOutOfBounds, storage.ErrOutOfOrderSample, storage.ErrDuplicateSampleForTimestamp}), nil, nil, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+		{
+			name:              "size 3 with replication two commit errors",
+			status:            http.StatusInternalServerError,
+			replicationFactor: 3,
+			wreq:              wreq1,
+			appendables: []*fakeAppendable{
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, commitErrFn, nil),
+				},
+				&fakeAppendable{
+					appender: newFakeAppender(nil, nil, nil, nil),
+				},
+			},
+		},
+	} {
+		handlers, hashring, close := newHandlerHashring(tc.appendables, tc.replicationFactor)
+		defer close()
+		tenant := "test"
+		// Test from the point of view of every node
+		// so that we know status code does not depend
+		// on which node is erroring and which node is receiving.
+		for i, handler := range handlers {
+			// Test that the correct status is returned.
+			status, err := makeRequest(handler, tenant, tc.wreq)
+			if err != nil {
+				t.Fatalf("test case %q, handler %d: unexpectedly failed making HTTP request: %v", tc.name, tc.status, err)
+			}
+			if status != tc.status {
+				t.Errorf("test case %q, handler %d: got unexpected HTTP status code: expected %d, got %d", tc.name, i, tc.status, status)
+			}
+		}
+		// Test that each time series is stored
+		// the correct amount of times in each fake DB.
+		for _, ts := range tc.wreq.Timeseries {
+			lset := make(labels.Labels, len(ts.Labels))
+			for j := range ts.Labels {
+				lset[j] = labels.Label{
+					Name:  ts.Labels[j].Name,
+					Value: ts.Labels[j].Value,
+				}
+			}
+			for j, a := range tc.appendables {
+				var expected int
+				got := uint64(len(a.appender.(*fakeAppender).samples[lset.String()]))
+				if a.appenderErr == nil && endpointHit(t, hashring, tc.replicationFactor, handlers[j].options.Endpoint, tenant, &ts) {
+					// We have len(handlers) copies of each sample because the test case
+					// is run once for each handler and they all use the same appender.
+					expected = len(handlers) * len(ts.Samples)
+				}
+				if uint64(expected) != got {
+					t.Errorf("test case %q, labels %q: expected %d samples, got %d", tc.name, lset.String(), expected, got)
+				}
+			}
+		}
+	}
+}
+
+// endpointHit is a helper to determine if a given endpoint in a hashring would be selected
+// for a given time series, tenant, and replication factor.
+func endpointHit(t *testing.T, h Hashring, rf uint64, endpoint, tenant string, timeSeries *prompb.TimeSeries) bool {
+	var i uint64
+	for i = 0; i < rf; i++ {
+		e, err := h.GetN(tenant, timeSeries, i)
+		if err != nil {
+			t.Fatalf("got unexpected error querying hashring: %v", err)
+		}
+		if e == endpoint {
+			return true
+		}
+	}
+	return false
+}
+
+// cycleErrors returns an error generator that cycles through every given error.
+func cycleErrors(errs []error) func() error {
+	var mu sync.Mutex
+	var i int
+	return func() error {
+		mu.Lock()
+		defer mu.Unlock()
+		err := errs[i]
+		i++
+		if i >= len(errs) {
+			i = 0
+		}
+		return err
+	}
+}
+
+// makeRequest is a helper to make a correct request against a remote write endpoint given a request.
+func makeRequest(h *Handler, tenant string, wreq *prompb.WriteRequest) (int, error) {
+	buf, err := proto.Marshal(wreq)
+	if err != nil {
+		return 0, errors.Wrap(err, "marshal request")
+	}
+	req, err := http.NewRequest("POST", h.options.Endpoint, bytes.NewBuffer(snappy.Encode(nil, buf)))
+	if err != nil {
+		return 0, errors.Wrap(err, "create request")
+	}
+	req.Header.Add(h.options.TenantHeader, tenant)
+	res, err := h.client.Do(req)
+	if err != nil {
+		return 0, errors.Wrap(err, "make request")
+	}
+	return res.StatusCode, nil
 }