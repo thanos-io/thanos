3ae2be2103ebb35f3186c51c742dbe07ab6ae94c
yapo.yang <yang_yapo@126.com>
2019-12-10 15:28:31 +0800

    add tailing period for comment
    Signed-off-by: yapo.yang <yang_yapo@126.com>

--- cmd/thanos/rule.go
+++ cmd/thanos/rule.go
@@ -475,11 +475,10 @@ func runRule(
 	{
 		cancel := make(chan struct{})
 		g.Add(func() error {
-
-			//initialize rules
+			//initialize rules.
 			if err := reloadRules(logger, ruleFiles, ruleMgr, evalInterval, configSuccess, configSuccessTime, rulesLoaded); err != nil {
 				level.Error(logger).Log("msg", "initialize rules failed", "err", err)
-				//returns when initialize with invalid pattern error
+				//returns when initialize with invalid pattern error.
 				if _, ok := err.(*errInvalidPattern); ok {
 					return err
 				}
@@ -760,7 +759,7 @@ func reloadRules(logger log.Logger,
 	for _, pat := range ruleFiles {
 		fs, err := filepath.Glob(pat)
 		if err != nil {
-			//check errInvalidPattern when initialize
+			//check errInvalidPattern when initialize.
 			return &errInvalidPattern{err, pat}
 		}
 
