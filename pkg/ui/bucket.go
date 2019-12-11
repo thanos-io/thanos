fe6553adecde9f41da47a4f72137152850a3073e
yeya24 <yb532204897@gmail.com>
2019-12-10 20:30:49 -0500

    add status tab to bucket web
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- pkg/ui/bucket.go
+++ pkg/ui/bucket.go
@@ -3,10 +3,12 @@ package ui
 import (
 	"html/template"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/prometheus/common/route"
+	"github.com/prometheus/common/version"
 	extpromhttp "github.com/thanos-io/thanos/pkg/extprom/http"
 )
 
@@ -20,14 +22,23 @@ type Bucket struct {
 	Blocks      template.JS
 	RefreshedAt time.Time
 	Err         error
+
+	cwd   string
+	birth time.Time
 }
 
 func NewBucketUI(logger log.Logger, label string, flagsMap map[string]string) *Bucket {
+	cwd, err := os.Getwd()
+	if err != nil {
+		cwd = "<error retrieving current working directory>"
+	}
 	return &Bucket{
 		BaseUI:   NewBaseUI(logger, "bucket_menu.html", queryTmplFuncs()),
 		Blocks:   "[]",
 		Label:    label,
 		flagsMap: flagsMap,
+		cwd:      cwd,
+		birth:    time.Now(),
 	}
 }
 
@@ -38,6 +49,8 @@ func (b *Bucket) Register(r *route.Router, ins extpromhttp.InstrumentationMiddle
 	}
 
 	r.Get("/", instrf("root", b.root))
+	r.Get("/status", instrf("status", b.status))
+
 	r.Get("/static/*filepath", instrf("static", b.serveStaticAsset))
 }
 
@@ -51,4 +64,25 @@ func (b *Bucket) Set(data string, err error) {
 	b.RefreshedAt = time.Now()
 	b.Blocks = template.JS(string(data))
 	b.Err = err
+}
+
+func (b *Bucket) status(w http.ResponseWriter, r *http.Request) {
+	prefix := GetWebPrefix(b.logger, b.flagsMap, r)
+
+	b.executeTemplate(w, "status.html", prefix, struct {
+		Birth   time.Time
+		CWD     string
+		Version thanosVersion
+	}{
+		Birth: b.birth,
+		CWD:   b.cwd,
+		Version: thanosVersion{
+			Version:   version.Version,
+			Revision:  version.Revision,
+			Branch:    version.Branch,
+			BuildUser: version.BuildUser,
+			BuildDate: version.BuildDate,
+			GoVersion: version.GoVersion,
+		},
+	})
 }
