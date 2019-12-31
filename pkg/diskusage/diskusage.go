5b44ad213c27d172764416e8b476965bc1b99be3
Giedrius Statkevičius <giedriuswork@gmail.com>
2019-12-31 14:53:52 +0200

    diskusage: add missing trailing periods
    Signed-off-by: Giedrius Statkevičius <giedriuswork@gmail.com>

--- pkg/diskusage/diskusage.go
+++ pkg/diskusage/diskusage.go
@@ -2,7 +2,7 @@ package diskusage
 
 // Usage holds information about total and available storage on a volume.
 type Usage struct {
-	TotalBytes uint64 // Size of volume
-	FreeBytes  uint64 // Unused size
-	AvailBytes uint64 // Available bytes to a non-privileged user
+	TotalBytes uint64 // Size of volume.
+	FreeBytes  uint64 // Unused size.
+	AvailBytes uint64 // Available bytes to a non-privileged user.
 }
