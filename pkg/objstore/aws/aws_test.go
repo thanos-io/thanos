9d5603707b07f0db63aad332ae9fc1bc916958e5
Matthew Ceroni <matthew.ceroni@dat.com>
2020-01-30 16:06:02 -0800

    Add config tests
    Signed-off-by: Matthew Ceroni <matthew.ceroni@dat.com>

---
+++ pkg/objstore/aws/aws_test.go
@@ -0,0 +1,119 @@
+package aws
+
+import (
+	"testing"
+	"time"
+
+	"github.com/thanos-io/thanos/pkg/testutil"
+)
+
+func TestParseConfig(t *testing.T) {
+	input := []byte(`bucket: abcd`)
+
+	cfg, err := parseConfig(input)
+	testutil.Ok(t, err)
+
+	if cfg.Bucket != "abcd" {
+		t.Errorf("parsing of bucket failed: got %v, expected %v", cfg.Bucket, "abcd")
+	}
+}
+
+func TestParseConfig_DefaultHTTPConfig(t *testing.T) {
+	input := []byte(`bucket: abcd`)
+
+	cfg, err := parseConfig(input)
+	testutil.Ok(t, err)
+
+	if time.Duration(cfg.HTTPConfig.IdleConnTimeout) != time.Duration(90*time.Second) {
+		t.Errorf("parsing of idle_conn_timeout failed: got %v, expected %v",
+			time.Duration(cfg.HTTPConfig.IdleConnTimeout), time.Duration(90*time.Second))
+	}
+
+	if time.Duration(cfg.HTTPConfig.ResponseHeaderTimeout) != time.Duration(2*time.Minute) {
+		t.Errorf("parsing of response_header_timeout failed: got %v, expected %v",
+			time.Duration(cfg.HTTPConfig.IdleConnTimeout), time.Duration(2*time.Minute))
+	}
+
+	if cfg.HTTPConfig.InsecureSkipVerify {
+		t.Errorf("parsing of insecure_skip_verify failed: got %v, expected %v", cfg.HTTPConfig.InsecureSkipVerify, false)
+	}
+}
+
+func TestParseConfig_CustomHTTPConfig(t *testing.T) {
+	input := []byte(`bucket: abcd
+http_config:
+  insecure_skip_verify: true
+  idle_conn_timeout: 50s
+  response_header_timeout: 1m`)
+	cfg, err := parseConfig(input)
+	testutil.Ok(t, err)
+
+	if time.Duration(cfg.HTTPConfig.IdleConnTimeout) != time.Duration(50*time.Second) {
+		t.Errorf("parsing of idle_conn_timeout failed: got %v, expected %v",
+			time.Duration(cfg.HTTPConfig.IdleConnTimeout), time.Duration(50*time.Second))
+	}
+
+	if time.Duration(cfg.HTTPConfig.ResponseHeaderTimeout) != time.Duration(1*time.Minute) {
+		t.Errorf("parsing of response_header_timeout failed: got %v, expected %v",
+			time.Duration(cfg.HTTPConfig.IdleConnTimeout), time.Duration(1*time.Minute))
+	}
+
+	if !cfg.HTTPConfig.InsecureSkipVerify {
+		t.Errorf("parsing of insecure_skip_verify failed: got %v, expected %v", cfg.HTTPConfig.InsecureSkipVerify, false)
+	}
+}
+
+func TestValidate_OK(t *testing.T) {
+	input := []byte(`bucket: "bucket-name"
+access_key: "access_key"
+encrypt_sse: false
+secret_key: "secret_key"
+http_config:
+  insecure_skip_verify: false
+  idle_conn_timeout: 50s`)
+	cfg, err := parseConfig(input)
+	testutil.Ok(t, err)
+	testutil.Ok(t, validate(cfg))
+	testutil.Assert(t, cfg.PutUserMetadata != nil, "map should not be nil")
+
+	input2 := []byte(`bucket: "bucket-name"
+access_key: "access_key"
+encrypt_sse: false
+secret_key: "secret_key"
+put_user_metadata:
+  "X-Amz-Acl": "bucket-owner-full-control"
+http_config:
+  idle_conn_timeout: 0s`)
+	cfg2, err := parseConfig(input2)
+	testutil.Ok(t, err)
+	testutil.Ok(t, validate(cfg2))
+
+	testutil.Equals(t, "bucket-owner-full-control", cfg2.PutUserMetadata["X-Amz-Acl"])
+}
+
+func TestParseConfig_PartSize(t *testing.T) {
+	input := []byte(`bucket: "bucket-name"
+access_key: "access_key"
+encrypt_sse: false
+secret_key: "secret_key"
+http_config:
+  insecure_skip_verify: false
+  idle_conn_timeout: 50s`)
+
+	cfg, err := parseConfig(input)
+	testutil.Ok(t, err)
+	testutil.Assert(t, cfg.PartSize == 1024*1024*128, "when part size not set it should default to 128MiB")
+
+	input2 := []byte(`bucket: "bucket-name"
+access_key: "access_key"
+encrypt_sse: false
+secret_key: "secret_key"
+part_size: 104857600
+http_config:
+  insecure_skip_verify: false
+  idle_conn_timeout: 50s`)
+
+	cfg2, err := parseConfig(input2)
+	testutil.Ok(t, err)
+	testutil.Assert(t, cfg2.PartSize == 1024*1024*100, "when part size should be set to 100MiB")
+}
