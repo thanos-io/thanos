7336031a1c0de1812fa5ebe1b6182097362c615e
Matthias Loibl <mail@matthiasloibl.com>
2020-03-03 12:38:53 +0100

    Merge release-0.11 back into master (#2212)
    * Create release v0.11.0-rc.0 (#2156)
    
    * Update version to v0.11.0-rc.0
    
    * Update CHANGELOG with all PRs for v0.11
    
    * Improve CHANGELOG by being more explicit
    
    * Bumped minio-go library to v6.0.49, fixing an IAM bug in v6.0.45 (#2189)
    
    Signed-off-by: Kraig Amador <kraig.amador@ticketmaster.com>
    
    * Create release candidate  v0.11.0-rc.1 (#2192)
    
    Signed-off-by: Matthias Loibl <mail@matthiasloibl.com>
    
    * Release v0.11.0 (#2205)
    
    Signed-off-by: Matthias Loibl <mail@matthiasloibl.com>
    
    * Update VERSION to 0.12.0-dev
    
    Signed-off-by: Matthias Loibl <mail@matthiasloibl.com>
    
    * Resolve go.sum merge conflict and run go mod tidy
    
    Signed-off-by: Matthias Loibl <mail@matthiasloibl.com>
    
    Co-authored-by: Kraig Amador <508403+bigkraig@users.noreply.github.com>

--- tutorials/katacoda/thanos/1-globalview/courseBase.sh
+++ tutorials/katacoda/thanos/1-globalview/courseBase.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker pull quay.io/prometheus/prometheus:v2.14.0
-docker pull quay.io/thanos/thanos:v0.10.0
+docker pull quay.io/prometheus/prometheus:v2.16.0
+docker pull quay.io/thanos/thanos:0.11.0
