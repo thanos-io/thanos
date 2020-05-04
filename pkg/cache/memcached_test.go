1baa24cb85f793f9f39d6535f14c5092811f5b12
Peter Štibraný <peter.stibrany@grafana.com>
2020-05-04 17:03:24 +0200

    Fixed tests.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/cache/memcached_test.go
+++ pkg/cache/memcached_test.go
@@ -27,17 +27,15 @@ func TestMemcachedIndexCache(t *testing.T) {
 	value3 := []byte{3}
 
 	tests := map[string]struct {
-		setup          map[string][]byte
-		mockedErr      error
-		fetchKeys      []string
-		expectedHits   map[string][]byte
-		expectedMisses []string
+		setup        map[string][]byte
+		mockedErr    error
+		fetchKeys    []string
+		expectedHits map[string][]byte
 	}{
 		"should return no hits on empty cache": {
-			setup:          nil,
-			fetchKeys:      []string{key1, key2},
-			expectedHits:   nil,
-			expectedMisses: []string{key1, key2},
+			setup:        nil,
+			fetchKeys:    []string{key1, key2},
+			expectedHits: nil,
 		},
 		"should return no misses on 100% hit ratio": {
 			setup: map[string][]byte{
@@ -49,16 +47,14 @@ func TestMemcachedIndexCache(t *testing.T) {
 			expectedHits: map[string][]byte{
 				key1: value1,
 			},
-			expectedMisses: nil,
 		},
 		"should return hits and misses on partial hits": {
 			setup: map[string][]byte{
 				key1: value1,
 				key2: value2,
 			},
-			fetchKeys:      []string{key1, key3},
-			expectedHits:   map[string][]byte{key1: value1},
-			expectedMisses: []string{key3},
+			fetchKeys:    []string{key1, key3},
+			expectedHits: map[string][]byte{key1: value1},
 		},
 		"should return no hits on memcached error": {
 			setup: map[string][]byte{
@@ -66,10 +62,9 @@ func TestMemcachedIndexCache(t *testing.T) {
 				key2: value2,
 				key3: value3,
 			},
-			mockedErr:      errors.New("mocked error"),
-			fetchKeys:      []string{key1},
-			expectedHits:   nil,
-			expectedMisses: []string{key1},
+			mockedErr:    errors.New("mocked error"),
+			fetchKeys:    []string{key1},
+			expectedHits: nil,
 		},
 	}
 
@@ -83,9 +78,8 @@ func TestMemcachedIndexCache(t *testing.T) {
 			c.Store(ctx, testData.setup, time.Hour)
 
 			// Fetch postings from cached and assert on it.
-			hits, misses := c.Fetch(ctx, testData.fetchKeys)
+			hits := c.Fetch(ctx, testData.fetchKeys)
 			testutil.Equals(t, testData.expectedHits, hits)
-			testutil.Equals(t, testData.expectedMisses, misses)
 
 			// Assert on metrics.
 			testutil.Equals(t, float64(len(testData.fetchKeys)), prom_testutil.ToFloat64(c.requests))
