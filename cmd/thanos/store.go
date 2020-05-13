87606e480a9818af09cd32b6941895e971302383
Thota Harshitha Chowdary <37958850+Harshitha1234@users.noreply.github.com>
2020-05-13 22:04:29 +0530

    Update store.go
    Signed-off-by: Harshitha Chowdary <harshithachowdary.t17@iiits.in>

--- cmd/thanos/store.go
+++ cmd/thanos/store.go
@@ -45,7 +45,7 @@ func registerStore(m map[string]setupFunc, app *kingpin.Application) {
 	httpBindAddr, httpGracePeriod := regHTTPFlags(cmd)
 	grpcBindAddr, grpcGracePeriod, grpcCert, grpcKey, grpcClientCA := regGRPCFlags(cmd)
 
-	dataDir := cmd.Flag("data-dir", "Data directory in which to cache remote blocks.").
+	dataDir := cmd.Flag("data-dir", "Data directory on the disk space for local cache which stores information about all the remote blocks ").
 		Default("./data").String()
 
 	indexCacheSize := cmd.Flag("index-cache-size", "Maximum size of items held in the in-memory index cache. Ignored if --index-cache.config or --index-cache.config-file option is specified.").
