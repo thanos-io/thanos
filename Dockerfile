7349a0873ab7db3205c041fb79ec836cd223b8e9
Bartlomiej Plotka <bwplotka@gmail.com>
2020-05-14 18:30:31 +0100

    Fixed make docs; Updated last disprepancies. (#2611)
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM quay.io/prometheus/busybox:latest
 LABEL maintainer="The Thanos Authors"
 
-COPY thanos /bin/thanos
+COPY /thanos_tmp_for_docker /bin/thanos
 
 ENTRYPOINT [ "/bin/thanos" ]
--- scripts/genflagdocs.sh
+++ scripts/genflagdocs.sh
@@ -6,6 +6,7 @@ set -u
 
 EMBEDMD_BIN=${EMBEDMD_BIN:-embedmd}
 SED_BIN=${SED_BIN:-sed}
+THANOS_BIN=${THANOS_BIN:-${GOBIN}/thanos}
 
 function docs {
 # If check arg was passed, instead of the docs generation verifies if docs coincide with the codebase.
@@ -38,17 +39,17 @@ CHECK=${1:-}
 
 commands=("compact" "query" "rule" "sidecar" "store" "tools")
 for x in "${commands[@]}"; do
-    ./thanos "${x}" --help &> "docs/components/flags/${x}.txt"
+    ${THANOS_BIN} "${x}" --help &> "docs/components/flags/${x}.txt"
 done
 
 toolsCommands=("bucket" "rules-check")
 for x in "${toolsCommands[@]}"; do
-    ./thanos tools "${x}" --help &> "docs/components/flags/tools_${x}.txt"
+    ${THANOS_BIN} tools "${x}" --help &> "docs/components/flags/tools_${x}.txt"
 done
 
 toolsBucketCommands=("verify" "ls" "inspect" "web" "replicate" "downsample")
 for x in "${toolsBucketCommands[@]}"; do
-    ./thanos tools bucket "${x}" --help &> "docs/components/flags/tools_bucket_${x}.txt"
+    ${THANOS_BIN} tools bucket "${x}" --help &> "docs/components/flags/tools_bucket_${x}.txt"
 done
 
 # Remove white noise.
