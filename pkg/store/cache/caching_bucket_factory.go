2659848c3bc167522e255f67b99edd49606ff4b1
Peter Štibraný <peter.stibrany@grafana.com>
2020-05-15 16:39:49 +0200

    Metadata caching in bucket (#2579)
    * Added caching for Iter.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Added cache for Exists call for meta-files.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Added cache for reading block metadata files.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Make caching bucket configurable with different caches for different type of objects.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Fixed tests.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Added caching for ObjectSize. Enabled caching of index.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Lint feedback.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Use single set of metrics for all operations.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Constants.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Use operation specific config. Generic configuration is only for user.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Fix typo, make lint happy.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Simplify constants.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Simplify caching configuration.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Refactor cache configuration.
    
    Configuration is now passed to the cache when created.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Review feedback.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Fix operationRequests and operationHits for getRange.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Make codec for Iter results configurable.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Added header.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Renamed "dir" config to "blocks-iter".
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Bump default values for meta exists/doesntExist ttls.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Removed example how cache could be configured for index.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Address review feedback.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Get now implements streaming reader, and buffers object in memory.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Added test for partial read.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Removed unused function.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/store/cache/caching_bucket_factory.go
+++ pkg/store/cache/caching_bucket_factory.go
@@ -4,38 +4,71 @@
 package storecache
 
 import (
+	"regexp"
+	"strings"
+	"time"
+
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"gopkg.in/yaml.v2"
 
+	"github.com/thanos-io/thanos/pkg/block/metadata"
 	cache "github.com/thanos-io/thanos/pkg/cache"
 	"github.com/thanos-io/thanos/pkg/cacheutil"
+	"github.com/thanos-io/thanos/pkg/model"
 	"github.com/thanos-io/thanos/pkg/objstore"
 )
 
 // BucketCacheProvider is a type used to evaluate all bucket cache providers.
 type BucketCacheProvider string
 
-const (
-	MemcachedBucketCacheProvider BucketCacheProvider = "memcached" // Memcached cache-provider for caching bucket.
-)
+const MemcachedBucketCacheProvider BucketCacheProvider = "memcached" // Memcached cache-provider for caching bucket.
 
-// CachingBucketWithBackendConfig is a configuration of caching bucket used by Store component.
-type CachingBucketWithBackendConfig struct {
+// CachingWithBackendConfig is a configuration of caching bucket used by Store component.
+type CachingWithBackendConfig struct {
 	Type          BucketCacheProvider `yaml:"backend"`
 	BackendConfig interface{}         `yaml:"backend_config"`
 
-	CachingBucketConfig CachingBucketConfig `yaml:"caching_config"`
+	// Basic unit used to cache chunks.
+	ChunkSubrangeSize int64 `yaml:"chunk_subrange_size"`
+
+	// Maximum number of GetRange requests issued by this bucket for single GetRange call. Zero or negative value = unlimited.
+	MaxChunksGetRangeRequests int `yaml:"max_chunks_get_range_requests"`
+
+	// TTLs for various cache items.
+	ChunkObjectSizeTTL time.Duration `yaml:"chunk_object_size_ttl"`
+	ChunkSubrangeTTL   time.Duration `yaml:"chunk_subrange_ttl"`
+
+	// How long to cache result of Iter call in root directory.
+	BlocksIterTTL time.Duration `yaml:"blocks_iter_ttl"`
+
+	// Config for Exists and Get operations for metadata files.
+	MetafileExistsTTL      time.Duration `yaml:"metafile_exists_ttl"`
+	MetafileDoesntExistTTL time.Duration `yaml:"metafile_doesnt_exist_ttl"`
+	MetafileContentTTL     time.Duration `yaml:"metafile_content_ttl"`
+	MetafileMaxSize        model.Bytes   `yaml:"metafile_max_size"`
+}
+
+func (cfg *CachingWithBackendConfig) Defaults() {
+	cfg.ChunkSubrangeSize = 16000 // Equal to max chunk size.
+	cfg.ChunkObjectSizeTTL = 24 * time.Hour
+	cfg.ChunkSubrangeTTL = 24 * time.Hour
+	cfg.MaxChunksGetRangeRequests = 3
+	cfg.BlocksIterTTL = 5 * time.Minute
+	cfg.MetafileExistsTTL = 2 * time.Hour
+	cfg.MetafileDoesntExistTTL = 15 * time.Minute
+	cfg.MetafileContentTTL = 24 * time.Hour
+	cfg.MetafileMaxSize = 1024 * 1024 // Equal to default MaxItemSize in memcached client.
 }
 
 // NewCachingBucketFromYaml uses YAML configuration to create new caching bucket.
 func NewCachingBucketFromYaml(yamlContent []byte, bucket objstore.Bucket, logger log.Logger, reg prometheus.Registerer) (objstore.InstrumentedBucket, error) {
 	level.Info(logger).Log("msg", "loading caching bucket configuration")
 
-	config := &CachingBucketWithBackendConfig{}
-	config.CachingBucketConfig = DefaultCachingBucketConfig()
+	config := &CachingWithBackendConfig{}
+	config.Defaults()
 
 	if err := yaml.UnmarshalStrict(yamlContent, config); err != nil {
 		return nil, errors.Wrap(err, "parsing config YAML file")
@@ -60,5 +93,32 @@ func NewCachingBucketFromYaml(yamlContent []byte, bucket objstore.Bucket, logger
 		return nil, errors.Errorf("unsupported cache type: %s", config.Type)
 	}
 
-	return NewCachingBucket(bucket, c, config.CachingBucketConfig, logger, reg)
+	cfg := NewCachingBucketConfig()
+
+	// Configure cache.
+	cfg.CacheGetRange("chunks", c, isTSDBChunkFile, config.ChunkSubrangeSize, config.ChunkObjectSizeTTL, config.ChunkSubrangeTTL, config.MaxChunksGetRangeRequests)
+	cfg.CacheExists("meta.jsons", c, isMetaFile, config.MetafileExistsTTL, config.MetafileDoesntExistTTL)
+	cfg.CacheGet("meta.jsons", c, isMetaFile, int(config.MetafileMaxSize), config.MetafileContentTTL, config.MetafileExistsTTL, config.MetafileDoesntExistTTL)
+
+	// Cache Iter requests for root.
+	cfg.CacheIter("blocks-iter", c, isBlocksRootDir, config.BlocksIterTTL, JSONIterCodec{})
+
+	cb, err := NewCachingBucket(bucket, cfg, logger, reg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cb, nil
+}
+
+var chunksMatcher = regexp.MustCompile(`^.*/chunks/\d+$`)
+
+func isTSDBChunkFile(name string) bool { return chunksMatcher.MatchString(name) }
+
+func isMetaFile(name string) bool {
+	return strings.HasSuffix(name, "/"+metadata.MetaFilename) || strings.HasSuffix(name, "/"+metadata.DeletionMarkFilename)
+}
+
+func isBlocksRootDir(name string) bool {
+	return name == ""
 }
