ec5737ed261f29188ede179f9a2fbf4ec3fbc9d0
Bartlomiej Plotka <bwplotka@gmail.com>
2020-07-29 00:47:01 +0100

    Update2
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- tutorials/katacoda/thanos/8-prom-operator-demo/courseBase.sh
+++ tutorials/katacoda/thanos/8-prom-operator-demo/courseBase.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-# 2 node cluster with 1 master, 1 node. Based on Ubuntu.
-# Run launch.sh when the scenario starts to ensure cluster is running.
-./launch.sh
+mkdir /root/manifests
