6464d5281a427e43403f9d39c06fb2523e65959c
Kemal Akkoyun <kakkoyun@gmail.com>
2020-07-29 14:07:30 +0200

    katacode: mount everything
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- tutorials/katacoda/thanos/8-prom-operator-demo/courseBase.sh
+++ tutorials/katacoda/thanos/8-prom-operator-demo/courseBase.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p /root/manifests/crds
+mkdir -p /root/manifests/prometheus
+mkdir -p /root/manifests/operator
+mkdir -p /root/manifests/ruler
+mkdir -p /root/manifests/sidecar
+mkdir -p /root/manifests/query
--- tutorials/katacoda/thanos/9-prom-operator-demo-receive/courseBase.sh
+++ tutorials/katacoda/thanos/9-prom-operator-demo-receive/courseBase.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p /root/manifests/crds
+mkdir -p /root/manifests/prometheus
+mkdir -p /root/manifests/operator
 mkdir -p /root/manifests/controller
 mkdir -p /root/manifests/receive
+mkdir -p /root/manifests/query
