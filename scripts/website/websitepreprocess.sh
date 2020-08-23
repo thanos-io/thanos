f75edf4ccb2c6e53414cbe4fc5bdfa2978f061e2
thisisobate <obasiuche62@gmail.com>
2020-08-23 20:23:28 +0100

    cleaned up code
    Signed-off-by: thisisobate <obasiuche62@gmail.com>

--- scripts/website/websitepreprocess.sh
+++ scripts/website/websitepreprocess.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO(bwplotka): Take it from outside as param?
 # Regexp take from https://semver.org/
 # If we want to limit those we can sort, and have only head -n X of them etc
 RELEASE_FILTER_RE="release-(0|[1-9]\d*)\.(0|[1-9]\d*)$"
@@ -13,8 +12,7 @@ git remote add upstream https://github.com/thanos-io/thanos.git
 git remote add origin https://github.com/thanos-io/thanos.git
 git remote -v
 git fetch origin
-# TODO: Remove head -n 3 when ready for prod.
-# TODO: Here add logic what releases to filter (regexp) based on some parameter.
+
 RELEASE_BRANCHES=$(git branch --all | grep -P "remotes/origin/${RELEASE_FILTER_RE}" | egrep --invert-match '(:?HEAD|master)$' | sort -V)
 echo ">> chosen $(echo ${RELEASE_BRANCHES}) releases to deploy docs from"
 
@@ -23,12 +21,11 @@ mkdir -p "${OUTPUT_CONTENT_DIR}/tip"
 
 # Copy original content from current state first.
 cp -r ${ORIGINAL_CONTENT_DIR}/* "${OUTPUT_CONTENT_DIR}/tip"
-bash scripts/website/contentpreprocess.sh "${OUTPUT_CONTENT_DIR}/tip" 100000
+scripts/website/contentpreprocess.sh "${OUTPUT_CONTENT_DIR}/tip" 100000
 
 #create variable for weight value
 WEIGHT_VALUE=0
 
-# TODO: In future, fix older release that does not have font matter.
 for branchRef in ${RELEASE_BRANCHES}; do
   WEIGHT_VALUE=$((WEIGHT_VALUE + 1))
   branchName=${branchRef##*/}
@@ -36,7 +33,7 @@ for branchRef in ${RELEASE_BRANCHES}; do
   echo ">> cloning docs for versioning ${tags}"
   mkdir -p "${OUTPUT_CONTENT_DIR}/${tags}"
   git archive --format=tar "refs/${branchRef}" | tar -C${OUTPUT_CONTENT_DIR}/${tags} -x "docs/" --strip-components=1
-  bash scripts/website/contentpreprocess.sh "${OUTPUT_CONTENT_DIR}/${tags}" ${WEIGHT_VALUE}
+  scripts/website/contentpreprocess.sh "${OUTPUT_CONTENT_DIR}/${tags}" ${WEIGHT_VALUE}
 done
 
 # Find and remove _index.md from components, proposal, and operating dirs inorder to render docs content
@@ -45,6 +42,3 @@ for f in $FILES; do
   # take action on each file. $f store current file name
   find $FILES -mindepth 2 -name "_index.md" -delete
 done
-
-# TODO: Open problems to solve:
-# * We can first ensure that public contains the layout we want, then we can adjust html accordingly.
