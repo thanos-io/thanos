e56641cef4d2ed953d6329f268a40c88eebec48b
aribalam <arib.alam.iitkgp@gmail.com>
2020-10-01 00:05:14 +0530

    display data from api response
    Signed-off-by: aribalam <arib.alam.iitkgp@gmail.com>

--- pkg/ui/react-app/src/pages/configfiles/ConfigFiles.tsx
+++ pkg/ui/react-app/src/pages/configfiles/ConfigFiles.tsx
@@ -2,16 +2,39 @@ import { RouteComponentProps } from "@reach/router";
 import React, { FC } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import PathPrefixProps from "../../types/PathPrefixProps";
+import { Table } from 'reactstrap';
+import { withStatusIndicator } from "../../components/withStatusIndicator";
 
 interface ConfigFilesMap {
     [key: string]: string;
 }
 
+interface ConfigFileProps {
+    data?: ConfigFilesMap
+}
+
+const ConfigFilesContent: FC<ConfigFileProps> = ({ data = {} }) => {
+    return (
+      <>
+      <h2>Configuration Files</h2>
+      <Table bordered striped>
+        <tbody>
+          {Object.keys(data).map(key => (
+            <tr key={key}>
+              <th>{key}</th>
+              <td>{data[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+    )
+} 
+const ConfigFilesWithStatusIndicator = withStatusIndicator(ConfigFilesContent);
+
 const ConfigFiles: FC<RouteComponentProps & PathPrefixProps> = ({ pathPrefix = '' }) => {
     const { response, error, isLoading } = useFetch<ConfigFilesMap>(`${pathPrefix}/api/v1/status/configfiles`);
-    return (
-        <h1>This is the main Configuration Page</h1>
-    );
+    return <ConfigFilesWithStatusIndicator data={response.data} error={error} isLoading={isLoading} />
 }
 
 export default ConfigFiles;
