b231b745f1c9b7130243eec915ac8b970a8ae95e
aribalam <arib.alam.iitkgp@gmail.com>
2020-10-01 00:28:17 +0530

    check for empty config files and remove lint errors
    Signed-off-by: aribalam <arib.alam.iitkgp@gmail.com>

--- pkg/ui/react-app/src/pages/configfiles/ConfigFiles.tsx
+++ pkg/ui/react-app/src/pages/configfiles/ConfigFiles.tsx
@@ -1,25 +1,26 @@
-import { RouteComponentProps } from "@reach/router";
-import React, { FC } from "react";
-import { useFetch } from "../../hooks/useFetch";
-import PathPrefixProps from "../../types/PathPrefixProps";
+import { RouteComponentProps } from '@reach/router';
+import React, { FC } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import PathPrefixProps from '../../types/PathPrefixProps';
 import { Table } from 'reactstrap';
-import { withStatusIndicator } from "../../components/withStatusIndicator";
+import { withStatusIndicator } from '../../components/withStatusIndicator';
 
 interface ConfigFilesMap {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 interface ConfigFileProps {
-    data?: ConfigFilesMap
+  data?: ConfigFilesMap;
 }
 
 const ConfigFilesContent: FC<ConfigFileProps> = ({ data = {} }) => {
-    return (
-      <>
+  const keys = Object.keys(data);
+  return keys.length > 0 ? (
+    <>
       <h2>Configuration Files</h2>
       <Table bordered striped>
         <tbody>
-          {Object.keys(data).map(key => (
+          {keys.map(key => (
             <tr key={key}>
               <th>{key}</th>
               <td className="config-file-cell">{data[key]}</td>
@@ -28,13 +29,15 @@ const ConfigFilesContent: FC<ConfigFileProps> = ({ data = {} }) => {
         </tbody>
       </Table>
     </>
-    )
-} 
+  ) : (
+    <h2> No Configuration files found </h2>
+  );
+};
 const ConfigFilesWithStatusIndicator = withStatusIndicator(ConfigFilesContent);
 
 const ConfigFiles: FC<RouteComponentProps & PathPrefixProps> = ({ pathPrefix = '' }) => {
-    const { response, error, isLoading } = useFetch<ConfigFilesMap>(`${pathPrefix}/api/v1/status/configfiles`);
-    return <ConfigFilesWithStatusIndicator data={response.data} error={error} isLoading={isLoading} />
-}
+  const { response, error, isLoading } = useFetch<ConfigFilesMap>(`${pathPrefix}/api/v1/status/configfiles`);
+  return <ConfigFilesWithStatusIndicator data={response.data} error={error} isLoading={isLoading} />;
+};
 
 export default ConfigFiles;
