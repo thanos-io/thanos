cd07284cbeb1cafa43660e4687e4882a9db67993
Jarod Watkins <jarod@42lines.net>
2020-10-30 15:11:46 -0400

    UI: Add toggle to enable/disable metric autocomplete (#3381)
    * UI: Add toggle to enable/disable metric autocomplete
    
    This change adds a toggle to enable or disable the metric autocomplete
    functionality. By default it is enabled.
    
    Signed-off-by: Jarod Watkins <jarod@42lines.net>
    
    * Fixing tests
    
    Signed-off-by: Jarod Watkins <jarod@42lines.net>
    
    * Fixing lint suggestions
    
    Signed-off-by: Jarod Watkins <jarod@42lines.net>
    
    * Run make assets
    
    Signed-off-by: Jarod Watkins <jarod@42lines.net>
    Signed-off-by: Oghenebrume50 <raphlbrume@gmail.com>

--- pkg/ui/react-app/src/pages/graph/Panel.tsx
+++ pkg/ui/react-app/src/pages/graph/Panel.tsx
@@ -26,6 +26,7 @@ interface PanelProps {
   removePanel: () => void;
   onExecuteQuery: (query: string) => void;
   stores: Store[];
+  enableMetricAutocomplete: boolean;
 }
 
 interface PanelState {
@@ -283,6 +284,7 @@ class Panel extends Component<PanelProps & PathPrefixProps, PanelState> {
               onExpressionChange={this.handleExpressionChange}
               executeQuery={this.executeQuery}
               loading={this.state.loading}
+              enable={this.props.enableMetricAutocomplete}
               autocompleteSections={{
                 'Query History': pastQueries,
                 'Metric Names': metricNames,
