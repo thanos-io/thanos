ae23c586bbbd7bba9c26602abb49795b3f780692
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2020-11-03 23:52:21 +0100

    REPLICATOR: Add TimePartitionMetaFilter to thanos replicate. (#2979)
    * Add TimePartitionMetaFilter to thanos replicate.
    
    This will allow time based replication.
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Update docs and changelog.
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Fix lint
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Add posibility to delete blocks older than max time
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Fix metric naming.
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Fix order of arguments for max and min time.
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Remove deletion of blocks from replicator.
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Fix import formatting
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Revert unneeded changes due to replicator no longer deleting blocs
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    
    * Fix typo
    
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
    Signed-off-by: Oghenebrume50 <raphlbrume@gmail.com>

--- cmd/thanos/tools_bucket.go
+++ cmd/thanos/tools_bucket.go
@@ -36,6 +36,7 @@ import (
 	"github.com/thanos-io/thanos/pkg/extprom"
 	extpromhttp "github.com/thanos-io/thanos/pkg/extprom/http"
 	"github.com/thanos-io/thanos/pkg/logging"
+	"github.com/thanos-io/thanos/pkg/model"
 	"github.com/thanos-io/thanos/pkg/objstore"
 	"github.com/thanos-io/thanos/pkg/objstore/client"
 	"github.com/thanos-io/thanos/pkg/prober"
@@ -444,6 +445,10 @@ func registerBucketReplicate(app extkingpin.AppClause, objStoreConfig *extflag.P
 	compactions := cmd.Flag("compaction", "Only blocks with these compaction levels will be replicated. Repeated flag.").Default("1", "2", "3", "4").Ints()
 	matcherStrs := cmd.Flag("matcher", "Only blocks whose external labels exactly match this matcher will be replicated.").PlaceHolder("key=\"value\"").Strings()
 	singleRun := cmd.Flag("single-run", "Run replication only one time, then exit.").Default("false").Bool()
+	minTime := model.TimeOrDuration(cmd.Flag("min-time", "Start of time range limit to replicate. Thanos Replicate will replicate only metrics, which happened later than this value. Option can be a constant time in RFC3339 format or time duration relative to current time, such as -1d or 2h45m. Valid duration units are ms, s, m, h, d, w, y.").
+		Default("0000-01-01T00:00:00Z"))
+	maxTime := model.TimeOrDuration(cmd.Flag("max-time", "End of time range limit to replicate. Thanos Replicate will replicate only metrics, which happened earlier than this value. Option can be a constant time in RFC3339 format or time duration relative to current time, such as -1d or 2h45m. Valid duration units are ms, s, m, h, d, w, y.").
+		Default("9999-12-31T23:59:59Z"))
 
 	cmd.Setup(func(g *run.Group, logger log.Logger, reg *prometheus.Registry, tracer opentracing.Tracer, _ <-chan struct{}, _ bool) error {
 		matchers, err := replicate.ParseFlagMatchers(*matcherStrs)
@@ -469,6 +474,8 @@ func registerBucketReplicate(app extkingpin.AppClause, objStoreConfig *extflag.P
 			objStoreConfig,
 			toObjStoreConfig,
 			*singleRun,
+			minTime,
+			maxTime,
 		)
 	})
 }
