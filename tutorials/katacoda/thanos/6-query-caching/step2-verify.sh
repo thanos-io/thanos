4310a5124d7366f813407114e02768b7ec515293
Kemal Akkoyun <kakkoyun@gmail.com>
2020-11-19 11:23:21 +0100

    Removed unused files
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- tutorials/katacoda/thanos/6-query-caching/step2-verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -s 127.0.0.1:29090/metrics >/dev/null || exit 1
-
-curl -s 127.0.0.1:39091 >/dev/null || exit 1
-curl -s 127.0.0.1:39092 >/dev/null || exit 1
-
-echo '"done"'
