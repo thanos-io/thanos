thanos-io/thanos

commit c21337e5c9e73f597345e54ed3bc3bfe04bc4cb8

cmd/thanos/query.go: Timeout DNS resolution with refresh interval (#3508)
While the context is passed to DNS resolver, no timeout is ever set,
which leaves us in an unpredictable state, as we wait for the resolution
indefinitely. Every interval we repeat the DNS resolution, but don't ever
cancel previous attempts even if they have not finished.

Signed-off-by: Lili Cosic <cosiclili@gmail.com>

cmd/thanos/query.go
@@ -391,6 +391,8 @@ func runQuery(
 		ctx, cancel := context.WithCancel(context.Background())
 		g.Add(func() error {
 			return runutil.Repeat(dnsSDInterval, ctx.Done(), func() error {
+				ctx, cancel = context.WithTimeout(ctx, dnsSDInterval)
+				defer cancel()
 				if err := dnsStoreProvider.Resolve(ctx, append(fileSDCache.Addresses(), storeAddrs...)); err != nil {
 					level.Error(logger).Log("msg", "failed to resolve addresses for storeAPIs", "err", err)
 				}

commit 063b94611b4bde27d5b3722542f3ac61fe577d0c

cmd/thanos: fix DNS resolution when ctx is canceled
c21337e5 introduced a subtle bug: when the DNS resolution context
expires, all subsequent resolutions will fail with 'operation was
canceled' because the function overwrites the global context instead of
creating child context. To make it more obvious, we use different
variable names for the parent and child contexts.

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

cmd/thanos/query.go
@@ -391,12 +391,12 @@ func runQuery(
 		ctx, cancel := context.WithCancel(context.Background())
 		g.Add(func() error {
 			return runutil.Repeat(dnsSDInterval, ctx.Done(), func() error {
-				ctx, cancel = context.WithTimeout(ctx, dnsSDInterval)
-				defer cancel()
-				if err := dnsStoreProvider.Resolve(ctx, append(fileSDCache.Addresses(), storeAddrs...)); err != nil {
+				resolveCtx, resolveCancel := context.WithTimeout(ctx, dnsSDInterval)
+				defer resolveCancel()
+				if err := dnsStoreProvider.Resolve(resolveCtx, append(fileSDCache.Addresses(), storeAddrs...)); err != nil {
 					level.Error(logger).Log("msg", "failed to resolve addresses for storeAPIs", "err", err)
 				}
-				if err := dnsRuleProvider.Resolve(ctx, ruleAddrs); err != nil {
+				if err := dnsRuleProvider.Resolve(resolveCtx, ruleAddrs); err != nil {
 					level.Error(logger).Log("msg", "failed to resolve addresses for rulesAPIs", "err", err)
 				}
 				return nil