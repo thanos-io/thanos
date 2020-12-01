0feea941df398f2d7bc645c0a01af381559de0d6
Bartlomiej Plotka <bwplotka@gmail.com>
2020-12-01 21:31:02 +0100

    Added better tracking.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- pkg/isolation/usagepb/alloc/alloc.go
+++ pkg/isolation/usagepb/alloc/alloc.go
@@ -6,7 +6,6 @@ import (
 	"sync"
 
 	"github.com/pkg/errors"
-	"github.com/thanos-io/thanos/pkg/errutil"
 )
 
 type Tracker interface {
@@ -38,69 +37,76 @@ func (t *tracker) MemoryBytesAllocated(b int) {
 	t.allocatedBytes[c] += uint64(b)
 }
 
+type ValidationFailedError struct {
+	Problems []Problem
+}
+
+func (e ValidationFailedError) Error() string {
+	ret := "alloc: Validation did not passed, found:\n"
+	for _, p := range e.Problems {
+		ret += p.Error()
+	}
+	return ret
+}
+
+type Problem struct {
+	TrackedBytes uint64
+	Allocated    MemProfileFramedRecords
+	File         string
+	Line         int
+}
+
+func (p Problem) Error() string {
+	prefix := ""
+	if p.File != "" {
+		prefix = fmt.Sprintf("%v:%v ", p.File, p.Line)
+	}
+	if p.Allocated.AllocatedBytes == 0 {
+		return fmt.Sprintf("%sTracked %v bytes, but nothing was actually allocated in near location\n", prefix, p.TrackedBytes)
+	}
+
+	return fmt.Sprintf("%sTracked %v bytes, but allocated nearby %v\n", prefix, p.TrackedBytes, p.Allocated.String())
+}
+
 // ValidateTracking...
 // Not go-routine safe due to global memory profiling.
 func ValidateTracking(skip int, toleranceBytes uint64, f func(tr Tracker)) (err error) {
-	// Setting 1 to include every allocated block in the profile. (512 * 1024 by default).
-	oldProfileRate := runtime.MemProfileRate
-	runtime.MemProfileRate = 1
-	defer func() { runtime.MemProfileRate = oldProfileRate }()
-
 	var (
-		tracked  = &tracker{allocatedBytes: map[codeRef]uint64{}}
-		callerPC uintptr
-		ok       bool
+		tracked         = &tracker{allocatedBytes: map[codeRef]uint64{}}
+		foundAllocs     = make(map[codeRef]MemProfileFramedRecords, len(tracked.allocatedBytes))
+		problems        []Problem
+		fr              runtime.Frame
+		untrackedAllocs []memProfileFramedRecord
+		toleratedBytes  uint64
 	)
 
-	// Anonymous function to get scoped PC.
-	// NOTE: PC means program [stack] counter.
-	func() {
-		callerPC, _, _, ok = runtime.Caller(1)
-		tracked.MemoryBytesAllocated(208)
-		if !ok {
-			err = errors.New("failed to get caller info")
-			return
-		}
-		f(tracked)
-	}()
+	records, err := recordMemoryProfiles(skip, func() { f(tracked) })
 	if err != nil {
 		return err
 	}
-
-	var (
-		foundAllocs     = make(map[codeRef]memProfileFramedRecords, len(tracked.allocatedBytes))
-		errs            errutil.MultiError
-		fr              runtime.Frame
-		untrackedAllocs []memProfileFramedRecord
-	)
-
 RecordsLoop:
-	for _, r := range getRelatedMemRecords(callerPC) {
+	for _, r := range records {
 		frames := runtime.CallersFrames(r.relevantPCs)
-		var (
-			frs []runtime.Frame
-			ok  = true
-		)
+
+		ok := true
+		framed := memProfileFramedRecord{
+			MemProfileRecord: r.MemProfileRecord,
+		}
 		for ok {
 			fr, ok = frames.Next()
 			if _, isTrackerInvocation := tracked.allocatedBytes[codeRef{file: fr.File, line: fr.Line}]; isTrackerInvocation {
 				// Ignore tracker allocated bytes.
 				continue RecordsLoop
 			}
-			frs = append(frs, fr)
+			framed.frames = append(framed.frames, fr)
 		}
-		framed := memProfileFramedRecord{
-			MemProfileRecord: r.MemProfileRecord,
-			// -1 so we ignore f call.
-			frames: frs[:len(frs)-skip-1],
-		}
-		for _, fr := range frs {
+		for _, fr := range framed.frames {
 			// Hacky: Assume for now tracker is right after allocation.
 			fcodeRef := codeRef{file: fr.File, line: fr.Line + 1}
 			_, ok = tracked.allocatedBytes[fcodeRef]
 			if ok {
 				found := foundAllocs[fcodeRef]
-				found.allocatedBytes += uint64(r.AllocBytes)
+				found.AllocatedBytes += uint64(r.AllocBytes)
 				found.records = append(found.records, framed)
 				foundAllocs[fcodeRef] = found
 				break
@@ -113,40 +119,44 @@ RecordsLoop:
 
 	for cr, b := range tracked.allocatedBytes {
 		if found, ok := foundAllocs[cr]; ok {
-			if b+toleranceBytes >= found.allocatedBytes && b-toleranceBytes <= found.allocatedBytes {
+			if b >= found.AllocatedBytes && b-found.AllocatedBytes <= toleranceBytes {
+				continue
+			} else if found.AllocatedBytes-b <= toleranceBytes {
 				continue
 			}
-			errs = append(errs, errors.Errorf("%v:%v Tracked %v Bytes, but actually nearby allocated %v\n", cr.file, cr.line, b, found.String()))
+			problems = append(problems, Problem{TrackedBytes: b, File: cr.file, Line: cr.line, Allocated: found})
 			continue
 		}
-		errs = append(errs, errors.Errorf("%v:%v Tracked %v Bytes, but noting was actually allocated in near location\n", cr.file, cr.line, b))
+		problems = append(problems, Problem{TrackedBytes: b, File: cr.file, Line: cr.line})
 	}
 
-	var toleratedBytes uint64
-	var toleratedErrs errutil.MultiError
+	var toleratedAllocs []memProfileFramedRecord
 	for _, u := range untrackedAllocs {
-		err := errors.Errorf("Untracked allocations of %v\n", u.String())
 		if uint64(u.AllocBytes) <= toleranceBytes {
 			toleratedBytes += uint64(u.AllocBytes)
-			toleratedErrs = append(toleratedErrs, err)
+			toleratedAllocs = append(toleratedAllocs, u)
 			continue
 		}
-		errs = append(errs, err)
+		problems = append(problems, Problem{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: uint64(u.AllocBytes), records: []memProfileFramedRecord{u}}})
 	}
 
 	if toleratedBytes > toleranceBytes {
-		errs = append(errs, errors.Wrapf(toleratedErrs.Err(), "Untracked tiny allocations accumulated above tolerance: %v\n", toleratedBytes))
+		// Untracked tiny allocations accumulated above tolerance.
+		problems = append(problems, Problem{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: toleratedBytes, records: untrackedAllocs}})
 	}
-	return errs.Err()
+	if len(problems) > 0 {
+		return ValidationFailedError{Problems: problems}
+	}
+	return nil
 }
 
-type memProfileFramedRecords struct {
-	allocatedBytes uint64
+type MemProfileFramedRecords struct {
+	AllocatedBytes uint64
 	records        []memProfileFramedRecord
 }
 
-func (r memProfileFramedRecords) String() string {
-	msg := fmt.Sprintf("%v Details:\n", r.allocatedBytes)
+func (r MemProfileFramedRecords) String() string {
+	msg := fmt.Sprintf("%v Details:\n", r.AllocatedBytes)
 	for _, rec := range r.records {
 		msg += "  " + rec.String()
 	}
@@ -171,30 +181,61 @@ type memProfileRecord struct {
 	relevantPCs []uintptr
 }
 
-func getRelatedMemRecords(basePC uintptr) (relatedRecords []memProfileRecord) {
-	records := make([]runtime.MemProfileRecord, 100)
-	for {
-		l, ok := runtime.MemProfile(records, true)
-		if ok {
-			break
+func recordMemoryProfiles(skip int, fn func()) (ret []memProfileRecord, err error) {
+	// Setting 1 to include every allocated block in the profile. (512 * 1024 by default).
+	oldProfileRate := runtime.MemProfileRate
+	runtime.MemProfileRate = 1
+	defer func() { runtime.MemProfileRate = oldProfileRate }()
+
+	var (
+		// NOTE: PC means program [stack] counter.
+		funcPC, beforePC, sincePC uintptr
+		before, since             []runtime.MemProfileRecord
+		ok                        bool
+	)
+
+	// Anonymous function to get scoped PC.
+	func() {
+		funcPC, _, _, ok = runtime.Caller(1)
+		if !ok {
+			err = errors.New("failed to get caller info")
+			return
 		}
-		records = make([]runtime.MemProfileRecord, l)
+
+		beforePC, before, err = getMemProfile()
+		if err != nil {
+			return
+		}
+		// Run requested workload.
+		fn()
+		sincePC, since, err = memProfileSince(before)
+	}()
+	if err != nil {
+		return nil, err
 	}
 
-	for _, r := range records {
+	for _, r := range since {
 		if r.AllocBytes == 0 && r.AllocObjects == 0 {
 			break
 		}
-
 		s := r.Stack()
 
 		// Get records only downstream of this PC.
 		for i, pc := range s {
-			if pc == basePC+1 {
+			// PCs to ignore should be at the higher of stack vs function PC (they are deeper).
+			if pc == beforePC+1 || pc == sincePC+1 {
+				break
+			}
+
+			if pc == funcPC+1 {
+				if i-skip <= 0 {
+					break
+				}
+
 				// Copy as runtime reuse all arrays.
-				relevantPCs := make([]uintptr, i)
-				copy(relevantPCs, s[:i])
-				relatedRecords = append(relatedRecords, memProfileRecord{
+				relevantPCs := make([]uintptr, i-skip)
+				copy(relevantPCs, s[:i-skip])
+				ret = append(ret, memProfileRecord{
 					MemProfileRecord: r,
 					relevantPCs:      relevantPCs,
 				})
@@ -202,5 +243,65 @@ func getRelatedMemRecords(basePC uintptr) (relatedRecords []memProfileRecord) {
 			}
 		}
 	}
-	return relatedRecords
+	return ret, nil
+
+}
+
+func getMemProfile() (uintptr, []runtime.MemProfileRecord, error) {
+	myPC, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return 0, nil, errors.New("failed to get caller info")
+	}
+	// The returned profile may be up to two garbage collection cycles old, that's why make sure to get latest.
+	runtime.GC()
+	runtime.GC()
+	runtime.GC()
+
+	// Find out how many records there are (MemProfile(nil, true)),
+	// allocate that many records, and get the data.
+	// There's a race—more records might be added between
+	// the two calls—so allocate a few extra records for safety
+	// and also try again if we're very unlucky.
+	// The loop should only execute one iteration in the common case.
+	var p []runtime.MemProfileRecord
+	n, ok := runtime.MemProfile(nil, true)
+	for {
+		// Allocate room for a slightly bigger profile,
+		// in case a few more entries have been added
+		// since the call to MemProfile.
+		p = make([]runtime.MemProfileRecord, n+50)
+		n, ok = runtime.MemProfile(p, true)
+		if ok {
+			p = p[0:n]
+			break
+		}
+		// Profile grew; try again.
+	}
+	return myPC, p, nil
+}
+
+func memProfileSince(since []runtime.MemProfileRecord) (uintptr, []runtime.MemProfileRecord, error) {
+	myPC, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return 0, nil, errors.New("failed to get caller info")
+	}
+
+	_, now, err := getMemProfile()
+	if err != nil {
+		return 0, nil, err
+	}
+	var ret []runtime.MemProfileRecord
+	for i, _ := range now {
+		for _, sinceRecord := range since {
+			if now[i].Stack0 == sinceRecord.Stack0 {
+				now[i].AllocObjects -= sinceRecord.AllocObjects
+				now[i].AllocBytes -= sinceRecord.AllocBytes
+				break
+			}
+		}
+		if now[i].AllocBytes > 0 {
+			ret = append(ret, now[i])
+		}
+	}
+	return myPC, ret, nil
 }
--- pkg/isolation/usagepb/alloc/alloc_test.go
+++ pkg/isolation/usagepb/alloc/alloc_test.go
@@ -1,13 +1,38 @@
-package alloc_test
+package alloc
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strings"
 	"testing"
 
-	"github.com/thanos-io/thanos/pkg/isolation/usagepb/alloc"
 	"github.com/thanos-io/thanos/pkg/testutil"
 )
+
+func TestMemProfileSince(t *testing.T) {
+	for i := 0; i < 2; i++ {
+		// Run at least twice so we can ensure determinism.
+		t.Run("", func(t *testing.T) {
+			r, err := recordMemoryProfiles(0, func() {})
+			testutil.Ok(t, err)
+			testutil.Equals(t, []memProfileRecord(nil), r)
+
+			var x1 []string
+			r, err = recordMemoryProfiles(0, func() {
+				x1 = make([]string, 100)
+			})
+			testutil.Ok(t, err)
+			testutil.Equals(t, 1, len(r))
+			testutil.Equals(t, int64(1), r[0].AllocObjects)
+			testutil.Equals(t, int64(1792), r[0].AllocBytes)
+
+			// Use fprint to ensure compiler does not optimize above allocs away.
+			fmt.Fprint(ioutil.Discard, x1)
+		})
+	}
+}
 
 type testStruct struct {
 	yolo      int64
@@ -26,7 +51,7 @@ func testWithAllocations_NoTracking() {
 
 }
 
-func testWithAllocations_InaccurateTracking(tracker alloc.Tracker) {
+func testWithAllocations_InaccurateTracking(tracker Tracker) {
 	x1 := make([]string, 100)
 	tracker.MemoryBytesAllocated(1792)
 
@@ -34,89 +59,141 @@ func testWithAllocations_InaccurateTracking(tracker alloc.Tracker) {
 	tracker.MemoryBytesAllocated(8) // Wrong, this is on stack.
 
 	x3 := &testStruct{}
-	tracker.MemoryBytesAllocated(1214)
+	tracker.MemoryBytesAllocated(32)
 
 	// Use fprint to ensure compiler does not optimize those allocs away.
 	fmt.Fprint(ioutil.Discard, x1, x2, x3)
-	tracker.MemoryBytesAllocated(32 + 128 + 1600 + 32)
+	tracker.MemoryBytesAllocated(3672)
 }
 
-func testWithAllocations_InaccurateTracking2(tracker alloc.Tracker) {
+func testWithAllocations_InaccurateTracking2(tracker Tracker) {
 	x1 := make([]string, 100)
-	tracker.MemoryBytesAllocated(1791) // Wrong.
+	tracker.MemoryBytesAllocated(1891) // Wrong of 99.
 
 	x2 := uint64(1244)
 
 	x3 := &testStruct{}
-	tracker.MemoryBytesAllocated(1214)
+	tracker.MemoryBytesAllocated(32)
 
 	// Use fprint to ensure compiler does not optimize those allocs away.
 	fmt.Fprint(ioutil.Discard, x1, x2, x3)
-	tracker.MemoryBytesAllocated(32 + 128 + 1600 + 32)
+	tracker.MemoryBytesAllocated(3672)
 }
 
-func testWithAllocations_AccurateTracking(tracker alloc.Tracker) {
+func testWithAllocations_AccurateTracking(tracker Tracker) {
 	x1 := make([]string, 100)
 	tracker.MemoryBytesAllocated(1792)
 
 	x2 := uint64(1244)
 
 	x3 := &testStruct{}
-	tracker.MemoryBytesAllocated(1214)
+	tracker.MemoryBytesAllocated(32)
 
 	// Use fprint to ensure compiler does not optimize those allocs away.
 	fmt.Fprint(ioutil.Discard, x1, x2, x3)
-	tracker.MemoryBytesAllocated(32 + 128 + 1600 + 32)
+	tracker.MemoryBytesAllocated(3672)
 }
 
 func TestResourceTracker_MemoryBytesAllocated(t *testing.T) {
-	t.Run("no tracking", func(t *testing.T) {
-		err := alloc.ValidateTracking(1, 125, func(_ alloc.Tracker) {
-			testWithAllocations_NoTracking()
+	for i := 0; i < 2; i++ {
+		// Run at least twice so we can ensure determinism.
+		// This is especially relevant for checking same functions couple of times. Frame allocations are shared between
+		// traces/stacks (:
+		t.Run("", func(t *testing.T) {
+			t.Run("no tracking, no allocations", func(t *testing.T) {
+				testutil.Ok(t, ValidateTracking(0, 0, func(_ Tracker) {}))
+			})
+			t.Run("something tracked, but no allocations", func(t *testing.T) {
+				shallowCompare(t, []Problem{
+					{TrackedBytes: 124, File: "pkg/isolation/usagepb/alloc/alloc_test.go", Line: 108},
+				}, ValidateTracking(0, 0, func(tr Tracker) {
+					tr.MemoryBytesAllocated(124)
+				}))
+			})
+			t.Run("something tracked, but no allocations, with tolerance", func(t *testing.T) {
+				shallowCompare(t, []Problem{
+					{TrackedBytes: 124, File: "pkg/isolation/usagepb/alloc/alloc_test.go", Line: 115},
+				}, ValidateTracking(0, 200, func(tr Tracker) {
+					tr.MemoryBytesAllocated(124)
+				}))
+			})
+			t.Run("no tracking, with allocations", func(t *testing.T) {
+				var x1 []byte
+				shallowCompare(t, []Problem{
+					{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 112}},
+				}, ValidateTracking(0, 0, func(tr Tracker) {
+					x1 = make([]byte, 100)
+				}))
+				// Use fprint to ensure compiler does not optimize above allocs away.
+				fmt.Fprint(ioutil.Discard, x1)
+			})
+			t.Run("no tracking, with allocations and tolerance", func(t *testing.T) {
+				var x1 []byte
+				testutil.Ok(t, ValidateTracking(0, 113, func(tr Tracker) {
+					x1 = make([]byte, 100)
+				}))
+				// Use fprint to ensure compiler does not optimize above allocs away.
+				fmt.Fprint(ioutil.Discard, x1)
+			})
+			t.Run("inside function; tolerance 100", func(t *testing.T) {
+				t.Run("no tracking, with allocations", func(t *testing.T) {
+					shallowCompare(t, []Problem{
+						{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 232}},
+						{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 208}},
+						{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 1792}},
+						{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 1600}},
+						{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 1536}},
+						{TrackedBytes: 0, Allocated: MemProfileFramedRecords{AllocatedBytes: 128}},
+					}, ValidateTracking(1, 100, func(tr Tracker) {
+						testWithAllocations_NoTracking()
+					}))
+				})
+				t.Run("inaccurate tracking", func(t *testing.T) {
+					shallowCompare(t, []Problem{
+						{TrackedBytes: 8, File: "pkg/isolation/usagepb/alloc/alloc_test.go", Line: 59},
+					}, ValidateTracking(1, 100, func(tr Tracker) {
+						testWithAllocations_InaccurateTracking(tr)
+					}))
+				})
+				t.Run("inaccurate tracking 2; lower tolerance", func(t *testing.T) {
+					shallowCompare(t, []Problem{
+						{TrackedBytes: 1891, Allocated: MemProfileFramedRecords{AllocatedBytes: 1792}, File: "pkg/isolation/usagepb/alloc/alloc_test.go", Line: 71},
+					}, ValidateTracking(1, 98, func(tr Tracker) {
+						testWithAllocations_InaccurateTracking2(tr)
+					}))
+				})
+				t.Run("inaccurate tracking 2", func(t *testing.T) {
+					testutil.Ok(t, ValidateTracking(1, 100, func(tr Tracker) {
+						testWithAllocations_InaccurateTracking2(tr)
+					}))
+				})
+				t.Run("accurate tracking", func(t *testing.T) {
+					testutil.Ok(t, ValidateTracking(1, 100, func(tr Tracker) {
+						testWithAllocations_AccurateTracking(tr)
+					}))
+				})
+			})
 		})
-		testutil.NotOk(t, err)
-		testutil.Equals(t, `3 errors: Untracked allocations of 128 Bytes for: 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:86 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:867 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:716 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:1161 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:232 
-    /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:25 
+	}
+}
 
-; Untracked allocations of 1600 Bytes for: 
-    /home/bwplotka/.gvm/gos/go1.15/src/reflect/value.go:119 
-    /home/bwplotka/.gvm/gos/go1.15/src/reflect/value.go:1045 
-    /home/bwplotka/.gvm/gos/go1.15/src/reflect/value.go:1015 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:726 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:869 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:716 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:1161 
-    /home/bwplotka/.gvm/gos/go1.15/src/fmt/print.go:232 
-    /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:25 
+func shallowCompare(t *testing.T, expected []Problem, err error) {
+	t.Helper()
 
-; Untracked allocations of 1792 Bytes for: 
-    /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:19 
+	testutil.NotOk(t, err)
+	v := ValidationFailedError{}
+	testutil.Assert(t, errors.As(err, &v))
+	sort.Slice(v.Problems, func(i, j int) bool {
+		return strings.Compare(v.Problems[i].Error(), v.Problems[j].Error()) > 0
+	})
 
-`, err.Error())
-
-	})
-	t.Run("inaccurate tracking", func(t *testing.T) {
-		err := alloc.ValidateTracking(1, 125, func(tr alloc.Tracker) {
-			testWithAllocations_InaccurateTracking(tr)
-		})
-		testutil.NotOk(t, err)
-		testutil.Equals(t, `5 errors: /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc.go:59 Tracked 208 Bytes, but noting was actually allocated in near location; /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:31 Tracked 1792 Bytes, but noting was actually allocated in near location; /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:34 Tracked 8 Bytes, but noting was actually allocated in near location; /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:37 Tracked 1214 Bytes, but noting was actually allocated in near location; /home/bwplotka/Repos/thanos/pkg/isolation/usagepb/alloc/alloc_test.go:41 Tracked 1792 Bytes, but noting was actually allocated in near location"`, err.Error())
-	})
-	t.Run("inaccurate tracking 2", func(t *testing.T) {
-		err := alloc.ValidateTracking(1, 125, func(tr alloc.Tracker) {
-			testWithAllocations_InaccurateTracking2(tr)
-		})
-		testutil.NotOk(t, err)
-		testutil.Equals(t, `6 errors: Untracked allocations of 32 Bytes for: `, err.Error())
-	})
-	t.Run("accurate tracking", func(t *testing.T) {
-		testutil.Ok(t, alloc.ValidateTracking(1, 125, func(tr alloc.Tracker) {
-			testWithAllocations_AccurateTracking(tr)
-		}))
-	})
+	for i := range v.Problems {
+		// Don't compare frames, they are likely different.
+		v.Problems[i].Allocated.records = nil
+		// Trim as everyone has this code in different place.
+		if v.Problems[i].File != "" {
+			v.Problems[i].File = v.Problems[i].File[strings.Index(v.Problems[i].File, "pkg/isolation/"):]
+		}
+	}
+	testutil.Equals(t, expected, v.Problems)
 }
