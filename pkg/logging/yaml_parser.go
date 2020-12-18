276f6747427a86dd6a0a8997765cd4a79bbce531
Yash Sharma <yashrsharma44@gmail.com>
2020-12-18 13:16:24 +0530

    break down the yaml struct for grpc and http into its individual configs
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- pkg/logging/yaml_parser.go
+++ pkg/logging/yaml_parser.go
@@ -4,14 +4,19 @@
 package logging
 
 type ReqLogConfig struct {
-	HTTP    ProtocolConfigs `yaml:"http"`
-	GRPC    ProtocolConfigs `yaml:"grpc"`
-	Options OptionsConfig   `yaml:"options"`
+	HTTP    HTTPProtocolConfigs `yaml:"http"`
+	GRPC    GRPCProtocolConfigs `yaml:"grpc"`
+	Options OptionsConfig       `yaml:"options"`
 }
 
-type ProtocolConfigs struct {
-	Options OptionsConfig    `yaml:"options"`
-	Config  []ProtocolConfig `yaml:"config"`
+type HTTPProtocolConfigs struct {
+	Options OptionsConfig        `yaml:"options"`
+	Config  []HTTPProtocolConfig `yaml:"config"`
+}
+
+type GRPCProtocolConfigs struct {
+	Options OptionsConfig        `yaml:"options"`
+	Config  []GRPCProtocolConfig `yaml:"config"`
 }
 
 type OptionsConfig struct {
@@ -24,13 +29,12 @@ type DecisionConfig struct {
 	LogEnd   bool `yaml:"log_end"`
 }
 
-type ProtocolConfig struct {
-	// The first two options are for HTTP config.
+type HTTPProtocolConfig struct {
 	Path string `yaml:"path"`
 	Port uint64 `yaml:"port"`
-	// The last two options are for gRPC config.
+}
+
+type GRPCProtocolConfig struct {
 	Service string `yaml:"service"`
 	Method  string `yaml:"method"`
-	// Raise an error if all four options are configured
-	// Can fill up first two/last option field in yaml
 }
