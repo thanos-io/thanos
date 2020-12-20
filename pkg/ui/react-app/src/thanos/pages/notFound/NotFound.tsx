beff20314be10a175eca361a30809d8cb45bbbc9
ajinkya359 <sajinkya359@gmail.com>
2020-12-20 14:53:31 +0530

    add default path to the NotFoundComponent , handled errors in ui.go
    Signed-off-by: ajinkya359 <sajinkya359@gmail.com>

--- pkg/ui/react-app/src/thanos/pages/notFound/NotFound.tsx
+++ pkg/ui/react-app/src/thanos/pages/notFound/NotFound.tsx
@@ -1,9 +1,24 @@
 import React, { FC } from 'react';
+import PathPrefixProps from '../../../types/PathPrefixProps';
 
 interface NotFoundProps {
   default: boolean;
+  defaultRoute: string;
 }
 
-const NotFound: FC<NotFoundProps> = () => <h3>404, Page Not Found</h3>;
+
+const NotFound: FC<NotFoundProps& PathPrefixProps> = ({pathPrefix,defaultRoute})=>{
+  return(
+    <div style={{
+      position: 'absolute', left: '50%', top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      <h1>404, Page Not Found!</h1>
+      <h5>
+        <a href={`${pathPrefix}/new${defaultRoute}`}>Go back to home page</a></h5> 
+    </div>
+    
+  )
+}
 
 export default NotFound;
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -39,7 +39,10 @@ func NewBaseUI(logger log.Logger, menuTmpl string, funcMap template.FuncMap, tmp
 func (bu *BaseUI) serveStaticAsset(w http.ResponseWriter, req *http.Request) {
 	fp := route.Param(req.Context(), "filepath")
 	fp = filepath.Join("pkg/ui/static", fp)
-	_ = bu.serveAsset(fp, w, req)
+	if err := bu.serveAsset(fp, w, req); err != nil {
+		level.Warn(bu.logger).Log("msg", "Could not get file", "err", err, "file", fp)
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func (bu *BaseUI) serveReactUI(w http.ResponseWriter, req *http.Request) {
@@ -47,7 +50,7 @@ func (bu *BaseUI) serveReactUI(w http.ResponseWriter, req *http.Request) {
 	fp = filepath.Join("pkg/ui/static/react/", fp)
 	if err := bu.serveAsset(fp, w, req); err != nil {
 		bu.serveReactIndex("pkg/ui/static/react/index.html", w, req)
-		return
+
 	}
 }
 
@@ -88,7 +91,6 @@ func (bu *BaseUI) getAssetFile(filename string) (os.FileInfo, []byte, error) {
 func (bu *BaseUI) serveAsset(fp string, w http.ResponseWriter, req *http.Request) error {
 	info, file, err := bu.getAssetFile(fp)
 	if err != nil {
-		level.Warn(bu.logger).Log("msg", "Could not get file", "err", err, "file", fp)
 		return err
 	}
 	http.ServeContent(w, req, info.Name(), info.ModTime(), bytes.NewReader(file))
