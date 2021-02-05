42888ab94d6b067a91f40ec951a1e8f9900f7e47
Ben Kochie <superq@gmail.com>
2021-02-05 19:39:35 +0100

    Pad compaction planner size check (#3773)
    Pad the index size check in the compaction planner by 15% to avoid
    situations where the sum of index bytes bloats to be larger than the
    sum of the original index sizes.
    
    * https://github.com/thanos-io/thanos/issues/3724
    * https://github.com/thanos-io/thanos/issues/3750
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- pkg/compact/planner_test.go
+++ pkg/compact/planner_test.go
@@ -710,11 +710,11 @@ func TestLargeTotalIndexSizeFilter_Plan(t *testing.T) {
 		{
 			name: "Blocks to fill the entire parent, but with last size exceeded (should not matter and not even marked).",
 			metas: []*metadata.Meta{
-				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 30}}},
+				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 10}}},
 					BlockMeta: tsdb.BlockMeta{Version: 1, ULID: ulid.MustNew(1, nil), MinTime: 0, MaxTime: 20}},
-				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 30}}},
+				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 10}}},
 					BlockMeta: tsdb.BlockMeta{Version: 1, ULID: ulid.MustNew(2, nil), MinTime: 20, MaxTime: 40}},
-				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 30}}},
+				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 10}}},
 					BlockMeta: tsdb.BlockMeta{Version: 1, ULID: ulid.MustNew(3, nil), MinTime: 40, MaxTime: 60}},
 				{Thanos: metadata.Thanos{Files: []metadata.File{{RelPath: block.IndexFilename, SizeBytes: 90}}},
 					BlockMeta: tsdb.BlockMeta{Version: 1, ULID: ulid.MustNew(4, nil), MinTime: 60, MaxTime: 80}},
