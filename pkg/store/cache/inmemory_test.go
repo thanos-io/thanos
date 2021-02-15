4e93eebc15688e7eae67ad67401cbb5c2c133a89
Sudharshann D <sudhar287@gmail.com>
2021-02-15 00:06:46 -0600

    cleaning comments
    Signed-off-by: Sudharshann D <sudhar287@gmail.com>

--- pkg/store/cache/inmemory_test.go
+++ pkg/store/cache/inmemory_test.go
@@ -45,7 +45,7 @@ max_item_size: 2KB
 	testutil.Equals(t, uint64(1024*1024), cache.maxSizeBytes)
 	testutil.Equals(t, uint64(2*1024), cache.maxItemSizeBytes)
 
-	// Should instance an in-memory index cache with specified YAML config.s with units.
+	// Should return error when max_size is lesser than max_item_size of cache.
 	conf = []byte(`
 max_size: 2KB
 max_item_size: 1MB
@@ -53,11 +53,6 @@ max_item_size: 1MB
 	cache, err = NewInMemoryIndexCache(log.NewNopLogger(), nil, conf)
 	testutil.NotOk(t, err)
 	testutil.Equals(t, (*InMemoryIndexCache)(nil), cache)
-	// testutil.Equals(t, uint64(1024*1024), cache.maxSizeBytes)
-	// testutil.Equals(t, uint64(2*1024), cache.maxItemSizeBytes)
-
-	// testutil.Equals(t, uint64(1024*1024), cache.maxItemSizeBytes)
-	// testutil.Equals(t, uint64(2*1024), cache.maxSizeBytes)
 }
 
 func TestInMemoryIndexCache_AvoidsDeadlock(t *testing.T) {
