ee337ea9431cad5273d8eb9958be89f8c5db1030
allenzhli <allenzhli@tencent.com>
2021-04-06 14:51:53 +0800

    update metrics
    Signed-off-by: allenzhli <allenzhli@tencent.com>

--- pkg/compact/compact.go
+++ pkg/compact/compact.go
@@ -310,7 +310,7 @@ func (g *DefaultGrouper) Groups(blocks map[ulid.ULID]*metadata.Meta) (res []*Gro
 			groups[groupKey] = group
 			res = append(res, group)
 		}
-		if err := group.Add(m); err != nil {
+		if err := group.AppendMeta(m); err != nil {
 			return nil, errors.Wrap(err, "add compaction group")
 		}
 	}
@@ -388,8 +388,8 @@ func (cg *Group) Key() string {
 	return cg.key
 }
 
-// Add the block with the given meta to the group.
-func (cg *Group) Add(meta *metadata.Meta) error {
+// AppendMeta the block with the given meta to the group.
+func (cg *Group) AppendMeta(meta *metadata.Meta) error {
 	cg.mtx.Lock()
 	defer cg.mtx.Unlock()
 
