dd9746845a114deced76df62cee9ad8968546b5a
Giedrius Statkevičius <giedriuswork@gmail.com>
2021-04-10 11:47:38 +0300

    Merge pull request #4041 from GiedriusS/fix/logger
    logging: fix the HTTP logger host field parsing.

--- pkg/logging/http_test.go
+++ pkg/logging/http_test.go
@@ -4,10 +4,13 @@
 package logging
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/go-kit/kit/log"
@@ -15,23 +18,50 @@ import (
 )
 
 func TestHTTPServerMiddleware(t *testing.T) {
-	m := NewHTTPServerMiddleware(log.NewNopLogger())
+	b := bytes.Buffer{}
+
+	m := NewHTTPServerMiddleware(log.NewLogfmtLogger(io.Writer(&b)))
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		_, err := io.WriteString(w, "Test Works")
 		if err != nil {
-			t.Log(err)
+			testutil.Ok(t, err)
 		}
 	}
 	hm := m.HTTPMiddleware("test", http.HandlerFunc(handler))
 
-	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	// Regression test for Cortex way - https://github.com/thanos-io/thanos/pull/4041
+	u, err := url.Parse("http://example.com:5555/foo")
+	testutil.Ok(t, err)
+	req := &http.Request{
+		Method: "GET",
+		URL:    u,
+		Body:   nil,
+	}
+
 	w := httptest.NewRecorder()
 
 	hm(w, req)
 
 	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	testutil.Ok(t, err)
 
 	testutil.Equals(t, 200, resp.StatusCode)
 	testutil.Equals(t, "Test Works", string(body))
+	testutil.Assert(t, !strings.Contains(b.String(), "err="))
+
+	// Typical way:
+	req = httptest.NewRequest("GET", "http://example.com:5555/foo", nil)
+	b.Reset()
+
+	w = httptest.NewRecorder()
+	hm(w, req)
+
+	resp = w.Result()
+	body, err = ioutil.ReadAll(resp.Body)
+	testutil.Ok(t, err)
+
+	testutil.Equals(t, 200, resp.StatusCode)
+	testutil.Equals(t, "Test Works", string(body))
+	testutil.Assert(t, !strings.Contains(b.String(), "err="))
 }
