3def283887dbb3cb785b3a8d40b7b295a717ec5d
Somesh Koli <kolisomesh27@gmail.com>
2021-04-20 00:26:24 +0530

    Update cmd/thanos/store.go
    fix str formatting on min/max time
    
    Co-authored-by: Kemal Akkoyun <kakkoyun@users.noreply.github.com>
    Signed-off-by: someshkoli <kolisomesh27@gmail.com>

--- cmd/thanos/store.go
+++ cmd/thanos/store.go
@@ -172,7 +172,7 @@ func registerStore(app *extkingpin.App) {
 	cmd.Setup(func(g *run.Group, logger log.Logger, reg *prometheus.Registry, tracer opentracing.Tracer, _ <-chan struct{}, debugLogging bool) error {
 		if conf.filterConf.MinTime.PrometheusTimestamp() > conf.filterConf.MaxTime.PrometheusTimestamp() {
 			return errors.Errorf("invalid argument: --min-time '%s' can't be greater than --max-time '%s'",
-				conf.filterConf.MaxTime, conf.filterConf.MinTime)
+				conf.filterConf.MinTime, conf.filterConf.MaxTime)
 		}
 
 		httpLogOpts, err := logging.ParseHTTPOptions("", conf.reqLogConfig)
