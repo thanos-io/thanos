05841768de1a8da1e4ce9aef4f5fd7e3d13f1ec9
Hitanshu Mehta <hitanshu99amehta@gmail.com>
2021-04-20 17:15:32 +0530

    Fix error
    Signed-off-by: Hitanshu Mehta <hitanshu99amehta@gmail.com>

--- cmd/thanos/rule.go
+++ cmd/thanos/rule.go
@@ -593,7 +593,7 @@ func runRule(
 			}
 		}()
 
-		s := shipper.New(logger, reg, conf.dataDir, bkt, func() labels.Labels { return conf.lset }, metadata.RulerSource, false, conf.shipper.allowOutOfOrderUpload, metadata.HashFunc(conf.shipper.hashFunc))
+		s := shipper.New(logger, reg, conf.dataDir, bkt, func() labels.Labels { return conf.lset }, metadata.RulerSource, false, conf.shipper.uploadDebubgMetaFiles, conf.shipper.allowOutOfOrderUpload, metadata.HashFunc(conf.shipper.hashFunc))
 
 		ctx, cancel := context.WithCancel(context.Background())
 
