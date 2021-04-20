2b390c3c4270655a9bbd72245a51aa088374b301
Augustin Husson <husson.augustin@gmail.com>
2021-04-20 12:16:00 +0200

    make the reactUI the default one
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- pkg/ui/rule.go
+++ pkg/ui/rule.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"path"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/go-kit/kit/log"
@@ -161,24 +162,25 @@ func (ru *Rule) root(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ru *Rule) Register(r *route.Router, ins extpromhttp.InstrumentationMiddleware) {
-	instrf := func(name string, next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
-		return ins.NewHandler(name, http.HandlerFunc(next))
-	}
-
-	r.Get("/", instrf("root", ru.root))
-	r.Get("/alerts", instrf("alerts", ru.alerts))
-	r.Get("/rules", instrf("rules", ru.rules))
-
-	r.Get("/static/*filepath", instrf("static", ru.serveStaticAsset))
-	// Make sure that "<path-prefix>/new" is redirected to "<path-prefix>/new/" and
-	// not just the naked "/new/", which would be the default behavior of the router
-	// with the "RedirectTrailingSlash" option (https://godoc.org/github.com/julienschmidt/httprouter#Router.RedirectTrailingSlash),
-	// and which breaks users with a --web.route-prefix that deviates from the path derived
-	// from the external URL.
-	r.Get("/new", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, path.Join(GetWebPrefix(ru.logger, ru.externalPrefix, ru.prefixHeader, r), "new")+"/", http.StatusFound)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, path.Join(GetWebPrefix(ru.logger, ru.externalPrefix, ru.prefixHeader, r), "/alerts"), http.StatusFound)
 	})
-	r.Get("/new/*filepath", instrf("react-static", ru.serveReactUI))
+
+	r.Get("/classic/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, path.Join(GetWebPrefix(ru.logger, ru.externalPrefix, ru.prefixHeader, r), "/classic/alerts"), http.StatusFound)
+	})
+
+	// Redirect the original React UI's path (under "/new") to its new path at the root.
+	r.Get("/new/*path", func(w http.ResponseWriter, r *http.Request) {
+		p := route.Param(r.Context(), "path")
+		http.Redirect(w, r, path.Join(GetWebPrefix(ru.logger, ru.externalPrefix, ru.prefixHeader, r), strings.TrimPrefix(p, "/new"))+"?"+r.URL.RawQuery, http.StatusFound)
+	})
+
+	r.Get("/classic/alerts", instrf("alerts", ins, ru.alerts))
+	r.Get("/classic/rules", instrf("rules", ins, ru.rules))
+
+	r.Get("/classic/static/*filepath", instrf("static", ins, ru.serveStaticAsset))
+	registerReactApp(r, ins, ru.BaseUI)
 }
 
 // AlertStatus bundles alerting rules and the mapping of alert states to row classes.
