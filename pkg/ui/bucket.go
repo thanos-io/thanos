991cf2e239b516003607b159fe2188ed1d669718
Augustin Husson <husson.augustin@gmail.com>
2021-04-21 19:27:19 +0200

    rollback the bucket to see if it fixed the e2e
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- pkg/ui/bucket.go
+++ pkg/ui/bucket.go
@@ -49,26 +49,35 @@ func NewBucketUI(logger log.Logger, label, externalPrefix, prefixHeader, uiPrefi
 
 // Register registers http routes for bucket UI.
 func (b *Bucket) Register(r *route.Router, ins extpromhttp.InstrumentationMiddleware) {
-	redirectPath := "/blocks"
-	if len(b.uiPrefix) > 0 {
-		redirectPath = b.uiPrefix
-	}
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, path.Join(GetWebPrefix(b.logger, b.externalPrefix, b.prefixHeader, r), redirectPath), http.StatusFound)
-	})
-	// Redirect the original React UI's path (under "/new") to its new path at the root.
-	r.Get("/new/*path", func(w http.ResponseWriter, r *http.Request) {
-		p := route.Param(r.Context(), "path")
-		http.Redirect(w, r, path.Join(GetWebPrefix(b.logger, b.externalPrefix, b.prefixHeader, r), strings.TrimPrefix(p, "/new"))+"?"+r.URL.RawQuery, http.StatusFound)
-	})
+	/*	redirectPath := "/blocks"
+		if len(b.uiPrefix) > 0 {
+			redirectPath = b.uiPrefix
+		}
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, path.Join(GetWebPrefix(b.logger, b.externalPrefix, b.prefixHeader, r), redirectPath), http.StatusFound)
+		})
+		// Redirect the original React UI's path (under "/new") to its new path at the root.
+		r.Get("/new/*path", func(w http.ResponseWriter, r *http.Request) {
+			p := route.Param(r.Context(), "path")
+			http.Redirect(w, r, path.Join(GetWebPrefix(b.logger, b.externalPrefix, b.prefixHeader, r), strings.TrimPrefix(p, "/new"))+"?"+r.URL.RawQuery, http.StatusFound)
+		})
 
-	// here we have two routes that serve the same document. It's because it depends where do we come from.
-	// If we are coming from the new UI, it will use the first route.
-	// If we are coming from the old UI, it will use the second route.
-	r.Get(path.Join("/classic", b.uiPrefix), instrf("bucket", ins, b.bucket))
-	r.WithPrefix(b.uiPrefix).Get("/classic", instrf("bucket", ins, b.bucket))
-	r.WithPrefix(b.uiPrefix).Get("/classic/static/*filepath", instrf("static", ins, b.serveStaticAsset))
-	registerReactApp(r, ins, b.BaseUI)
+		// here we have two routes that serve the same document. It's because it depends where do we come from.
+		// If we are coming from the new UI, it will use the first route.
+		// If we are coming from the old UI, it will use the second route.
+		r.Get(path.Join("/classic", b.uiPrefix), instrf("bucket", ins, b.bucket))
+		r.WithPrefix(b.uiPrefix).Get("/classic", instrf("bucket", ins, b.bucket))
+		r.WithPrefix(b.uiPrefix).Get("/classic/static/*filepath", instrf("static", ins, b.serveStaticAsset))
+		registerReactApp(r, ins, b.BaseUI)*/
+	instrf := func(name string, next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+		return ins.NewHandler(b.externalPrefix+name, http.HandlerFunc(next))
+	}
+	r.WithPrefix(b.uiPrefix).Get("/", instrf("root", b.root))
+	r.WithPrefix(b.uiPrefix).Get("/static/*filepath", instrf("static", b.serveStaticAsset))
+}
+
+func (b *Bucket) root(w http.ResponseWriter, r *http.Request) {
+	b.executeTemplate(w, "bucket.html", GetWebPrefix(b.logger, path.Join(b.externalPrefix, strings.TrimPrefix(b.uiPrefix, "/")), b.prefixHeader, r), b)
 }
 
 // Handle / of bucket UIs.
