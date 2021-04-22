642bb55e51a15685b1c754de0cc32cb123da5f42
Peter Štibraný <peter.stibrany@grafana.com>
2021-04-22 15:20:07 +0200

    Move call to TryToGetSize into newTracingReadCloser, instead of calling it on each ObjectSize() call.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/objstore/objstore_test.go
+++ pkg/objstore/objstore_test.go
@@ -5,6 +5,7 @@ package objstore
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	promtest "github.com/prometheus/client_golang/prometheus/testutil"
@@ -66,11 +67,22 @@ func TestMetricBucket_Close(t *testing.T) {
 }
 
 func TestTracingReader(t *testing.T) {
-	r := bytes.NewBuffer([]byte("hello"))
-	tr := &tracingReadCloser{r: NopCloserWithSize(r)}
+	r := bytes.NewReader([]byte("hello world"))
+	tr := newTracingReadCloser(NopCloserWithSize(r), nil)
 
 	size, err := TryToGetSize(tr)
 
 	testutil.Ok(t, err)
-	testutil.Equals(t, int64(5), size)
+	testutil.Equals(t, int64(11), size)
+
+	smallBuf := make([]byte, 4)
+	n, err := io.ReadFull(tr, smallBuf)
+	testutil.Ok(t, err)
+	testutil.Equals(t, 4, n)
+
+	// Verify that size is still the same, after reading 4 bytes.
+	size, err = TryToGetSize(tr)
+
+	testutil.Ok(t, err)
+	testutil.Equals(t, int64(11), size)
 }
