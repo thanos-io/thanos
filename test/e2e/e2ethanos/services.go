43d2fa81f6c2c3cd78f425592ea75d4f7cc3a768
yeya24 <yb532204897@gmail.com>
2021-05-01 19:27:01 -0400

    update E2E test Prometheus image to 2.26
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- test/e2e/e2ethanos/services.go
+++ test/e2e/e2ethanos/services.go
@@ -38,7 +38,7 @@ var defaultBackoffConfig = util.BackoffConfig{
 
 // TODO(bwplotka): Run against multiple?
 func DefaultPrometheusImage() string {
-	return "quay.io/prometheus/prometheus:v2.19.3"
+	return "quay.io/prometheus/prometheus:v2.26.0"
 }
 
 func DefaultAlertmanagerImage() string {
