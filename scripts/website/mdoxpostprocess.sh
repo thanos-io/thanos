ea275ea7aa7009e35e9e97b977cb8c3236fc2d92
Saswata Mukherjee <saswataminsta@yahoo.com>
2021-08-01 13:19:52 +0530

    Update mdox and config
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- scripts/website/mdoxpostprocess.sh
+++ scripts/website/mdoxpostprocess.sh
@@ -14,20 +14,3 @@ weight: ${TOP_WEIGHT}
 ---
 EOF
 )" >${OUTPUT_CONTENT_DIR}/_index.md
-
-# Add edit footer to all markdown files assumed as content.
-ALL_DOC_CONTENT_FILES=$(echo "${OUTPUT_CONTENT_DIR}/**/*.md")
-for file in ${ALL_DOC_CONTENT_FILES}; do
-  relFile=${file##${OUTPUT_CONTENT_DIR}/}
-  echo "$(
-    cat <<EOF
-
----
-
-Found a typo, inconsistency or missing information in our docs?
-Help us to improve [Thanos](https://thanos.io) documentation by proposing a fix [on GitHub here](https://github.com/thanos-io/thanos/edit/main/docs/${relFile}) :heart:
-
-EOF
-  )" >>${file}
-
-done
