206ce9880845a50f2e3062a2e69ffdef8aac9e96
Bartlomiej Plotka <bwplotka@gmail.com>
2021-11-02 17:17:17 +0100

    Reverted due to broken main.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- tutorials/katacoda/thanos/4-receiver-agent/courseBase.sh
+++ tutorials/katacoda/thanos/4-receiver-agent/courseBase.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker pull quay.io/prometheus/prometheus:main # TODO(bwplotka): Lock to the release with agent.
+docker pull quay.io/bwplotka/prometheus:agent1
 docker pull quay.io/thanos/thanos:v0.21.0
 
 mkdir /root/editor
