f07f26987419aabb0b7f380183d0d3d02b26362b
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2021-11-29 16:07:16 +0200

    e2e: add downsampling testcase to compact_test
    Try to add a test for https://github.com/thanos-io/thanos/issues/4549
    but couldn't reproduce it :/
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- test/e2e/tools_bucket_web_test.go
+++ test/e2e/tools_bucket_web_test.go
@@ -209,7 +209,7 @@ func TestToolsBucketWebWithTimeAndRelabelFilter(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		t.Cleanup(cancel)
 
-		id, err := b.Create(ctx, dir, 0, b.hashFunc)
+		id, err := b.Create(ctx, dir, 0, b.hashFunc, 120)
 		testutil.Ok(t, err)
 		testutil.Ok(t, runutil.Retry(time.Second, ctx.Done(), func() error {
 			return objstore.UploadDir(ctx, logger, bkt, path.Join(dir, id.String()), id.String())
