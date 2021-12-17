00ed04e444cb146ae45a869ed8682c577a37e630
Giedrius Statkevičius <giedriuswork@gmail.com>
2021-12-17 14:32:48 +0200

    downsample: fix deadlock if error occurs (#4962)
    Fix deadlock that occurs if there is some backlog of blocks to be
    deduplicated, and an error occurs. For this to trigger, there needs to
    be at least `downsampleConcurrency + 2` blocks in the backlog.
    
    Add test to cover this regression. Affected versions 0.22.0 - 0.23.1.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- cmd/thanos/main_test.go
+++ cmd/thanos/main_test.go
@@ -5,9 +5,12 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 	"testing"
 	"time"
 
@@ -24,6 +27,144 @@ import (
 	"github.com/thanos-io/thanos/pkg/testutil"
 	"github.com/thanos-io/thanos/pkg/testutil/e2eutil"
 )
+
+type erroringBucket struct {
+	bkt objstore.InstrumentedBucket
+}
+
+func (b *erroringBucket) Close() error {
+	return b.bkt.Close()
+}
+
+// WithExpectedErrs allows to specify a filter that marks certain errors as expected, so it will not increment
+// thanos_objstore_bucket_operation_failures_total metric.
+func (b *erroringBucket) WithExpectedErrs(f objstore.IsOpFailureExpectedFunc) objstore.Bucket {
+	return b.bkt.WithExpectedErrs(f)
+}
+
+// ReaderWithExpectedErrs allows to specify a filter that marks certain errors as expected, so it will not increment
+// thanos_objstore_bucket_operation_failures_total metric.
+func (b *erroringBucket) ReaderWithExpectedErrs(f objstore.IsOpFailureExpectedFunc) objstore.BucketReader {
+	return b.bkt.ReaderWithExpectedErrs(f)
+}
+
+func (b *erroringBucket) Iter(ctx context.Context, dir string, f func(string) error, options ...objstore.IterOption) error {
+	return b.bkt.Iter(ctx, dir, f, options...)
+}
+
+// Get returns a reader for the given object name.
+func (b *erroringBucket) Get(ctx context.Context, name string) (io.ReadCloser, error) {
+	if strings.Contains(name, "chunk") {
+		return nil, fmt.Errorf("some random error has occurred")
+	}
+	return b.bkt.Get(ctx, name)
+}
+
+// GetRange returns a new range reader for the given object name and range.
+func (b *erroringBucket) GetRange(ctx context.Context, name string, off, length int64) (io.ReadCloser, error) {
+	if strings.Contains(name, "chunk") {
+		return nil, fmt.Errorf("some random error has occurred")
+	}
+	return b.bkt.GetRange(ctx, name, off, length)
+}
+
+// Exists checks if the given object exists in the bucket.
+func (b *erroringBucket) Exists(ctx context.Context, name string) (bool, error) {
+	return b.bkt.Exists(ctx, name)
+}
+
+// IsObjNotFoundErr returns true if error means that object is not found. Relevant to Get operations.
+func (b *erroringBucket) IsObjNotFoundErr(err error) bool {
+	return b.bkt.IsObjNotFoundErr(err)
+}
+
+// Attributes returns information about the specified object.
+func (b *erroringBucket) Attributes(ctx context.Context, name string) (objstore.ObjectAttributes, error) {
+	return b.bkt.Attributes(ctx, name)
+}
+
+// Upload the contents of the reader as an object into the bucket.
+// Upload should be idempotent.
+func (b *erroringBucket) Upload(ctx context.Context, name string, r io.Reader) error {
+	return b.bkt.Upload(ctx, name, r)
+}
+
+// Delete removes the object with the given name.
+// If object does not exists in the moment of deletion, Delete should throw error.
+func (b *erroringBucket) Delete(ctx context.Context, name string) error {
+	return b.bkt.Delete(ctx, name)
+}
+
+// Name returns the bucket name for the provider.
+func (b *erroringBucket) Name() string {
+	return b.bkt.Name()
+}
+
+// Ensures that downsampleBucket() stops its work properly
+// after an error occurs with some blocks in the backlog.
+// Testing for https://github.com/thanos-io/thanos/issues/4960.
+func TestRegression4960_Deadlock(t *testing.T) {
+	logger := log.NewLogfmtLogger(os.Stderr)
+	dir, err := ioutil.TempDir("", "test-compact-cleanup")
+	testutil.Ok(t, err)
+	defer func() { testutil.Ok(t, os.RemoveAll(dir)) }()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	bkt := objstore.WithNoopInstr(objstore.NewInMemBucket())
+	bkt = &erroringBucket{bkt: bkt}
+	var id, id2, id3 ulid.ULID
+	{
+		id, err = e2eutil.CreateBlock(
+			ctx,
+			dir,
+			[]labels.Labels{{{Name: "a", Value: "1"}}},
+			1, 0, downsample.DownsampleRange0+1, // Pass the minimum DownsampleRange0 check.
+			labels.Labels{{Name: "e1", Value: "1"}},
+			downsample.ResLevel0, metadata.NoneFunc)
+		testutil.Ok(t, err)
+		testutil.Ok(t, block.Upload(ctx, logger, bkt, path.Join(dir, id.String()), metadata.NoneFunc))
+	}
+	{
+		id2, err = e2eutil.CreateBlock(
+			ctx,
+			dir,
+			[]labels.Labels{{{Name: "a", Value: "2"}}},
+			1, 0, downsample.DownsampleRange0+1, // Pass the minimum DownsampleRange0 check.
+			labels.Labels{{Name: "e1", Value: "2"}},
+			downsample.ResLevel0, metadata.NoneFunc)
+		testutil.Ok(t, err)
+		testutil.Ok(t, block.Upload(ctx, logger, bkt, path.Join(dir, id2.String()), metadata.NoneFunc))
+	}
+	{
+		id3, err = e2eutil.CreateBlock(
+			ctx,
+			dir,
+			[]labels.Labels{{{Name: "a", Value: "2"}}},
+			1, 0, downsample.DownsampleRange0+1, // Pass the minimum DownsampleRange0 check.
+			labels.Labels{{Name: "e1", Value: "2"}},
+			downsample.ResLevel0, metadata.NoneFunc)
+		testutil.Ok(t, err)
+		testutil.Ok(t, block.Upload(ctx, logger, bkt, path.Join(dir, id3.String()), metadata.NoneFunc))
+	}
+
+	meta, err := block.DownloadMeta(ctx, logger, bkt, id)
+	testutil.Ok(t, err)
+
+	metrics := newDownsampleMetrics(prometheus.NewRegistry())
+	testutil.Equals(t, 0.0, promtest.ToFloat64(metrics.downsamples.WithLabelValues(compact.DefaultGroupKey(meta.Thanos))))
+	metaFetcher, err := block.NewMetaFetcher(nil, block.FetcherConcurrency, bkt, "", nil, nil, nil)
+	testutil.Ok(t, err)
+
+	metas, _, err := metaFetcher.Fetch(ctx)
+	testutil.Ok(t, err)
+	err = downsampleBucket(ctx, logger, metrics, bkt, metas, dir, 1, metadata.NoneFunc)
+	testutil.NotOk(t, err)
+
+	testutil.Assert(t, strings.Contains(err.Error(), "some random error has occurred"))
+
+}
 
 func TestCleanupDownsampleCacheFolder(t *testing.T) {
 	logger := log.NewLogfmtLogger(os.Stderr)
