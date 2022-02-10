b2c1ff02b3920f183ef8ef1626f1e8f17e10f229
Augustin Husson <husson.augustin@gmail.com>
2022-02-10 13:31:09 +0100

    UI: Optimize Target,  Alert and Service Discovery pages (#5119)
    * add generic component for infinite scroll and search bar
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * rework service discovery page
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * rework alert page
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * rework target page
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * add entry in the changelog
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * fix test
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * fix linter issue
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * add infinite scroll on target
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * modify the assets
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * fix Changelog
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * persis the search event
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * add missing bindata
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- pkg/ui/react-app/src/pages/targets/ScrapePoolList.tsx
+++ pkg/ui/react-app/src/pages/targets/ScrapePoolList.tsx
@@ -1,43 +1,127 @@
-import React, { FC } from 'react';
-import { FilterData } from './Filter';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import Filter, { Expanded } from './Filter';
 import { useFetch } from '../../hooks/useFetch';
-import { groupTargets, Target } from './target';
-import ScrapePoolPanel from './ScrapePoolPanel';
+import { groupTargets, ScrapePool, ScrapePools, Target } from './target';
 import PathPrefixProps from '../../types/PathPrefixProps';
 import { withStatusIndicator } from '../../components/withStatusIndicator';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { ToggleMoreLess } from '../../components/ToggleMoreLess';
+import { KVSearch } from '@nexucis/kvsearch';
+import styles from './ScrapePoolPanel.module.css';
+import { Col, Collapse, Row } from 'reactstrap';
+import SearchBar from '../../components/SearchBar';
+import { ScrapePoolContent } from './ScrapePoolContent';
 
 interface ScrapePoolListProps {
-  filter: FilterData;
   activeTargets: Target[];
 }
 
-export const ScrapePoolContent: FC<ScrapePoolListProps> = ({ filter, activeTargets }) => {
-  const targetGroups = groupTargets(activeTargets);
+const kvSearch = new KVSearch({
+  shouldSort: true,
+  indexedKeys: ['labels', 'scrapePool', ['labels', /.*/]],
+});
+
+interface PanelProps {
+  scrapePool: string;
+  targetGroup: ScrapePool;
+  expanded: boolean;
+  toggleExpanded: () => void;
+}
+
+export const ScrapePoolPanel: FC<PanelProps> = (props: PanelProps) => {
+  const modifier = props.targetGroup.upCount < props.targetGroup.targets.length ? 'danger' : 'normal';
+  const id = `pool-${props.scrapePool}`;
+  const anchorProps = {
+    href: `#${id}`,
+    id,
+  };
+  return (
+    <div>
+      <ToggleMoreLess event={props.toggleExpanded} showMore={props.expanded}>
+        <a className={styles[modifier]} {...anchorProps}>
+          {`${props.scrapePool} (${props.targetGroup.upCount}/${props.targetGroup.targets.length} up)`}
+        </a>
+      </ToggleMoreLess>
+      <Collapse isOpen={props.expanded}>
+        <ScrapePoolContent targets={props.targetGroup.targets} />
+      </Collapse>
+    </div>
+  );
+};
+
+export const ScrapePoolListContent: FC<ScrapePoolListProps> = ({ activeTargets }) => {
+  const initialPoolList = groupTargets(activeTargets);
+  const [poolList, setPoolList] = useState<ScrapePools>(initialPoolList);
+  const [targetList, setTargetList] = useState(activeTargets);
+  const [filter, setFilter] = useLocalStorage('targets-page-filter', { showHealthy: true, showUnhealthy: true });
+
+  const initialExpanded: Expanded = Object.keys(initialPoolList).reduce(
+    (acc: { [scrapePool: string]: boolean }, scrapePool: string) => ({
+      ...acc,
+      [scrapePool]: true,
+    }),
+    {}
+  );
+  const [expanded, setExpanded] = useLocalStorage('targets-page-expansion-state', initialExpanded);
   const { showHealthy, showUnhealthy } = filter;
+
+  const handleSearchChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    if (e.target.value !== '') {
+      const result = kvSearch.filter(e.target.value.trim(), activeTargets);
+      setTargetList(
+        result.map((value) => {
+          return value.original as unknown as Target;
+        })
+      );
+    } else {
+      setTargetList(activeTargets);
+    }
+  };
+
+  useEffect(() => {
+    const list = targetList.filter((t) => showHealthy || t.health.toLowerCase() !== 'up');
+    setPoolList(groupTargets(list));
+  }, [showHealthy, targetList]);
+
   return (
     <>
-      {Object.keys(targetGroups).reduce<JSX.Element[]>((panels, scrapePool) => {
-        const targetGroup = targetGroups[scrapePool];
-        const isHealthy = targetGroup.upCount === targetGroup.targets.length;
-        return (isHealthy && showHealthy) || (!isHealthy && showUnhealthy)
-          ? [...panels, <ScrapePoolPanel key={scrapePool} scrapePool={scrapePool} targetGroup={targetGroup} />]
-          : panels;
-      }, [])}
+      <Row xs="4" className="align-items-center">
+        <Col>
+          <Filter filter={filter} setFilter={setFilter} expanded={expanded} setExpanded={setExpanded} />
+        </Col>
+        <Col xs="6">
+          <SearchBar handleChange={handleSearchChange} placeholder="Filter by endpoint or labels" />
+        </Col>
+      </Row>
+      {Object.keys(poolList)
+        .filter((scrapePool) => {
+          const targetGroup = poolList[scrapePool];
+          const isHealthy = targetGroup.upCount === targetGroup.targets.length;
+          return (isHealthy && showHealthy) || (!isHealthy && showUnhealthy);
+        })
+        .map<JSX.Element>((scrapePool) => (
+          <ScrapePoolPanel
+            key={scrapePool}
+            scrapePool={scrapePool}
+            targetGroup={poolList[scrapePool]}
+            expanded={expanded[scrapePool]}
+            toggleExpanded={(): void => setExpanded({ ...expanded, [scrapePool]: !expanded[scrapePool] })}
+          />
+        ))}
     </>
   );
 };
-ScrapePoolContent.displayName = 'ScrapePoolContent';
+ScrapePoolListContent.displayName = 'ScrapePoolListContent';
 
-const ScrapePoolListWithStatusIndicator = withStatusIndicator(ScrapePoolContent);
+const ScrapePoolListWithStatusIndicator = withStatusIndicator(ScrapePoolListContent);
 
-const ScrapePoolList: FC<{ filter: FilterData } & PathPrefixProps> = ({ pathPrefix, filter }) => {
+const ScrapePoolList: FC<PathPrefixProps> = ({ pathPrefix }) => {
   const { response, error, isLoading } = useFetch<ScrapePoolListProps>(`${pathPrefix}/api/v1/targets?state=active`);
   const { status: responseStatus } = response;
   const badResponse = responseStatus !== 'success' && responseStatus !== 'start fetching';
   return (
     <ScrapePoolListWithStatusIndicator
       {...response.data}
-      filter={filter}
       error={badResponse ? new Error(responseStatus) : error}
       isLoading={isLoading}
       componentTitle="Targets information"
--- pkg/ui/react-app/src/pages/targets/Targets.tsx
+++ pkg/ui/react-app/src/pages/targets/Targets.tsx
@@ -6,14 +6,11 @@ import PathPrefixProps from '../../types/PathPrefixProps';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 const Targets: FC<RouteComponentProps & PathPrefixProps> = ({ pathPrefix }) => {
-  const [filter, setFilter] = useLocalStorage('targets-page-filter', { showHealthy: true, showUnhealthy: true });
-  const filterProps = { filter, setFilter };
-  const scrapePoolListProps = { filter, pathPrefix };
+  const scrapePoolListProps = { pathPrefix };
 
   return (
     <>
       <h2>Targets</h2>
-      <Filter {...filterProps} />
       <ScrapePoolList {...scrapePoolListProps} />
     </>
   );
