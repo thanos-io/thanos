2d8efa4e2b838c1deb60bf173b9051b8702e7ce4
Augustin Husson <husson.augustin@gmail.com>
2022-02-11 11:22:39 +0100

    UI: improve graph color (#5144)
    * improve graph color
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * apply linter
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * add assets
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    
    * add changelog
    
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>
    Signed-off-by: Nicholaswang <wzhever@gmail.com>

--- pkg/ui/react-app/src/pages/graph/GraphHelpers.test.ts
+++ pkg/ui/react-app/src/pages/graph/GraphHelpers.test.ts
@@ -72,21 +72,6 @@ describe('GraphHelpers', () => {
       }
     });
   });
-  describe('getColors', () => {
-    it('should generate proper colors', () => {
-      const data: any = {
-        resultType: 'matrix',
-        result: [{}, {}, {}, {}, {}, {}, {}],
-      };
-      expect(
-        getColors(data)
-          .map((c) => c.toString())
-          .join(',')
-      ).toEqual(
-        'rgb(237,194,64),rgb(175,216,248),rgb(203,75,75),rgb(77,167,77),rgb(148,64,237),rgb(189,155,51),rgb(140,172,198)'
-      );
-    });
-  });
   describe('parseValue', () => {
     it('should parse number properly', () => {
       expect(parseValue('12.3e')).toEqual(12.3);
