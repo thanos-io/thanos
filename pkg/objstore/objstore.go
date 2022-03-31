0afd343224dc40d27843bb907b2c7008cdbe3219
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-03-31 10:15:42 +0200

    Change error cleanup in `objstore.DownloadDir` to delete files not destination dir (#5229)
    * Change error cleanup in objstore.DownloadDir to delete files not directories
    
    Dst is always a directory. If any file after the first fails to download,
    the cleanup will fail because the destination already contains at least one file.
    This commit changes the cleanup logic to clean up successfully downloaded files one by one
    instead of attempting to clean up the whole dst directory.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Add cleanup of root dst directory.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Add unit test for cleanup of DownloadDir
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Fix linter
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/objstore/objstore.go
+++ pkg/objstore/objstore.go
@@ -255,8 +255,13 @@ func DownloadDir(ctx context.Context, logger log.Logger, bkt BucketReader, origi
 
 	var downloadedFiles []string
 	if err := bkt.Iter(ctx, src, func(name string) error {
+		dst := filepath.Join(dst, filepath.Base(name))
 		if strings.HasSuffix(name, DirDelim) {
-			return DownloadDir(ctx, logger, bkt, originalSrc, name, filepath.Join(dst, filepath.Base(name)), ignoredPaths...)
+			if err := DownloadDir(ctx, logger, bkt, originalSrc, name, dst, ignoredPaths...); err != nil {
+				return err
+			}
+			downloadedFiles = append(downloadedFiles, dst)
+			return nil
 		}
 		for _, ignoredPath := range ignoredPaths {
 			if ignoredPath == strings.TrimPrefix(name, string(originalSrc)+DirDelim) {
@@ -271,6 +276,7 @@ func DownloadDir(ctx context.Context, logger log.Logger, bkt BucketReader, origi
 		downloadedFiles = append(downloadedFiles, dst)
 		return nil
 	}); err != nil {
+		downloadedFiles = append(downloadedFiles, dst) // Last, clean up the root dst directory.
 		// Best-effort cleanup if the download failed.
 		for _, f := range downloadedFiles {
 			if rerr := os.Remove(f); rerr != nil {
