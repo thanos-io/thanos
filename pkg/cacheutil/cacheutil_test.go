thanos-io/thanos

commit 1698a640e390971593056690980131483cd9d2ab

Avoid starting goroutines for memcached batch requests before gate (#5301)
Use the doWithBatch function to avoid starting goroutines to fetch batched
results from memcached before they are allowed to run via the concurrency
Gate. This avoids starting many goroutines which cannot make any progress
due to a concurrency limit.

Fixes #4967

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

pkg/cacheutil/memcached_client.go
@@ -442,7 +442,7 @@ func (c *memcachedClient) GetMulti(ctx context.Context, keys []string) map[strin
 func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([]map[string]*memcache.Item, error) {
 	// Do not batch if the input keys are less than the max batch size.
 	if (c.config.MaxGetMultiBatchSize <= 0) || (len(keys) <= c.config.MaxGetMultiBatchSize) {
-		items, err := c.getMultiSingle(ctx, keys)
+		items, err := c.getMultiSingle(keys)
 		if err != nil {
 			return nil, err
 		}
@@ -457,29 +457,29 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 		numResults++
 	}
 
-	// Spawn a goroutine for each batch request. The max concurrency will be
-	// enforced by getMultiSingle().
+	// If max concurrency is disabled, use a nil gate for the doWithBatch method which will
+	// not apply any limit to the number goroutines started to make batch requests in that case.
+	var getMultiGate gate.Gate
+	if c.config.MaxGetMultiConcurrency > 0 {
+		getMultiGate = c.getMultiGate
+	}
+
+	// Allocate a channel to store results for each batch request. The max concurrency will be
+	// enforced by doWithBatch.
 	results := make(chan *memcachedGetMultiResult, numResults)
 	defer close(results)
 
-	for batchStart := 0; batchStart < len(keys); batchStart += batchSize {
-		batchEnd := batchStart + batchSize
-		if batchEnd > len(keys) {
-			batchEnd = len(keys)
-		}
+	// Ignore the error here since it can only be returned by our provided function which
+	// always returns nil.
+	_ = doWithBatch(ctx, len(keys), c.config.MaxGetMultiBatchSize, getMultiGate, func(startIndex, endIndex int) error {
+		batchKeys := keys[startIndex:endIndex]
 
-		batchKeys := keys[batchStart:batchEnd]
+		res := &memcachedGetMultiResult{}
+		res.items, res.err = c.getMultiSingle(batchKeys)
 
-		c.workers.Add(1)
-		go func() {
-			defer c.workers.Done()
-
-			res := &memcachedGetMultiResult{}
-			res.items, res.err = c.getMultiSingle(ctx, batchKeys)
-
-			results <- res
-		}()
-	}
+		results <- res
+		return nil
+	})
 
 	// Wait for all batch results. In case of error, we keep
 	// track of the last error occurred.
@@ -499,16 +499,7 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 	return items, lastErr
 }
 
-func (c *memcachedClient) getMultiSingle(ctx context.Context, keys []string) (items map[string]*memcache.Item, err error) {
-	// Wait until we get a free slot from the gate, if the max
-	// concurrency should be enforced.
-	if c.config.MaxGetMultiConcurrency > 0 {
-		if err := c.getMultiGate.Start(ctx); err != nil {
-			return nil, errors.Wrapf(err, "failed to wait for turn. Instance: %s", c.name)
-		}
-		defer c.getMultiGate.Done()
-	}
-
+func (c *memcachedClient) getMultiSingle(keys []string) (items map[string]*memcache.Item, err error) {
 	start := time.Now()
 	c.operations.WithLabelValues(opGetMulti).Inc()
 	items, err = c.client.GetMulti(keys)

pkg/cacheutil/memcached_client_test.go
@@ -140,7 +140,7 @@ func TestMemcachedClient_SetAsync(t *testing.T) {
 	testutil.Ok(t, client.SetAsync(ctx, "key-2", []byte("value-2"), time.Second))
 	testutil.Ok(t, backendMock.waitItems(2))
 
-	actual, err := client.getMultiSingle(ctx, []string{"key-1", "key-2"})
+	actual, err := client.getMultiSingle([]string{"key-1", "key-2"})
 	testutil.Ok(t, err)
 	testutil.Equals(t, []byte("value-1"), actual["key-1"].Value)
 	testutil.Equals(t, []byte("value-2"), actual["key-2"].Value)
@@ -166,7 +166,7 @@ func TestMemcachedClient_SetAsyncWithCustomMaxItemSize(t *testing.T) {
 	testutil.Ok(t, client.SetAsync(ctx, "key-2", []byte("value-2-too-long-to-be-stored"), time.Second))
 	testutil.Ok(t, backendMock.waitItems(1))
 
-	actual, err := client.getMultiSingle(ctx, []string{"key-1", "key-2"})
+	actual, err := client.getMultiSingle([]string{"key-1", "key-2"})
 	testutil.Ok(t, err)
 	testutil.Equals(t, []byte("value-1"), actual["key-1"].Value)
 	testutil.Equals(t, (*memcache.Item)(nil), actual["key-2"])

pkg/cacheutil/cacheutil.go
@@ -1,0 +1,42 @@
+// Copyright (c) The Thanos Authors.
+// Licensed under the Apache License 2.0.
+
+package cacheutil
+
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/thanos-io/thanos/pkg/gate"
+)
+
+// doWithBatch do func with batch and gate. batchSize==0 means one batch. gate==nil means no gate.
+func doWithBatch(ctx context.Context, totalSize int, batchSize int, ga gate.Gate, f func(startIndex, endIndex int) error) error {
+	if totalSize == 0 {
+		return nil
+	}
+	if batchSize <= 0 {
+		return f(0, totalSize)
+	}
+	g, ctx := errgroup.WithContext(ctx)
+	for i := 0; i < totalSize; i += batchSize {
+		j := i + batchSize
+		if j > totalSize {
+			j = totalSize
+		}
+		if ga != nil {
+			if err := ga.Start(ctx); err != nil {
+				return nil
+			}
+		}
+		startIndex, endIndex := i, j
+		g.Go(func() error {
+			if ga != nil {
+				defer ga.Done()
+			}
+			return f(startIndex, endIndex)
+		})
+	}
+	return g.Wait()
+}

pkg/cacheutil/redis_client.go
@@ -16,10 +16,10 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"gopkg.in/yaml.v3"
+
 	"github.com/thanos-io/thanos/pkg/extprom"
 	"github.com/thanos-io/thanos/pkg/gate"
-	"golang.org/x/sync/errgroup"
-	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -274,36 +274,6 @@ func stringToBytes(s string) []byte {
 			Cap int
 		}{s, len(s)},
 	))
-}
-
-// doWithBatch do func with batch and gate. batchSize==0 means one batch. gate==nil means no gate.
-func doWithBatch(ctx context.Context, totalSize int, batchSize int, ga gate.Gate, f func(startIndex, endIndex int) error) error {
-	if totalSize == 0 {
-		return nil
-	}
-	if batchSize <= 0 {
-		return f(0, totalSize)
-	}
-	g, ctx := errgroup.WithContext(ctx)
-	for i := 0; i < totalSize; i += batchSize {
-		j := i + batchSize
-		if j > totalSize {
-			j = totalSize
-		}
-		if ga != nil {
-			if err := ga.Start(ctx); err != nil {
-				return nil
-			}
-		}
-		startIndex, endIndex := i, j
-		g.Go(func() error {
-			if ga != nil {
-				defer ga.Done()
-			}
-			return f(startIndex, endIndex)
-		})
-	}
-	return g.Wait()
 }
 
 // parseRedisClientConfig unmarshals a buffer into a RedisClientConfig with default values.

commit c77f371e8a50757d55e56a53d59c31c8de56e27c

Ensure memcached batched requests handle context cancellation
Ensure that when the context used for Memcached GetMulti is cancelled,
getMultiBatched does not hang waiting for results that will never be
generated (since the batched requests will not run if the context has
been cancelled).

Fixes an issue introduced in #5301

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

pkg/cacheutil/memcached_client.go
@@ -442,7 +442,7 @@ func (c *memcachedClient) GetMulti(ctx context.Context, keys []string) map[strin
 func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([]map[string]*memcache.Item, error) {
 	// Do not batch if the input keys are less than the max batch size.
 	if (c.config.MaxGetMultiBatchSize <= 0) || (len(keys) <= c.config.MaxGetMultiBatchSize) {
-		items, err := c.getMultiSingle(keys)
+		items, err := c.getMultiSingle(ctx, keys)
 		if err != nil {
 			return nil, err
 		}
@@ -467,7 +467,12 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 	// Allocate a channel to store results for each batch request. The max concurrency will be
 	// enforced by doWithBatch.
 	results := make(chan *memcachedGetMultiResult, numResults)
-	defer close(results)
+	// NOTE: we are not closing the results channel here on purpose. doWithBatch will start
+	// goroutines to fetch data and write the results back to the `results` channel. If we
+	// close the channel when this method exits before all goroutines have finished writing
+	// (such as when our context is cancelled) they will panic trying to write to a closed
+	// channel. Instead, we let the GC take care of cleaning the channel up when it is no
+	// longer referenced by this method or the workers in doWithBatch.
 
 	// Ignore the error here since it can only be returned by our provided function which
 	// always returns nil.
@@ -475,7 +480,7 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 		batchKeys := keys[startIndex:endIndex]
 
 		res := &memcachedGetMultiResult{}
-		res.items, res.err = c.getMultiSingle(batchKeys)
+		res.items, res.err = c.getMultiSingle(ctx, batchKeys)
 
 		results <- res
 		return nil
@@ -487,22 +492,38 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 	var lastErr error
 
 	for i := 0; i < numResults; i++ {
-		result := <-results
-		if result.err != nil {
-			lastErr = result.err
-			continue
-		}
+		select {
+		case <-ctx.Done():
+			// If the context is cancelled, it's possible not all batches will be run by doWithBatch,
+			// so we should avoid waiting on the results channel here. This ensures that we don't block
+			// indefinitely waiting results that will never come.
+			return nil, ctx.Err()
+		case result := <-results:
+			if result.err != nil {
+				lastErr = result.err
+				continue
+			}
 
-		items = append(items, result.items)
+			items = append(items, result.items)
+		}
 	}
 
 	return items, lastErr
 }
 
-func (c *memcachedClient) getMultiSingle(keys []string) (items map[string]*memcache.Item, err error) {
+func (c *memcachedClient) getMultiSingle(ctx context.Context, keys []string) (items map[string]*memcache.Item, err error) {
 	start := time.Now()
 	c.operations.WithLabelValues(opGetMulti).Inc()
-	items, err = c.client.GetMulti(keys)
+
+	select {
+	case <-ctx.Done():
+		// Make sure our context hasn't been cancelled before fetching cache items using
+		// cache client backend.
+		return nil, ctx.Err()
+	default:
+		items, err = c.client.GetMulti(keys)
+	}
+
 	if err != nil {
 		level.Debug(c.logger).Log("msg", "failed to get multiple items from memcached", "err", err)
 		c.trackError(opGetMulti, err)

pkg/cacheutil/cacheutil_test.go
@@ -4,11 +4,73 @@
 package cacheutil
 
 import (
+	"context"
 	"testing"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/atomic"
 	"go.uber.org/goleak"
+
+	"github.com/thanos-io/thanos/pkg/gate"
+	"github.com/thanos-io/thanos/pkg/testutil"
 )
 
 func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m)
+}
+
+func TestDoWithBatch(t *testing.T) {
+	tests := map[string]struct {
+		items           []string
+		batchSize       int
+		expectedBatches int
+		concurrency     gate.Gate
+	}{
+		"no items": {
+			items:           []string{},
+			batchSize:       2,
+			expectedBatches: 0,
+			concurrency:     nil,
+		},
+
+		"fewer than batch size": {
+			items:           []string{"key1"},
+			batchSize:       2,
+			expectedBatches: 1,
+			concurrency:     nil,
+		},
+
+		"perfect sized for batch": {
+			items:           []string{"key1", "key2", "key3", "key4"},
+			batchSize:       2,
+			expectedBatches: 2,
+			concurrency:     nil,
+		},
+
+		"odd sized for batch": {
+			items:           []string{"key1", "key2", "key3", "key4", "key5"},
+			batchSize:       2,
+			expectedBatches: 3,
+			concurrency:     nil,
+		},
+
+		"odd sized with concurrency limit": {
+			items:           []string{"key1", "key2", "key3", "key4", "key5"},
+			batchSize:       2,
+			expectedBatches: 3,
+			concurrency:     gate.New(prometheus.NewPedanticRegistry(), 1),
+		},
+	}
+
+	for testName, testData := range tests {
+		t.Run(testName, func(t *testing.T) {
+			actualBatches := atomic.Int64{}
+			_ = doWithBatch(context.Background(), len(testData.items), testData.batchSize, testData.concurrency, func(startIndex, endIndex int) error {
+				actualBatches.Inc()
+				return nil
+			})
+
+			testutil.Equals(t, int64(testData.expectedBatches), actualBatches.Load())
+		})
+	}
 }

pkg/cacheutil/memcached_client_test.go
@@ -140,7 +140,7 @@ func TestMemcachedClient_SetAsync(t *testing.T) {
 	testutil.Ok(t, client.SetAsync(ctx, "key-2", []byte("value-2"), time.Second))
 	testutil.Ok(t, backendMock.waitItems(2))
 
-	actual, err := client.getMultiSingle([]string{"key-1", "key-2"})
+	actual, err := client.getMultiSingle(ctx, []string{"key-1", "key-2"})
 	testutil.Ok(t, err)
 	testutil.Equals(t, []byte("value-1"), actual["key-1"].Value)
 	testutil.Equals(t, []byte("value-2"), actual["key-2"].Value)
@@ -166,7 +166,7 @@ func TestMemcachedClient_SetAsyncWithCustomMaxItemSize(t *testing.T) {
 	testutil.Ok(t, client.SetAsync(ctx, "key-2", []byte("value-2-too-long-to-be-stored"), time.Second))
 	testutil.Ok(t, backendMock.waitItems(1))
 
-	actual, err := client.getMultiSingle([]string{"key-1", "key-2"})
+	actual, err := client.getMultiSingle(ctx, []string{"key-1", "key-2"})
 	testutil.Ok(t, err)
 	testutil.Equals(t, []byte("value-1"), actual["key-1"].Value)
 	testutil.Equals(t, (*memcache.Item)(nil), actual["key-2"])
@@ -465,4 +465,54 @@ func TestMultipleClientsCanUseSameRegistry(t *testing.T) {
 	client2, err := NewMemcachedClientWithConfig(log.NewNopLogger(), "b", config, reg)
 	testutil.Ok(t, err)
 	defer client2.Stop()
+}
+
+func TestMemcachedClient_GetMulti_ContextCancelled(t *testing.T) {
+	config := defaultMemcachedClientConfig
+	config.Addresses = []string{"127.0.0.1:11211"}
+	config.MaxGetMultiBatchSize = 2
+	config.MaxGetMultiConcurrency = 2
+
+	// Create a new context that will be used for our "blocking" backend so that we can
+	// actually stop it at the end of the test and not leak goroutines.
+	backendCtx, backendCancel := context.WithCancel(context.Background())
+	defer backendCancel()
+
+	selector := &MemcachedJumpHashSelector{}
+	backendMock := newMemcachedClientBlockingMock(backendCtx)
+
+	client, err := newMemcachedClient(log.NewNopLogger(), backendMock, selector, config, prometheus.NewPedanticRegistry(), "test")
+	testutil.Ok(t, err)
+	defer client.Stop()
+
+	// Immediately cancel the context that will be used for the GetMulti request. This will
+	// ensure that the underlying batching doesn't actually take place and won't generate any
+	// results. We're making sure that we don't block forever waiting for results that will
+	// not be generated. If we did, this test would hang.
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	items := client.GetMulti(ctx, []string{"key1", "key2", "key3", "key4"})
+	testutil.Equals(t, 0, len(items))
+}
+
+type memcachedClientBlockingMock struct {
+	ctx context.Context
+}
+
+func newMemcachedClientBlockingMock(ctx context.Context) *memcachedClientBlockingMock {
+	return &memcachedClientBlockingMock{ctx: ctx}
+}
+
+func (c *memcachedClientBlockingMock) GetMulti([]string) (map[string]*memcache.Item, error) {
+	// Block until this backend client is explicitly stopped so that we can ensure the memcached
+	// client won't be blocked waiting for results that will never be returned.
+	select {
+	case <-c.ctx.Done():
+		return nil, nil
+	}
+}
+
+func (c *memcachedClientBlockingMock) Set(*memcache.Item) error {
+	return nil
 }