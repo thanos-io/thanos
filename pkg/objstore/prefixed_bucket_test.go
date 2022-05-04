0e55117ee88f4a1a0492952e74311dc983e215f3
jademcosta <jade.costa@nubank.com.br>
2022-05-04 09:43:07 -0300

    Test more prefix cases on PrefixedBucket
    Signed-off-by: jademcosta <jade.costa@nubank.com.br>

--- pkg/objstore/prefixed_bucket_test.go
+++ pkg/objstore/prefixed_bucket_test.go
@@ -5,4 +5,16 @@ import "testing"
 func TestPrefixedBucket_AcceptanceTest(t *testing.T) {
 	bkt := NewPrefixedBucket(NewInMemBucket(), "/someprefix/anotherprefix/")
 	AcceptanceTest(t, bkt)
+
+	bkt = NewPrefixedBucket(NewInMemBucket(), "someprefix/anotherprefix/")
+	AcceptanceTest(t, bkt)
+
+	bkt = NewPrefixedBucket(NewInMemBucket(), "someprefix/anotherprefix")
+	AcceptanceTest(t, bkt)
+
+	bkt = NewPrefixedBucket(NewInMemBucket(), "someprefix/")
+	AcceptanceTest(t, bkt)
+
+	bkt = NewPrefixedBucket(NewInMemBucket(), "someprefix")
+	AcceptanceTest(t, bkt)
 }
