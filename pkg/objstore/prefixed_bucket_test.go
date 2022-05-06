d4acbb73d85076ecb84d0b01774dc16fb8711ceb
jademcosta <jade.costa@nubank.com.br>
2022-05-06 18:38:26 -0300

    Remove "@" from test string to make them simpler
    Signed-off-by: jademcosta <jade.costa@nubank.com.br>

--- pkg/objstore/prefixed_bucket_test.go
+++ pkg/objstore/prefixed_bucket_test.go
@@ -38,7 +38,7 @@ func TestPrefixedBucket_Acceptance(t *testing.T) {
 }
 
 func UsesPrefixTest(t *testing.T, bkt Bucket, prefix string) {
-	bkt.Upload(context.Background(), strings.Trim(prefix, "/")+"/file1.jpg", strings.NewReader("@test-data1"))
+	bkt.Upload(context.Background(), strings.Trim(prefix, "/")+"/file1.jpg", strings.NewReader("test-data1"))
 
 	pBkt := NewPrefixedBucket(bkt, prefix)
 	rc1, err := pBkt.Get(context.Background(), "file1.jpg")
@@ -47,16 +47,16 @@ func UsesPrefixTest(t *testing.T, bkt Bucket, prefix string) {
 	defer func() { testutil.Ok(t, rc1.Close()) }()
 	content, err := ioutil.ReadAll(rc1)
 	testutil.Ok(t, err)
-	testutil.Equals(t, "@test-data1", string(content))
+	testutil.Equals(t, "test-data1", string(content))
 
-	pBkt.Upload(context.Background(), "file2.jpg", strings.NewReader("@test-data2"))
+	pBkt.Upload(context.Background(), "file2.jpg", strings.NewReader("test-data2"))
 	rc2, err := bkt.Get(context.Background(), strings.Trim(prefix, "/")+"/file2.jpg")
 
 	testutil.Ok(t, err)
 	defer func() { testutil.Ok(t, rc2.Close()) }()
 	contentUpload, err := ioutil.ReadAll(rc2)
 	testutil.Ok(t, err)
-	testutil.Equals(t, "@test-data2", string(contentUpload))
+	testutil.Equals(t, "test-data2", string(contentUpload))
 
 	pBkt.Delete(context.Background(), "file2.jpg")
 	_, err = bkt.Get(context.Background(), strings.Trim(prefix, "/")+"/file2.jpg")
@@ -69,7 +69,7 @@ func UsesPrefixTest(t *testing.T, bkt Bucket, prefix string) {
 	defer func() { testutil.Ok(t, rc3.Close()) }()
 	content, err = ioutil.ReadAll(rc3)
 	testutil.Ok(t, err)
-	testutil.Equals(t, "tes", string(content))
+	testutil.Equals(t, "est", string(content))
 
 	ok, err := pBkt.Exists(context.Background(), "file1.jpg")
 	testutil.Ok(t, err)
@@ -77,9 +77,9 @@ func UsesPrefixTest(t *testing.T, bkt Bucket, prefix string) {
 
 	attrs, err := pBkt.Attributes(context.Background(), "file1.jpg")
 	testutil.Ok(t, err)
-	testutil.Assert(t, attrs.Size == 11, "expected size to be equal to 11")
+	testutil.Assert(t, attrs.Size == 10, "expected size to be equal to 10")
 
-	bkt.Upload(context.Background(), strings.Trim(prefix, "/")+"/dir/file1.jpg", strings.NewReader("@test-data1"))
+	bkt.Upload(context.Background(), strings.Trim(prefix, "/")+"/dir/file1.jpg", strings.NewReader("test-data1"))
 	seen := []string{}
 	testutil.Ok(t, pBkt.Iter(context.Background(), "", func(fn string) error {
 		seen = append(seen, fn)
