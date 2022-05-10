2278d5ca7d7bf09e987a653a5b391c1b0d000f38
Bartlomiej Plotka <bwplotka@gmail.com>
2022-05-10 13:37:09 +0200

    Fix.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- test/e2e/e2ethanos/services.go
+++ test/e2e/e2ethanos/services.go
@@ -742,7 +742,7 @@ func NewMinio(e e2e.Environment, name, bktName string) e2e.InstrumentedRunnable 
 		filepath.Join(f.Dir(), "certs", "public.crt"),
 		filepath.Join(f.Dir(), "certs", "private.key"),
 		filepath.Join(f.Dir(), "certs", "CAs", "ca.crt"),
-		e.Name()+"-"+name,
+		fmt.Sprintf("minio-%s", name),
 	); err != nil {
 		return e2e.NewErrInstrumentedRunnable(name, errors.Wrap(err, "fail to generate certs"))
 	}
