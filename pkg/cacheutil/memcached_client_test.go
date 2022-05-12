thanos-io/thanos

commit 1698a640e390971593056690980131483cd9d2ab

Avoid starting goroutines for memcached batch requests before gate (#5301)
Use the doWithBatch function to avoid starting goroutines to fetch batched
results from memcached before they are allowed to run via the concurrency
Gate. This avoids starting many goroutines which cannot make any progress
due to a concurrency limit.

Fixes #4967

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

pkg/cacheutil/memcached_client.go
@@ -442,7 +442,7 @@ func (c *memcachedClient) GetMulti(ctx context.Context, keys []string) map[strin
 func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([]map[string]*memcache.Item, error) {
 	// Do not batch if the input keys are less than the max batch size.
 	if (c.config.MaxGetMultiBatchSize <= 0) || (len(keys) <= c.config.MaxGetMultiBatchSize) {
-		items, err := c.getMultiSingle(ctx, keys)
+		items, err := c.getMultiSingle(keys)
 		if err != nil {
 			return nil, err
 		}
@@ -457,29 +457,29 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 		numResults++
 	}
 
-	// Spawn a goroutine for each batch request. The max concurrency will be
-	// enforced by getMultiSingle().
+	// If max concurrency is disabled, use a nil gate for the doWithBatch method which will
+	// not apply any limit to the number goroutines started to make batch requests in that case.
+	var getMultiGate gate.Gate
+	if c.config.MaxGetMultiConcurrency > 0 {
+		getMultiGate = c.getMultiGate
+	}
+
+	// Allocate a channel to store results for each batch request. The max concurrency will be
+	// enforced by doWithBatch.
 	results := make(chan *memcachedGetMultiResult, numResults)
 	defer close(results)
 
-	for batchStart := 0; batchStart < len(keys); batchStart += batchSize {
-		batchEnd := batchStart + batchSize
-		if batchEnd > len(keys) {
-			batchEnd = len(keys)
-		}
+	// Ignore the error here since it can only be returned by our provided function which
+	// always returns nil.
+	_ = doWithBatch(ctx, len(keys), c.config.MaxGetMultiBatchSize, getMultiGate, func(startIndex, endIndex int) error {
+		batchKeys := keys[startIndex:endIndex]
 
-		batchKeys := keys[batchStart:batchEnd]
+		res := &memcachedGetMultiResult{}
+		res.items, res.err = c.getMultiSingle(batchKeys)
 
-		c.workers.Add(1)
-		go func() {
-			defer c.workers.Done()
-
-			res := &memcachedGetMultiResult{}
-			res.items, res.err = c.getMultiSingle(ctx, batchKeys)
-
-			results <- res
-		}()
-	}
+		results <- res
+		return nil
+	})
 
 	// Wait for all batch results. In case of error, we keep
 	// track of the last error occurred.
@@ -499,16 +499,7 @@ func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([
 	return items, lastErr
 }
 
-func (c *memcachedClient) getMultiSingle(ctx context.Context, keys []string) (items map[string]*memcache.Item, err error) {
-	// Wait until we get a free slot from the gate, if the max
-	// concurrency should be enforced.
-	if c.config.MaxGetMultiConcurrency > 0 {
-		if err := c.getMultiGate.Start(ctx); err != nil {
-			return nil, errors.Wrapf(err, "failed to wait for turn. Instance: %s", c.name)
-		}
-		defer c.getMultiGate.Done()
-	}
-
+func (c *memcachedClient) getMultiSingle(keys []string) (items map[string]*memcache.Item, err error) {
 	start := time.Now()
 	c.operations.WithLabelValues(opGetMulti).Inc()
 	items, err = c.client.GetMulti(keys)

pkg/cacheutil/memcached_client_test.go
@@ -140,7 +140,7 @@ func TestMemcachedClient_SetAsync(t *testing.T) {
 	testutil.Ok(t, client.SetAsync(ctx, "key-2", []byte("value-2"), time.Second))
 	testutil.Ok(t, backendMock.waitItems(2))
 
-	actual, err := client.getMultiSingle(ctx, []string{"key-1", "key-2"})
+	actual, err := client.getMultiSingle([]string{"key-1", "key-2"})
 	testutil.Ok(t, err)
 	testutil.Equals(t, []byte("value-1"), actual["key-1"].Value)
 	testutil.Equals(t, []byte("value-2"), actual["key-2"].Value)
@@ -166,7 +166,7 @@ func TestMemcachedClient_SetAsyncWithCustomMaxItemSize(t *testing.T) {
 	testutil.Ok(t, client.SetAsync(ctx, "key-2", []byte("value-2-too-long-to-be-stored"), time.Second))
 	testutil.Ok(t, backendMock.waitItems(1))
 
-	actual, err := client.getMultiSingle(ctx, []string{"key-1", "key-2"})
+	actual, err := client.getMultiSingle([]string{"key-1", "key-2"})
 	testutil.Ok(t, err)
 	testutil.Equals(t, []byte("value-1"), actual["key-1"].Value)
 	testutil.Equals(t, (*memcache.Item)(nil), actual["key-2"])

pkg/cacheutil/cacheutil.go
@@ -1,0 +1,42 @@
+// Copyright (c) The Thanos Authors.
+// Licensed under the Apache License 2.0.
+
+package cacheutil
+
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/thanos-io/thanos/pkg/gate"
+)
+
+// doWithBatch do func with batch and gate. batchSize==0 means one batch. gate==nil means no gate.
+func doWithBatch(ctx context.Context, totalSize int, batchSize int, ga gate.Gate, f func(startIndex, endIndex int) error) error {
+	if totalSize == 0 {
+		return nil
+	}
+	if batchSize <= 0 {
+		return f(0, totalSize)
+	}
+	g, ctx := errgroup.WithContext(ctx)
+	for i := 0; i < totalSize; i += batchSize {
+		j := i + batchSize
+		if j > totalSize {
+			j = totalSize
+		}
+		if ga != nil {
+			if err := ga.Start(ctx); err != nil {
+				return nil
+			}
+		}
+		startIndex, endIndex := i, j
+		g.Go(func() error {
+			if ga != nil {
+				defer ga.Done()
+			}
+			return f(startIndex, endIndex)
+		})
+	}
+	return g.Wait()
+}

pkg/cacheutil/redis_client.go
@@ -16,10 +16,10 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"gopkg.in/yaml.v3"
+
 	"github.com/thanos-io/thanos/pkg/extprom"
 	"github.com/thanos-io/thanos/pkg/gate"
-	"golang.org/x/sync/errgroup"
-	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -274,36 +274,6 @@ func stringToBytes(s string) []byte {
 			Cap int
 		}{s, len(s)},
 	))
-}
-
-// doWithBatch do func with batch and gate. batchSize==0 means one batch. gate==nil means no gate.
-func doWithBatch(ctx context.Context, totalSize int, batchSize int, ga gate.Gate, f func(startIndex, endIndex int) error) error {
-	if totalSize == 0 {
-		return nil
-	}
-	if batchSize <= 0 {
-		return f(0, totalSize)
-	}
-	g, ctx := errgroup.WithContext(ctx)
-	for i := 0; i < totalSize; i += batchSize {
-		j := i + batchSize
-		if j > totalSize {
-			j = totalSize
-		}
-		if ga != nil {
-			if err := ga.Start(ctx); err != nil {
-				return nil
-			}
-		}
-		startIndex, endIndex := i, j
-		g.Go(func() error {
-			if ga != nil {
-				defer ga.Done()
-			}
-			return f(startIndex, endIndex)
-		})
-	}
-	return g.Wait()
 }
 
 // parseRedisClientConfig unmarshals a buffer into a RedisClientConfig with default values.

commit 02d351f50ccedeed2f779df5ae1071b3d753ced8

Enforce memcached concurrency limit with unbatched requests
This ensures that requests that are _not_ split into batches still count
towards the concurrency limit that the client enforces.

This fixes an issue introduced in #5301

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

pkg/cacheutil/memcached_client.go
@@ -442,6 +442,16 @@ func (c *memcachedClient) GetMulti(ctx context.Context, keys []string) map[strin
 func (c *memcachedClient) getMultiBatched(ctx context.Context, keys []string) ([]map[string]*memcache.Item, error) {
 	// Do not batch if the input keys are less than the max batch size.
 	if (c.config.MaxGetMultiBatchSize <= 0) || (len(keys) <= c.config.MaxGetMultiBatchSize) {
+		// Even if we're not splitting the input into batches, make sure that our single request
+		// still counts against the concurrency limit.
+		if c.config.MaxGetMultiConcurrency > 0 {
+			if err := c.getMultiGate.Start(ctx); err != nil {
+				return nil, errors.Wrapf(err, "failed to wait for turn. Instance: %s", c.name)
+			}
+
+			defer c.getMultiGate.Done()
+		}
+
 		items, err := c.getMultiSingle(ctx, keys)
 		if err != nil {
 			return nil, err

pkg/cacheutil/memcached_client_test.go
@@ -14,7 +14,9 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	prom_testutil "github.com/prometheus/client_golang/prometheus/testutil"
+	"go.uber.org/atomic"
 
+	"github.com/thanos-io/thanos/pkg/gate"
 	"github.com/thanos-io/thanos/pkg/model"
 	"github.com/thanos-io/thanos/pkg/testutil"
 )
@@ -179,13 +181,14 @@ func TestMemcachedClient_SetAsyncWithCustomMaxItemSize(t *testing.T) {
 
 func TestMemcachedClient_GetMulti(t *testing.T) {
 	tests := map[string]struct {
-		maxBatchSize          int
-		maxConcurrency        int
-		mockedGetMultiErrors  int
-		initialItems          []memcache.Item
-		getKeys               []string
-		expectedHits          map[string][]byte
-		expectedGetMultiCount int
+		maxBatchSize           int
+		maxConcurrency         int
+		mockedGetMultiErrors   int
+		initialItems           []memcache.Item
+		getKeys                []string
+		expectedHits           map[string][]byte
+		expectedGetMultiCount  int
+		expectedGateStartCount int
 	}{
 		"should fetch keys in a single batch if the input keys is <= the max batch size": {
 			maxBatchSize:   2,
@@ -199,7 +202,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-1": []byte("value-1"),
 				"key-2": []byte("value-2"),
 			},
-			expectedGetMultiCount: 1,
+			expectedGetMultiCount:  1,
+			expectedGateStartCount: 1,
 		},
 		"should fetch keys in multiple batches if the input keys is > the max batch size": {
 			maxBatchSize:   2,
@@ -215,7 +219,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-2": []byte("value-2"),
 				"key-3": []byte("value-3"),
 			},
-			expectedGetMultiCount: 2,
+			expectedGetMultiCount:  2,
+			expectedGateStartCount: 2,
 		},
 		"should fetch keys in multiple batches on input keys exact multiple of batch size": {
 			maxBatchSize:   2,
@@ -233,7 +238,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-3": []byte("value-3"),
 				"key-4": []byte("value-4"),
 			},
-			expectedGetMultiCount: 2,
+			expectedGetMultiCount:  2,
+			expectedGateStartCount: 2,
 		},
 		"should fetch keys in multiple batches on input keys exact multiple of batch size with max concurrency disabled (0)": {
 			maxBatchSize:   2,
@@ -251,7 +257,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-3": []byte("value-3"),
 				"key-4": []byte("value-4"),
 			},
-			expectedGetMultiCount: 2,
+			expectedGetMultiCount:  2,
+			expectedGateStartCount: 0,
 		},
 		"should fetch keys in multiple batches on input keys exact multiple of batch size with max concurrency lower than the batches": {
 			maxBatchSize:   1,
@@ -269,7 +276,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-3": []byte("value-3"),
 				"key-4": []byte("value-4"),
 			},
-			expectedGetMultiCount: 4,
+			expectedGetMultiCount:  4,
+			expectedGateStartCount: 4,
 		},
 		"should fetch keys in a single batch if max batch size is disabled (0)": {
 			maxBatchSize:   0,
@@ -287,7 +295,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-3": []byte("value-3"),
 				"key-4": []byte("value-4"),
 			},
-			expectedGetMultiCount: 1,
+			expectedGetMultiCount:  1,
+			expectedGateStartCount: 1,
 		},
 		"should fetch keys in a single batch if max batch size is disabled (0) and max concurrency is disabled (0)": {
 			maxBatchSize:   0,
@@ -305,7 +314,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-3": []byte("value-3"),
 				"key-4": []byte("value-4"),
 			},
-			expectedGetMultiCount: 1,
+			expectedGetMultiCount:  1,
+			expectedGateStartCount: 0,
 		},
 		"should return no hits on all keys missing": {
 			maxBatchSize:   2,
@@ -319,7 +329,8 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				"key-1": []byte("value-1"),
 				"key-2": []byte("value-2"),
 			},
-			expectedGetMultiCount: 2,
+			expectedGetMultiCount:  2,
+			expectedGateStartCount: 2,
 		},
 		"should return no hits on partial errors while fetching batches and no items found": {
 			maxBatchSize:         2,
@@ -330,9 +341,10 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				{Key: "key-2", Value: []byte("value-2")},
 				{Key: "key-3", Value: []byte("value-3")},
 			},
-			getKeys:               []string{"key-5", "key-6", "key-7"},
-			expectedHits:          map[string][]byte{},
-			expectedGetMultiCount: 2,
+			getKeys:                []string{"key-5", "key-6", "key-7"},
+			expectedHits:           map[string][]byte{},
+			expectedGetMultiCount:  2,
+			expectedGateStartCount: 2,
 		},
 		"should return no hits on all errors while fetching batches": {
 			maxBatchSize:         2,
@@ -343,9 +355,10 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 				{Key: "key-2", Value: []byte("value-2")},
 				{Key: "key-3", Value: []byte("value-3")},
 			},
-			getKeys:               []string{"key-5", "key-6", "key-7"},
-			expectedHits:          nil,
-			expectedGetMultiCount: 2,
+			getKeys:                []string{"key-5", "key-6", "key-7"},
+			expectedHits:           nil,
+			expectedGetMultiCount:  2,
+			expectedGateStartCount: 2,
 		},
 	}
 
@@ -364,6 +377,9 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 			testutil.Ok(t, err)
 			defer client.Stop()
 
+			// Replace the default gate with a counting version to allow checking the number of calls.
+			client.getMultiGate = newCountingGate(client.getMultiGate)
+
 			// Populate memcached with the initial items.
 			for _, item := range testData.initialItems {
 				testutil.Ok(t, client.SetAsync(ctx, item.Key, item.Value, time.Second))
@@ -379,6 +395,9 @@ func TestMemcachedClient_GetMulti(t *testing.T) {
 			backendMock.lock.Lock()
 			defer backendMock.lock.Unlock()
 			testutil.Equals(t, testData.expectedGetMultiCount, backendMock.getMultiCount)
+
+			// Ensure the client has interacted with the gate as expected.
+			testutil.Equals(t, uint32(testData.expectedGateStartCount), client.getMultiGate.(*countingGate).Count())
 
 			// Ensure metrics are tracked.
 			testutil.Equals(t, float64(testData.expectedGetMultiCount), prom_testutil.ToFloat64(client.operations.WithLabelValues(opGetMulti)))
@@ -450,6 +469,32 @@ func (c *memcachedClientBackendMock) waitItems(expected int) error {
 	}
 
 	return errors.New("timeout expired while waiting for items in the memcached mock")
+}
+
+// countingGate implements gate.Gate and counts the number of times Start is called
+type countingGate struct {
+	wrapped gate.Gate
+	count   *atomic.Uint32
+}
+
+func newCountingGate(g gate.Gate) gate.Gate {
+	return &countingGate{
+		wrapped: g,
+		count:   atomic.NewUint32(0),
+	}
+}
+
+func (c *countingGate) Start(ctx context.Context) error {
+	c.count.Inc()
+	return c.wrapped.Start(ctx)
+}
+
+func (c *countingGate) Done() {
+	c.wrapped.Done()
+}
+
+func (c *countingGate) Count() uint32 {
+	return c.count.Load()
 }
 
 func TestMultipleClientsCanUseSameRegistry(t *testing.T) {