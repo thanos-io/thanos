8e15037cb5e3150f50b2f651f1a07bc4344c1d43
Aditi Ahuja <ahuja.aditi@gmail.com>
2022-07-17 12:54:37 +0530

    minor refactor for OTLP
    Signed-off-by: Aditi Ahuja <ahuja.aditi@gmail.com>

--- pkg/tracing/otlp/otlp.go
+++ pkg/tracing/otlp/otlp.go
@@ -8,11 +8,9 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/pkg/errors"
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
@@ -56,14 +54,17 @@ func NewTracerProvider(ctx context.Context, logger log.Logger, conf []byte) (*tr
 		return nil, errors.New("otlp: invalid client type. Only 'http' and 'grpc' are accepted. ")
 	}
 
+	tp := newTraceProvider(exporter)
+
+	return tp, nil
+}
+
+func newTraceProvider(exporter *otlptrace.Exporter) *tracesdk.TracerProvider {
 	tp := tracesdk.NewTracerProvider(
 		tracesdk.WithBatcher(exporter),
 		tracesdk.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
 		)),
 	)
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.TraceContext{})
-
-	return tp, nil
+	return tp
 }
