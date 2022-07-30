f8977248d79313cc0492c77505f9306e95c36e3c
Ben Ye <ben.ye@bytedance.com>
2022-07-29 23:27:18 -0700

    rebase main
    Signed-off-by: Ben Ye <ben.ye@bytedance.com>

--- pkg/query/endpointset.go
+++ pkg/query/endpointset.go
@@ -246,9 +246,9 @@ type EndpointSet struct {
 
 	// Endpoint specifications can change dynamically. If some component is missing from the list, we assume it is no longer
 	// accessible and we close gRPC client for it, unless it is strict.
-	endpointSpec        func() []*GRPCEndpointSpec
-	dialOpts            []grpc.DialOption
-	endpointInfoTimeout time.Duration
+	endpointSpec             func() map[string]*GRPCEndpointSpec
+	dialOpts                 []grpc.DialOption
+	endpointInfoTimeout      time.Duration
 	unhealthyEndpointTimeout time.Duration
 
 	updateMtx sync.Mutex
@@ -293,16 +293,8 @@ func NewEndpointSet(
 		endpointsMetric: endpointsMetric,
 
 		dialOpts:                 dialOpts,
-<<<<<<< HEAD
-		gRPCInfoCallTimeout:      5 * time.Second,
-=======
-		endpointsMetric:          endpointsMetric,
 		endpointInfoTimeout:      endpointInfoTimeout,
-		endpoints:                make(map[string]*endpointRef),
-		endpointStatuses:         make(map[string]*EndpointStatus),
->>>>>>> 899a0020 (Expose Endpoint Info timeout parameter)
 		unhealthyEndpointTimeout: unhealthyEndpointTimeout,
-
 		endpointSpec: func() map[string]*GRPCEndpointSpec {
 			specs := make(map[string]*GRPCEndpointSpec)
 			for _, s := range endpointSpecs() {
@@ -335,7 +327,7 @@ func (e *EndpointSet) Update(ctx context.Context) {
 			wg.Add(1)
 			go func(spec *GRPCEndpointSpec) {
 				defer wg.Done()
-				ctx, cancel := context.WithTimeout(ctx, e.gRPCInfoCallTimeout)
+				ctx, cancel := context.WithTimeout(ctx, e.endpointInfoTimeout)
 				defer cancel()
 				e.updateEndpoint(ctx, spec, er)
 
@@ -350,7 +342,7 @@ func (e *EndpointSet) Update(ctx context.Context) {
 		wg.Add(1)
 		go func(spec *GRPCEndpointSpec) {
 			defer wg.Done()
-			ctx, cancel := context.WithTimeout(ctx, e.gRPCInfoCallTimeout)
+			ctx, cancel := context.WithTimeout(ctx, e.endpointInfoTimeout)
 			defer cancel()
 
 			newRef, err := e.newEndpointRef(ctx, spec)
