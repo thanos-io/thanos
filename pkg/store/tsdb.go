2f38f444df73b487bf3e507b187cacba13ce2204
Pedro Tanaka <pedro.tanaka@shopify.com>
2022-09-20 11:52:52 +0200

    calculating hash on TSDB Series() request as well
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>
    
    Final fix on error linting
    
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- pkg/store/tsdb.go
+++ pkg/store/tsdb.go
@@ -197,6 +197,7 @@ func (s *TSDBStore) Series(r *storepb.SeriesRequest, srv storepb.Store_SeriesSer
 				Raw: &storepb.Chunk{
 					Type: storepb.Chunk_Encoding(chk.Chunk.Encoding() - 1), // Proto chunk encoding is one off to TSDB one.
 					Data: chk.Chunk.Bytes(),
+					Hash: hashChunk(chk.Chunk.Bytes(), r.CalculateChunkChecksums),
 				},
 			}
 			frameBytesLeft -= c.Size()
