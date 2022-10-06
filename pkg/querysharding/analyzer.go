a61100c03ab0c922b9380f371af44af0c0378a70
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2022-10-06 20:03:24 +0530

    removed typos in comments in analyzer.go
    Signed-off-by: Rahul Kumar Jha <rahuljhakumar1600@gmail.com>

--- pkg/querysharding/analyzer.go
+++ pkg/querysharding/analyzer.go
@@ -79,8 +79,8 @@ func (a *CachedQueryAnalyzer) Analyze(query string) (QueryAnalysis, error) {
 //     used in grouping expressions. If non-empty, treat the query
 //     as shardable by those labels.
 //   - otherwise, treat the query as non-shardable.
-// The le label is excluded from sharding.
-
+//
+// The le label is excluded from sharding
 func (a *QueryAnalyzer) Analyze(query string) (QueryAnalysis, error) {
 
 	expr, err := parser.ParseExpr(query)
