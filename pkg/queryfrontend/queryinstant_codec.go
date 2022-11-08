9fc980a9432de7c3be92726759d9e72de085f608
Ben Ye <benye@amazon.com>
2022-11-07 16:46:54 -0800

    reuse slice samples method
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/queryfrontend/queryinstant_codec.go
+++ pkg/queryfrontend/queryinstant_codec.go
@@ -330,7 +330,7 @@ func matrixMerge(resps []*queryrange.PrometheusInstantQueryResponse) *queryrange
 					stream.Samples = stream.Samples[1:]
 				} else if existingEndTs > stream.Samples[0].TimestampMs {
 					// Overlap might be big, use heavier algorithm to remove overlap.
-					stream.Samples = sliceSamples(stream.Samples, existingEndTs)
+					stream.Samples = queryrange.SliceSamples(stream.Samples, existingEndTs)
 				} // else there is no overlap, yay!
 			}
 			existing.Samples = append(existing.Samples, stream.Samples...)
@@ -352,24 +352,4 @@ func matrixMerge(resps []*queryrange.PrometheusInstantQueryResponse) *queryrange
 	}
 
 	return result
-}
-
-// sliceSamples assumes given samples are sorted by timestamp in ascending order and
-// return a sub slice whose first element's is the smallest timestamp that is strictly
-// bigger than the given minTs. Empty slice is returned if minTs is bigger than all the
-// timestamps in samples.
-func sliceSamples(samples []cortexpb.Sample, minTs int64) []cortexpb.Sample {
-	if len(samples) <= 0 || minTs < samples[0].TimestampMs {
-		return samples
-	}
-
-	if len(samples) > 0 && minTs > samples[len(samples)-1].TimestampMs {
-		return samples[len(samples):]
-	}
-
-	searchResult := sort.Search(len(samples), func(i int) bool {
-		return samples[i].TimestampMs > minTs
-	})
-
-	return samples[searchResult:]
 }
