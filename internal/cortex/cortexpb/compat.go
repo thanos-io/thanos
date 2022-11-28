cfc5f1dd02b8552faaa6abec551e0fa597e4755e
Sebastian Rabenhorst <sebastian.rabenhorst@shopify.com>
2022-11-28 12:48:28 +0100

    Restored compat
    Signed-off-by: Sebastian Rabenhorst <sebastian.rabenhorst@shopify.com>

--- internal/cortex/cortexpb/compat.go
+++ internal/cortex/cortexpb/compat.go
@@ -4,12 +4,20 @@
 package cortexpb
 
 import (
+	stdjson "encoding/json"
+	"fmt"
+	"math"
 	"sort"
+	"strconv"
 	"strings"
+	"time"
 	"unsafe"
 
+	jsoniter "github.com/json-iterator/go"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/model/exemplar"
 	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/textparse"
 
 	"github.com/thanos-io/thanos/internal/cortex/util"
 )
@@ -24,6 +32,47 @@ func FromLabelAdaptersToLabels(ls []LabelAdapter) labels.Labels {
 	return *(*labels.Labels)(unsafe.Pointer(&ls))
 }
 
+// FromLabelAdaptersToLabelsWithCopy converts []LabelAdapter to labels.Labels.
+// Do NOT use unsafe to convert between data types because this function may
+// get in input labels whose data structure is reused.
+func FromLabelAdaptersToLabelsWithCopy(input []LabelAdapter) labels.Labels {
+	return CopyLabels(FromLabelAdaptersToLabels(input))
+}
+
+// Efficiently copies labels input slice. To be used in cases where input slice
+// can be reused, but long-term copy is needed.
+func CopyLabels(input []labels.Label) labels.Labels {
+	result := make(labels.Labels, len(input))
+
+	size := 0
+	for _, l := range input {
+		size += len(l.Name)
+		size += len(l.Value)
+	}
+
+	// Copy all strings into the buffer, and use 'yoloString' to convert buffer
+	// slices to strings.
+	buf := make([]byte, size)
+
+	for i, l := range input {
+		result[i].Name, buf = copyStringToBuffer(l.Name, buf)
+		result[i].Value, buf = copyStringToBuffer(l.Value, buf)
+	}
+	return result
+}
+
+// Copies string to buffer (which must be big enough), and converts buffer slice containing
+// the string copy into new string.
+func copyStringToBuffer(in string, buf []byte) (string, []byte) {
+	l := len(in)
+	c := copy(buf, in)
+	if c != l {
+		panic("not copied full string")
+	}
+
+	return yoloString(buf[0:l]), buf[l:]
+}
+
 // FromLabelsToLabelAdapters casts labels.Labels to []LabelAdapter.
 // It uses unsafe, but as LabelAdapter == labels.Label this should be safe.
 // This allows us to use labels.Labels directly in protos.
@@ -35,6 +84,12 @@ func FromLabelsToLabelAdapters(ls labels.Labels) []LabelAdapter {
 // Don't do this on any performance sensitive paths.
 func FromLabelAdaptersToMetric(ls []LabelAdapter) model.Metric {
 	return util.LabelsToMetric(FromLabelAdaptersToLabels(ls))
+}
+
+// FromLabelAdaptersToMetric converts []LabelAdapter to a model.Metric with copy.
+// Don't do this on any performance sensitive paths.
+func FromLabelAdaptersToMetricWithCopy(ls []LabelAdapter) model.Metric {
+	return util.LabelsToMetric(FromLabelAdaptersToLabelsWithCopy(ls))
 }
 
 // FromMetricsToLabelAdapters converts model.Metric to []LabelAdapter.
@@ -52,8 +107,151 @@ func FromMetricsToLabelAdapters(metric model.Metric) []LabelAdapter {
 	return result
 }
 
+func FromExemplarsToExemplarProtos(es []exemplar.Exemplar) []Exemplar {
+	result := make([]Exemplar, 0, len(es))
+	for _, e := range es {
+		result = append(result, Exemplar{
+			Labels:      FromLabelsToLabelAdapters(e.Labels),
+			Value:       e.Value,
+			TimestampMs: e.Ts,
+		})
+	}
+	return result
+}
+
+func FromExemplarProtosToExemplars(es []Exemplar) []exemplar.Exemplar {
+	result := make([]exemplar.Exemplar, 0, len(es))
+	for _, e := range es {
+		result = append(result, exemplar.Exemplar{
+			Labels: FromLabelAdaptersToLabels(e.Labels),
+			Value:  e.Value,
+			Ts:     e.TimestampMs,
+		})
+	}
+	return result
+}
+
 type byLabel []LabelAdapter
 
 func (s byLabel) Len() int           { return len(s) }
 func (s byLabel) Less(i, j int) bool { return strings.Compare(s[i].Name, s[j].Name) < 0 }
 func (s byLabel) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+// MetricMetadataMetricTypeToMetricType converts a metric type from our internal client
+// to a Prometheus one.
+func MetricMetadataMetricTypeToMetricType(mt MetricMetadata_MetricType) textparse.MetricType {
+	switch mt {
+	case UNKNOWN:
+		return textparse.MetricTypeUnknown
+	case COUNTER:
+		return textparse.MetricTypeCounter
+	case GAUGE:
+		return textparse.MetricTypeGauge
+	case HISTOGRAM:
+		return textparse.MetricTypeHistogram
+	case GAUGEHISTOGRAM:
+		return textparse.MetricTypeGaugeHistogram
+	case SUMMARY:
+		return textparse.MetricTypeSummary
+	case INFO:
+		return textparse.MetricTypeInfo
+	case STATESET:
+		return textparse.MetricTypeStateset
+	default:
+		return textparse.MetricTypeUnknown
+	}
+}
+
+// isTesting is only set from tests to get special behaviour to verify that custom sample encode and decode is used,
+// both when using jsonitor or standard json package.
+var isTesting = false
+
+// MarshalJSON implements json.Marshaler.
+func (s Sample) MarshalJSON() ([]byte, error) {
+	if isTesting && math.IsNaN(s.Value) {
+		return nil, fmt.Errorf("test sample")
+	}
+
+	t, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(model.Time(s.TimestampMs))
+	if err != nil {
+		return nil, err
+	}
+	v, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(model.SampleValue(s.Value))
+	if err != nil {
+		return nil, err
+	}
+	return []byte(fmt.Sprintf("[%s,%s]", t, v)), nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (s *Sample) UnmarshalJSON(b []byte) error {
+	var t model.Time
+	var v model.SampleValue
+	vs := [...]stdjson.Unmarshaler{&t, &v}
+	if err := jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal(b, &vs); err != nil {
+		return err
+	}
+	s.TimestampMs = int64(t)
+	s.Value = float64(v)
+
+	if isTesting && math.IsNaN(float64(v)) {
+		return fmt.Errorf("test sample")
+	}
+	return nil
+}
+
+func SampleJsoniterEncode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	sample := (*Sample)(ptr)
+
+	if isTesting && math.IsNaN(sample.Value) {
+		stream.Error = fmt.Errorf("test sample")
+		return
+	}
+
+	stream.WriteArrayStart()
+	stream.WriteFloat64(float64(sample.TimestampMs) / float64(time.Second/time.Millisecond))
+	stream.WriteMore()
+	stream.WriteString(model.SampleValue(sample.Value).String())
+	stream.WriteArrayEnd()
+}
+
+func SampleJsoniterDecode(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+	if !iter.ReadArray() {
+		iter.ReportError("cortexpb.Sample", "expected [")
+		return
+	}
+
+	t := model.Time(iter.ReadFloat64() * float64(time.Second/time.Millisecond))
+
+	if !iter.ReadArray() {
+		iter.ReportError("cortexpb.Sample", "expected ,")
+		return
+	}
+
+	bs := iter.ReadStringAsSlice()
+	ss := *(*string)(unsafe.Pointer(&bs))
+	v, err := strconv.ParseFloat(ss, 64)
+	if err != nil {
+		iter.ReportError("cortexpb.Sample", err.Error())
+		return
+	}
+
+	if isTesting && math.IsNaN(v) {
+		iter.Error = fmt.Errorf("test sample")
+		return
+	}
+
+	if iter.ReadArray() {
+		iter.ReportError("cortexpb.Sample", "expected ]")
+	}
+
+	*(*Sample)(ptr) = Sample{
+		TimestampMs: int64(t),
+		Value:       v,
+	}
+}
+
+func init() {
+	jsoniter.RegisterTypeEncoderFunc("cortexpb.Sample", SampleJsoniterEncode, func(unsafe.Pointer) bool { return false })
+	jsoniter.RegisterTypeDecoderFunc("cortexpb.Sample", SampleJsoniterDecode)
+}
