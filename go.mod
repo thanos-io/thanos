6db24f15e2cdb664e28b3ee25983af63412a951a
Sebastian Rabenhorst <sebastian@rabenhorst.me>
2022-12-12 13:51:44 +0100

    Started working on e2e tests
    Signed-off-by: Sebastian Rabenhorst <sebastian.rabenhorst@shopify.com>

--- go.mod
+++ go.mod
@@ -64,7 +64,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2
 	github.com/prometheus/alertmanager v0.24.0
-	github.com/prometheus/client_golang v1.13.1
+	github.com/prometheus/client_golang v1.14.0
 	github.com/prometheus/client_model v0.3.0
 	github.com/prometheus/common v0.37.0
 	github.com/prometheus/exporter-toolkit v0.8.1
--- test/e2e/query_test.go
+++ test/e2e/query_test.go
@@ -31,6 +31,7 @@ import (
 	"github.com/pkg/errors"
 	config_util "github.com/prometheus/common/config"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/model/histogram"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/prometheus/prometheus/prompb"
@@ -1699,4 +1700,71 @@ func TestConnectedQueriesWithLazyProxy(t *testing.T) {
 		return "sum(metric_that_does_not_exist)"
 	}, time.Now, promclient.QueryOptions{}, 0)
 
+}
+
+func TestQueryNativeHistograms(t *testing.T) {
+	e, err := e2e.NewDockerEnvironment("native-hist")
+	testutil.Ok(t, err)
+	t.Cleanup(e2ethanos.CleanScenario(t, e))
+
+	receiver := e2ethanos.NewReceiveBuilder(e, "1").WithIngestionEnabled().Init()
+	testutil.Ok(t, e2e.StartAndWaitReady(receiver))
+
+	prom1, sidecar1 := e2ethanos.NewPrometheusWithSidecar(e, "alone", e2ethanos.DefaultPromConfig("prom-alone", 0, "", "", e2ethanos.LocalPrometheusTarget), "", "quay.io/prometheus/prometheus:v2.40.5", "", "native-histograms", "remote-write-receiver")
+	testutil.Ok(t, e2e.StartAndWaitReady(prom1, sidecar1))
+
+	// Querier. Both fileSD and directly by flags.
+	querier := e2ethanos.NewQuerierBuilder(e, "1", sidecar1.InternalEndpoint("grpc"), receiver.InternalEndpoint("grpc")).Init()
+	testutil.Ok(t, e2e.StartAndWaitReady(querier))
+
+	remoteWriteURL, err := url.Parse("http://" + prom1.Endpoint("http") + "/api/v1/write")
+	testutil.Ok(t, err)
+
+	client, err := remote.NewWriteClient("remote-write-client", &remote.ClientConfig{
+		URL:     &config_util.URL{URL: remoteWriteURL},
+		Timeout: model.Duration(30 * time.Second),
+	})
+	testutil.Ok(t, err)
+
+	testHistogram := remote.HistogramToHistogramProto(time.Now().UnixMilli(), &histogram.Histogram{
+		Count:         5,
+		ZeroCount:     2,
+		Sum:           18.4,
+		ZeroThreshold: 1e-100,
+		Schema:        1,
+		PositiveSpans: []histogram.Span{
+			{Offset: 0, Length: 2},
+			{Offset: 1, Length: 2},
+		},
+		PositiveBuckets: []int64{1, 1, -1, 0}, // counts: 1, 2, 1, 1 (total 5)
+	})
+
+	samplespb := []prompb.TimeSeries{
+		{
+			Labels: []prompb.Label{
+				{Name: "__name__", Value: "test_histogram"},
+				{Name: "foo", Value: "bar"},
+			},
+			Histograms: []prompb.Histogram{
+				testHistogram,
+			},
+		},
+	}
+
+	sample := &prompb.WriteRequest{
+		Timeseries: samplespb,
+	}
+
+	var buf []byte
+	pBuf := proto.NewBuffer(nil)
+	err = pBuf.Marshal(sample)
+	testutil.Ok(t, err)
+
+	compressed := snappy.Encode(buf, pBuf.Bytes())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	t.Cleanup(cancel)
+
+	err = client.Store(ctx, compressed)
+	testutil.Ok(t, err)
 }
