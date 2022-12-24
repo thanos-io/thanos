19d190ee224a9c5f1456150d42f44b8325258fc8
Ben Ye <benye@amazon.com>
2022-12-23 23:03:14 -0800

    update alertmanager to latest release v0.25.0
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/httpconfig/http.go
+++ pkg/httpconfig/http.go
@@ -141,7 +141,7 @@ func NewRoundTripperFromConfig(cfg config_util.HTTPClientConfig, transportConfig
 			return nil, err
 		}
 
-		// If a authorization_credentials is provided, create a round tripper that will set the
+		// If an authorization_credentials is provided, create a round tripper that will set the
 		// Authorization header correctly on each request.
 		if cfg.Authorization != nil && len(cfg.Authorization.Credentials) > 0 {
 			rt = config_util.NewAuthorizationCredentialsRoundTripper(cfg.Authorization.Type, cfg.Authorization.Credentials, rt)
@@ -173,7 +173,7 @@ func NewRoundTripperFromConfig(cfg config_util.HTTPClientConfig, transportConfig
 		return newRT(tlsConfig)
 	}
 
-	return config_util.NewTLSRoundTripper(tlsConfig, cfg.TLSConfig.CAFile, newRT)
+	return config_util.NewTLSRoundTripper(tlsConfig, cfg.TLSConfig.CAFile, cfg.TLSConfig.CertFile, cfg.TLSConfig.KeyFile, newRT)
 }
 
 // NewHTTPClient returns a new HTTP client.
