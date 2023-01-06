52c638dc356e8590a6b8fa5002bdc94ccd05f75a
Sebastian Rabenhorst <sebastian.rabenhorst@shopify.com>
2023-01-06 21:19:35 +0100

    Removed commented cortex code
    Signed-off-by: Sebastian Rabenhorst <sebastian.rabenhorst@shopify.com>

--- internal/cortex/chunk/fixtures.go
+++ internal/cortex/chunk/fixtures.go
@@ -35,29 +35,6 @@ var BenchmarkLabels = labels.Labels{
 	{Name: "pod_name", Value: "some-other-name-5j8s8"},
 }
 
-// // DefaultSchemaConfig creates a simple schema config for testing
-// func DefaultSchemaConfig(store, schema string, from model.Time) SchemaConfig {
-// 	s := SchemaConfig{
-// 		Configs: []PeriodConfig{{
-// 			IndexType: store,
-// 			Schema:    schema,
-// 			From:      DayTime{from},
-// 			ChunkTables: PeriodicTableConfig{
-// 				Prefix: "cortex",
-// 				Period: 7 * 24 * time.Hour,
-// 			},
-// 			IndexTables: PeriodicTableConfig{
-// 				Prefix: "cortex_chunks",
-// 				Period: 7 * 24 * time.Hour,
-// 			},
-// 		}},
-// 	}
-// 	if err := s.Validate(); err != nil {
-// 		panic(err)
-// 	}
-// 	return s
-// }
-
 // ChunksToMatrix converts a set of chunks to a model.Matrix.
 func ChunksToMatrix(ctx context.Context, chunks []Chunk, from, through model.Time) (model.Matrix, error) {
 	// Group chunks by series, sort and dedupe samples.
