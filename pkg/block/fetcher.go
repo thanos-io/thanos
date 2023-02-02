84959bcfd923ea06a9fe931756f8445ac84c4ef8
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2023-02-02 04:05:31 +0100

    Compact: Remove spam of replica label removed log (#6088)
    * Remove spam of replica label removed log
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>
    
    * Reduce amount of removed replica label instead of removing it
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>
    
    * Reformat code
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- pkg/block/fetcher.go
+++ pkg/block/fetcher.go
@@ -709,6 +709,7 @@ func (r *ReplicaLabelRemover) Filter(_ context.Context, metas map[ulid.ULID]*met
 		return nil
 	}
 
+	countReplicaLabelRemoved := make(map[string]int, len(metas))
 	for u, meta := range metas {
 		l := make(map[string]string)
 		for n, v := range meta.Thanos.Labels {
@@ -717,8 +718,8 @@ func (r *ReplicaLabelRemover) Filter(_ context.Context, metas map[ulid.ULID]*met
 
 		for _, replicaLabel := range r.replicaLabels {
 			if _, exists := l[replicaLabel]; exists {
-				level.Debug(r.logger).Log("msg", "replica label removed", "label", replicaLabel)
 				delete(l, replicaLabel)
+				countReplicaLabelRemoved[replicaLabel] += 1
 				modified.WithLabelValues(replicaRemovedMeta).Inc()
 			}
 		}
@@ -730,6 +731,9 @@ func (r *ReplicaLabelRemover) Filter(_ context.Context, metas map[ulid.ULID]*met
 		nm := *meta
 		nm.Thanos.Labels = l
 		metas[u] = &nm
+	}
+	for replicaLabelRemoved, count := range countReplicaLabelRemoved {
+		level.Debug(r.logger).Log("msg", "removed replica label", "label", replicaLabelRemoved, "count", count)
 	}
 	return nil
 }
