96bdc7302870f15fa8682396da71e6ea7b3e3646
Filip Petkovski <filip.petkovsky@gmail.com>
2023-02-27 20:31:04 +0100

    Disable label names and label values APIs

---
+++ pkg/store/nop_label_values.go
@@ -0,0 +1,23 @@
+package store
+
+import (
+	"context"
+
+	"github.com/thanos-io/thanos/pkg/store/storepb"
+)
+
+type emptyLabelValues struct {
+	storepb.StoreServer
+}
+
+func NewEmptyLabelValuesStore(storeServer storepb.StoreServer) storepb.StoreServer {
+	return &emptyLabelValues{StoreServer: storeServer}
+}
+
+func (n *emptyLabelValues) LabelNames(context.Context, *storepb.LabelNamesRequest) (*storepb.LabelNamesResponse, error) {
+	return &storepb.LabelNamesResponse{}, nil
+}
+
+func (n *emptyLabelValues) LabelValues(context.Context, *storepb.LabelValuesRequest) (*storepb.LabelValuesResponse, error) {
+	return &storepb.LabelValuesResponse{}, nil
+}
