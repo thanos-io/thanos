f79fb5cda03dd6dc1c5693ad25fdccd52d688a42
Christopher Li <christopher.li@databricks.com>
2023-03-31 18:49:32 -0700

    Add query duration bucket in the metrics

--- internal/cortex/querier/queryrange/instrumentation.go
+++ internal/cortex/querier/queryrange/instrumentation.go
@@ -12,19 +12,24 @@ import (
 	"github.com/weaveworks/common/instrument"
 )
 
+const DAY = 24 * time.Hour
+const QUERYDURATIONBUCKET = "query_range_bucket"
+
 // InstrumentMiddleware can be inserted into the middleware chain to expose timing information.
 func InstrumentMiddleware(name string, metrics *InstrumentMiddlewareMetrics) Middleware {
-	var durationCol instrument.Collector
 
+	var durationCol instrument.Collector
 	// Support the case metrics shouldn't be tracked (ie. unit tests).
 	if metrics != nil {
-		durationCol = instrument.NewHistogramCollector(metrics.duration)
+		durationCol = NewDurationHistogramCollector(metrics.duration)
 	} else {
 		durationCol = &NoopCollector{}
 	}
 
 	return MiddlewareFunc(func(next Handler) Handler {
 		return HandlerFunc(func(ctx context.Context, req Request) (Response, error) {
+			queryRangeDurationBucket := getRangeBucket(req)
+			ctx = context.WithValue(ctx, QUERYDURATIONBUCKET, queryRangeDurationBucket)
 			var resp Response
 			err := instrument.CollectedRequest(ctx, name, durationCol, instrument.ErrorCode, func(ctx context.Context) error {
 				var err error
@@ -34,6 +39,32 @@ func InstrumentMiddleware(name string, metrics *InstrumentMiddlewareMetrics) Mid
 			return resp, err
 		})
 	})
+}
+
+func getRangeBucket(req Request) string {
+	queryRangeDuration := req.GetEnd() - req.GetStart()
+	var queryRangeDurationBucket string
+	switch {
+	case queryRangeDuration < 0:
+		return "Invalid"
+	case queryRangeDuration <= time.Hour.Milliseconds():
+		return "1h"
+	case queryRangeDuration <= 6*time.Hour.Milliseconds():
+		return "6h"
+	case queryRangeDuration <= 12*time.Hour.Milliseconds():
+		return "12h"
+	case queryRangeDuration <= DAY.Milliseconds():
+		return "1d"
+	case queryRangeDuration <= 2*DAY.Milliseconds():
+		return "2d"
+	case queryRangeDuration <= 7*DAY.Milliseconds():
+		return "7d"
+	case queryRangeDuration <= 30*DAY.Milliseconds():
+		return "30d"
+	default:
+		return "+INF"
+	}
+	return queryRangeDurationBucket
 }
 
 // InstrumentMiddlewareMetrics holds the metrics tracked by InstrumentMiddleware.
@@ -49,7 +80,7 @@ func NewInstrumentMiddlewareMetrics(registerer prometheus.Registerer) *Instrumen
 			Name:      "frontend_query_range_duration_seconds",
 			Help:      "Total time spent in seconds doing query range requests.",
 			Buckets:   prometheus.DefBuckets,
-		}, []string{"method", "status_code"}),
+		}, []string{"method", "status_code", QUERYDURATIONBUCKET}),
 	}
 }
 
@@ -65,3 +96,26 @@ func (c *NoopCollector) Before(ctx context.Context, method string, start time.Ti
 
 // After implements instrument.Collector.
 func (c *NoopCollector) After(ctx context.Context, method, statusCode string, start time.Time) {}
+
+// DurationHistogramCollector collects the duration of a request
+type DurationHistogramCollector struct {
+	metric *prometheus.HistogramVec
+}
+
+func (c *DurationHistogramCollector) Before(ctx context.Context, method string, start time.Time) {
+}
+
+func (c *DurationHistogramCollector) After(ctx context.Context, method, statusCode string, start time.Time) {
+	durationBucket, _ := ctx.Value(QUERYDURATIONBUCKET).(string)
+
+	if c.metric != nil {
+		instrument.ObserveWithExemplar(ctx, c.metric.WithLabelValues(method, statusCode, durationBucket), time.Since(start).Seconds())
+	}
+}
+func (c *DurationHistogramCollector) Register() {
+	prometheus.MustRegister(c.metric)
+}
+
+func NewDurationHistogramCollector(metric *prometheus.HistogramVec) *DurationHistogramCollector {
+	return &DurationHistogramCollector{metric}
+}
