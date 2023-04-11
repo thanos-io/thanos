9d05c09442a2b88fe58518751319c0e1bee4684f
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2023-04-11 14:43:01 +0300

    store/postings_codec: return both if possible
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- pkg/store/postings_codec.go
+++ pkg/store/postings_codec.go
@@ -91,15 +91,24 @@ type closeablePostings interface {
 	close()
 }
 
+// alias returns true if given slices have the same both backing array.
+// See: https://groups.google.com/g/golang-nuts/c/C6ufGl73Uzk.
+func alias(x, y []byte) bool {
+	return cap(x) > 0 && cap(y) > 0 && &x[0:cap(x)][cap(x)-1] == &y[0:cap(y)][cap(y)-1]
+}
+
 func diffVarintSnappyDecode(input []byte) (closeablePostings, error) {
 	if !isDiffVarintSnappyEncodedPostings(input) {
 		return nil, errors.New("header not found")
 	}
 
+	toFree := make([][]byte, 0, 2)
+
 	var dstBuf []byte
 	decodeBuf := snappyDecodePool.Get()
 	if decodeBuf != nil {
 		dstBuf = *(decodeBuf.(*[]byte))
+		toFree = append(toFree, dstBuf)
 	}
 
 	raw, err := s2.Decode(dstBuf, input[len(codecHeaderSnappy):])
@@ -107,30 +116,28 @@ func diffVarintSnappyDecode(input []byte) (closeablePostings, error) {
 		return nil, errors.Wrap(err, "snappy decode")
 	}
 
-	biggerSlice := raw
-	if cap(dstBuf) > cap(biggerSlice) {
-		biggerSlice = dstBuf
+	if !alias(raw, dstBuf) {
+		toFree = append(toFree, raw)
 	}
 
-	return newDiffVarintPostings(raw, biggerSlice), nil
+	return newDiffVarintPostings(raw, toFree), nil
 }
 
-func newDiffVarintPostings(input, freeSlice []byte) *diffVarintPostings {
-	return &diffVarintPostings{freeSlice: freeSlice, buf: &encoding.Decbuf{B: input}}
+func newDiffVarintPostings(input []byte, freeSlices [][]byte) *diffVarintPostings {
+	return &diffVarintPostings{freeSlices: freeSlices, buf: &encoding.Decbuf{B: input}}
 }
 
 // diffVarintPostings is an implementation of index.Postings based on diff+varint encoded data.
 type diffVarintPostings struct {
-	buf       *encoding.Decbuf
-	cur       storage.SeriesRef
-	freeSlice []byte
+	buf        *encoding.Decbuf
+	cur        storage.SeriesRef
+	freeSlices [][]byte
 }
 
 func (it *diffVarintPostings) close() {
-	if it.freeSlice == nil {
-		return
+	for i := range it.freeSlices {
+		snappyDecodePool.Put(&it.freeSlices[i])
 	}
-	snappyDecodePool.Put(&it.freeSlice)
 }
 
 func (it *diffVarintPostings) At() storage.SeriesRef {
