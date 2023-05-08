54230cae7e4427a2a8a8ea80b295faf6b3159684
Pradyumna Krishna <git@onpy.in>
2023-05-08 14:53:15 +0530

    Return Query Explaination in QueryAPI
    A param `explain` is added to QueryAPI, if true then explanation
    returned by the `Explain()` method of the query having structure
    `ExplainOutputNode` is returned in response.
    Query Explanation is added under new field in response that is
    `thanosInfo`.
    
    Signed-off-by: Pradyumna Krishna <git@onpy.in>

--- pkg/api/query/v1.go
+++ pkg/api/query/v1.go
@@ -76,6 +76,7 @@ const (
 	ShardInfoParam           = "shard_info"
 	LookbackDeltaParam       = "lookback_delta"
 	EngineParam              = "engine"
+	QueryExplainParam        = "explain"
 )
 
 type PromqlEngineType string
@@ -267,12 +268,17 @@ func (qapi *QueryAPI) Register(r *route.Router, tracer opentracing.Tracer, logge
 	r.Post("/query_exemplars", instr("exemplars", NewExemplarsHandler(qapi.exemplars, qapi.enableExemplarPartialResponse)))
 }
 
+type thanosInfo struct {
+	Explanation engine.ExplainOutputNode `json:"explanation,omitempty"`
+}
+
 type queryData struct {
 	ResultType parser.ValueType `json:"resultType"`
 	Result     parser.Value     `json:"result"`
 	Stats      stats.QueryStats `json:"stats,omitempty"`
 	// Additional Thanos Response field.
-	Warnings []error `json:"warnings,omitempty"`
+	ThanosInfo thanosInfo `json:"thanosInfo,omitempty"`
+	Warnings   []error    `json:"warnings,omitempty"`
 }
 
 func (qapi *QueryAPI) parseEnableDedupParam(r *http.Request) (enableDeduplication bool, _ *api.ApiError) {
@@ -419,6 +425,27 @@ func (qapi *QueryAPI) parseShardInfo(r *http.Request) (*storepb.ShardInfo, *api.
 	return &info, nil
 }
 
+func (qapi *QueryAPI) parseQueryExplainParam(r *http.Request, query promql.Query) (engine.ExplainOutputNode, *api.ApiError) {
+	var explanation engine.ExplainOutputNode
+
+	if val := r.FormValue(QueryExplainParam); val != "" {
+		var err error
+		enableExplanation, err := strconv.ParseBool(val)
+		if err != nil {
+			return explanation, &api.ApiError{Typ: api.ErrorBadData, Err: errors.Wrapf(err, "'%s' parameter", QueryExplainParam)}
+		}
+		if enableExplanation {
+			if eq, ok := query.(engine.ExplainableQuery); ok {
+				explanation = *eq.Explain()
+			} else {
+				return explanation, &api.ApiError{Typ: api.ErrorBadData, Err: errors.Errorf("Query not explainable")}
+			}
+		}
+	}
+
+	return explanation, nil
+}
+
 func (qapi *QueryAPI) query(r *http.Request) (interface{}, []error, *api.ApiError, func()) {
 	ts, err := parseTimeParam(r, "time", qapi.baseAPI.Now())
 	if err != nil {
@@ -509,6 +536,11 @@ func (qapi *QueryAPI) query(r *http.Request) (interface{}, []error, *api.ApiErro
 		return nil, nil, &api.ApiError{Typ: api.ErrorBadData, Err: err}, func() {}
 	}
 
+	explanation, apiErr := qapi.parseQueryExplainParam(r, qry)
+	if apiErr != nil {
+		return nil, nil, apiErr, func() {}
+	}
+
 	tracing.DoInSpan(ctx, "query_gate_ismyturn", func(ctx context.Context) {
 		err = qapi.gate.Start(ctx)
 	})
@@ -544,6 +576,9 @@ func (qapi *QueryAPI) query(r *http.Request) (interface{}, []error, *api.ApiErro
 		ResultType: res.Value.Type(),
 		Result:     res.Value,
 		Stats:      qs,
+		ThanosInfo: thanosInfo{
+			Explanation: explanation,
+		},
 	}, res.Warnings, nil, qry.Close
 }
 
@@ -667,6 +702,11 @@ func (qapi *QueryAPI) queryRange(r *http.Request) (interface{}, []error, *api.Ap
 		return nil, nil, &api.ApiError{Typ: api.ErrorBadData, Err: err}, func() {}
 	}
 
+	explanation, apiErr := qapi.parseQueryExplainParam(r, qry)
+	if apiErr != nil {
+		return nil, nil, apiErr, func() {}
+	}
+
 	tracing.DoInSpan(ctx, "query_gate_ismyturn", func(ctx context.Context) {
 		err = qapi.gate.Start(ctx)
 	})
@@ -700,6 +740,9 @@ func (qapi *QueryAPI) queryRange(r *http.Request) (interface{}, []error, *api.Ap
 		ResultType: res.Value.Type(),
 		Result:     res.Value,
 		Stats:      qs,
+		ThanosInfo: thanosInfo{
+			Explanation: explanation,
+		},
 	}, res.Warnings, nil, qry.Close
 }
 
