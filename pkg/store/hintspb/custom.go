003f880917df01040265e0e7828fe6a39a02d8d4
Ben Ye <benye@amazon.com>
2023-05-10 12:03:59 -0700

    add merge method
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/store/hintspb/custom.go
+++ pkg/store/hintspb/custom.go
@@ -22,3 +22,41 @@ func (m *LabelValuesResponseHints) AddQueriedBlock(id ulid.ULID) {
 		Id: id.String(),
 	})
 }
+
+func (m *QueryStats) Merge(other *QueryStats) {
+	m.BlocksQueried += other.BlocksQueried
+	m.MergeDuration += other.MergeDuration
+	m.GetAllDuration += other.GetAllDuration
+	m.MergedSeriesCount += other.MergedSeriesCount
+	m.MergedChunksCount += other.MergedChunksCount
+
+	m.CachedPostingsCompressions += other.CachedPostingsCompressions
+	m.CachedPostingsCompressionErrors += other.CachedPostingsCompressionErrors
+	m.CachedPostingsCompressedSizeSum += other.CachedPostingsCompressedSizeSum
+	m.CachedPostingsCompressionTimeSum += other.CachedPostingsCompressionTimeSum
+	m.CachedPostingsDecompressions += other.CachedPostingsDecompressions
+	m.CachedPostingsDecompressionErrors += other.CachedPostingsDecompressionErrors
+	m.CachedPostingsDecompressionTimeSum += other.CachedPostingsDecompressionTimeSum
+
+	m.PostingsFetched += other.PostingsFetched
+	m.PostingsToFetch += other.PostingsToFetch
+	m.PostingsFetchCount += other.PostingsFetchCount
+	m.PostingsFetchedSizeSum += other.PostingsFetchedSizeSum
+	m.PostingsFetchDurationSum += other.PostingsFetchDurationSum
+	m.PostingsTouched += other.PostingsTouched
+	m.PostingsTouchedSizeSum += other.PostingsTouchedSizeSum
+
+	m.SeriesFetched += other.SeriesFetched
+	m.SeriesFetchCount += other.SeriesFetchCount
+	m.SeriesFetchedSizeSum += m.SeriesFetchedSizeSum
+	m.SeriesFetchDurationSum += m.SeriesFetchDurationSum
+	m.SeriesTouched += other.SeriesTouched
+	m.SeriesTouchedSizeSum += other.SeriesTouchedSizeSum
+
+	m.ChunksFetched += other.ChunksFetched
+	m.ChunksFetchCount += other.ChunksFetchCount
+	m.ChunksFetchedSizeSum += other.ChunksFetchedSizeSum
+	m.ChunksFetchDurationSum += other.ChunksFetchDurationSum
+	m.ChunksTouched += other.ChunksTouched
+	m.ChunksTouchedSizeSum += other.ChunksTouchedSizeSum
+}
