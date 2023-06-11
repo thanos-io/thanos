8f7f2bf7c307eb53d0172c42c5c7d65ed8ba08bc
Eng Zer Jun <engzerjun@gmail.com>
2023-06-11 12:26:35 +0800

    chore: remove duplicated `gopkg.in/fsnotify.v1` dep (#6432)
    * chore: remove duplicated `gopkg.in/fsnotify.v1` dep
    
    `github.com/fsnotify/fsnotify` and `gopkg.in/fsnotify.v1` are the same
    dependency. We can keep `github.com/fsnotify/fsnotify` and remove
    `gopkg.in/fsnotify.v1`.
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>
    
    * docs: add changelog
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>
    
    ---------
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- pkg/receive/config.go
+++ pkg/receive/config.go
@@ -13,13 +13,13 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/common/model"
-	"gopkg.in/fsnotify.v1"
 )
 
 var (
