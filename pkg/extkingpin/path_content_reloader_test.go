097229ed89f93481d538618bc796b3fc45a92319
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2023-07-03 19:50:54 +0200

    Fix table tests
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- pkg/extkingpin/path_content_reloader_test.go
+++ pkg/extkingpin/path_content_reloader_test.go
@@ -81,6 +81,7 @@ func TestPathContentReloader(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			testFile := path.Join(t.TempDir(), "test")
