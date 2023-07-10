f804a761fb0bbb82b0fc468269706355949f0d92
nishchay-veer <nishchayveer19@gmail.com>
2023-07-10 12:30:09 +0530

    Return Query Analysis in API
    A param  is added to QueryAPI, if true then query analysis is
    returned by the  method of the query having structure
     is returned in response.
    
    Signed-off-by: nishchay-veer <nishchayveer19@gmail.com>

--- pkg/api/query/v1.go
+++ pkg/api/query/v1.go
@@ -78,6 +78,7 @@ const (
 	LookbackDeltaParam       = "lookback_delta"
 	EngineParam              = "engine"
 	QueryExplainParam        = "explain"
+	QueryAnalyzeParam        = "analyze"
 )
 
 type PromqlEngineType string
@@ -285,6 +286,7 @@ type queryData struct {
 	Stats      stats.QueryStats `json:"stats,omitempty"`
 	// Additional Thanos Response field.
 	QueryExplanation *engine.ExplainOutputNode `json:"explanation,omitempty"`
+	QueryAnalysis    *engine.AnalyzeOutputNode `json:"analysis,omitempty"`
 	Warnings         []error                   `json:"warnings,omitempty"`
 }
 
@@ -452,6 +454,26 @@ func (qapi *QueryAPI) parseQueryExplainParam(r *http.Request, query promql.Query
 
 	return explanation, nil
 }
+func (qapi *QueryAPI) parseQueryAnalyzeParam(r *http.Request, query promql.Query) (*engine.AnalyzeOutputNode, *api.ApiError) {
+	var analysis *engine.AnalyzeOutputNode
+
+	if val := r.FormValue(QueryAnalyzeParam); val != "" {
+		var err error
+		enableAnalysis, err := strconv.ParseBool(val)
+		if err != nil {
+			return analysis, &api.ApiError{Typ: api.ErrorBadData, Err: errors.Wrapf(err, "'%s' parameter", QueryAnalyzeParam)}
+		}
+		if enableAnalysis {
+			if eq, ok := query.(engine.ExplainableQuery); ok {
+				analysis = eq.Analyze()
+			} else {
+				return analysis, &api.ApiError{Typ: api.ErrorBadData, Err: errors.Errorf("Query not analyzable")}
+			}
+		}
+	}
+
+	return analysis, nil
+}
 
 func (qapi *QueryAPI) query(r *http.Request) (interface{}, []error, *api.ApiError, func()) {
 	ts, err := parseTimeParam(r, "time", qapi.baseAPI.Now())
@@ -555,6 +577,11 @@ func (qapi *QueryAPI) query(r *http.Request) (interface{}, []error, *api.ApiErro
 		return nil, nil, apiErr, func() {}
 	}
 
+	analysis, apiErr := qapi.parseQueryAnalyzeParam(r, qry)
+	if apiErr != nil {
+		return nil, nil, apiErr, func() {}
+	}
+
 	tracing.DoInSpan(ctx, "query_gate_ismyturn", func(ctx context.Context) {
 		err = qapi.gate.Start(ctx)
 	})
@@ -591,6 +618,7 @@ func (qapi *QueryAPI) query(r *http.Request) (interface{}, []error, *api.ApiErro
 		Result:           res.Value,
 		Stats:            qs,
 		QueryExplanation: explanation,
+		QueryAnalysis:    analysis,
 	}, res.Warnings, nil, qry.Close
 }
 
@@ -726,6 +754,11 @@ func (qapi *QueryAPI) queryRange(r *http.Request) (interface{}, []error, *api.Ap
 		return nil, nil, apiErr, func() {}
 	}
 
+	analysis, apiErr := qapi.parseQueryAnalyzeParam(r, qry)
+	if apiErr != nil {
+		return nil, nil, apiErr, func() {}
+	}
+
 	tracing.DoInSpan(ctx, "query_gate_ismyturn", func(ctx context.Context) {
 		err = qapi.gate.Start(ctx)
 	})
@@ -760,6 +793,7 @@ func (qapi *QueryAPI) queryRange(r *http.Request) (interface{}, []error, *api.Ap
 		Result:           res.Value,
 		Stats:            qs,
 		QueryExplanation: explanation,
+		QueryAnalysis:    analysis,
 	}, res.Warnings, nil, qry.Close
 }
 
