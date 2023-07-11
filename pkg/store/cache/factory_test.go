025e8217c058d22a0acaa6c3085efa24b31d511b
Alan Protasio <alanprot@gmail.com>
2023-07-11 13:47:51 -0700

    lint
    Signed-off-by: Alan Protasio <alanprot@gmail.com>

--- pkg/store/cache/factory_test.go
+++ pkg/store/cache/factory_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) The Thanos Authors.
+// Licensed under the Apache License 2.0.
+
 package storecache
 
 import (
@@ -19,6 +22,8 @@ func TestIndexCacheMetrics(t *testing.T) {
 max_size: 10MB
 max_item_size: 1MB
 `)
+	// Make sure that the in memory cache does not register the same metrics of the remote index cache.
+	// If so, we should move those metrics to the `commonMetrics`
 	_, err = NewInMemoryIndexCache(log.NewNopLogger(), commonMetrics, reg, conf)
 	testutil.Ok(t, err)
 }
