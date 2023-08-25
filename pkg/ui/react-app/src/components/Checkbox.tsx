d840f8f6086db0a5880b9c4f1bfcec958dad7c24
lmarques03 <henriquecondado@gmail.com>
2023-08-25 23:05:55 +0100

    reverted htmlFor
    Signed-off-by: Luis Marques <henriquecondado@gmail.com>

--- pkg/ui/react-app/src/components/Checkbox.tsx
+++ pkg/ui/react-app/src/components/Checkbox.tsx
@@ -10,7 +10,7 @@ const Checkbox: FC<CheckboxProps> = ({ children, wrapperStyles, id, disabled, is
   return (
     <FormGroup className="custom-control custom-checkbox" style={wrapperStyles}>
       <Input {...rest} id={id} type="checkbox" className="custom-control-input" disabled={disabled} />
-      <Label style={{ userSelect: 'none' }} className="custom-control-label" htmlFor={id}>
+      <Label style={{ userSelect: 'none' }} className="custom-control-label" for={id}>
         {children}
         {isExplainCheckbox && disabled && (
           <div className="popup-message">This functionality is only available when using the Thanos engine</div>
