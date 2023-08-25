9d8756081a1f6c1b4618d633ed73100c1886927b
lmarques03 <henriquecondado@gmail.com>
2023-08-25 22:51:14 +0100

    Solved infinite loop caused by useState function
    Signed-off-by: Luis Marques <henriquecondado@gmail.com>

--- pkg/ui/react-app/src/components/Checkbox.tsx
+++ pkg/ui/react-app/src/components/Checkbox.tsx
@@ -7,24 +7,15 @@ interface CheckboxProps extends InputProps {
 }
 
 const Checkbox: FC<CheckboxProps> = ({ children, wrapperStyles, id, disabled, isExplainCheckbox, ...rest }) => {
-  const [showMessage, setShowMessage] = useState(false);
-
-  const handleMouseEnter = () => {
-    if (isExplainCheckbox && disabled) {
-      setShowMessage(true);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    setShowMessage(false);
-  };
   return (
-    <FormGroup className="custom-control custom-checkbox" style={wrapperStyles} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <FormGroup className="custom-control custom-checkbox" style={wrapperStyles}>
       <Input {...rest} id={id} type="checkbox" className="custom-control-input" disabled={disabled} />
-      <Label style={{ userSelect: 'none' }} className="custom-control-label" for={id}>
+      <Label style={{ userSelect: 'none' }} className="custom-control-label" htmlFor={id}>
         {children}
+        {isExplainCheckbox && disabled && (
+          <div className="popup-message">This functionality is only available when using the Thanos engine</div>
+        )}
       </Label>
-      {isExplainCheckbox && showMessage && <div className="popup-message">This functionality is only available when using the Thanos engine</div>}
     </FormGroup>
   );
 };
