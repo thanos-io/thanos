68bef3fce0812b01e7970fd1d90c36dbb42f562b
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2023-11-19 05:17:32 +0200

    compact: hook nodownsamplemarkfilter into filters chain (#6893)
    We have a NoDownsampleMarkFilter that we were not using before in the
    compactor for some reason. Hook it into the filters chain if
    downsampling is enabled and then trim matching ULIDs from the
    downsampling process. Add a test to cover this scenario.
    
    Fixes https://github.com/thanos-io/thanos/issues/6179.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- cmd/thanos/downsample.go
+++ cmd/thanos/downsample.go
@@ -92,7 +92,7 @@ func RunDownsample(
 	// While fetching blocks, filter out blocks that were marked for no downsample.
 	metaFetcher, err := block.NewMetaFetcher(logger, block.FetcherConcurrency, insBkt, "", extprom.WrapRegistererWithPrefix("thanos_", reg), []block.MetadataFilter{
 		block.NewDeduplicateFilter(block.FetcherConcurrency),
-		downsample.NewGatherNoDownsampleMarkFilter(logger, insBkt),
+		downsample.NewGatherNoDownsampleMarkFilter(logger, insBkt, block.FetcherConcurrency),
 	})
 	if err != nil {
 		return errors.Wrap(err, "create meta fetcher")
