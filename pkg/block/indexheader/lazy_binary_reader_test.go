0b62099a5a6fb68493db567a521a17c1dd35cbc0
Ben Ye <benye@amazon.com>
2023-12-16 19:30:55 +0000

    address comments
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/block/indexheader/lazy_binary_reader_test.go
+++ pkg/block/indexheader/lazy_binary_reader_test.go
@@ -5,6 +5,7 @@ package indexheader
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
@@ -56,45 +57,47 @@ func TestNewLazyBinaryReader_ShouldBuildIndexHeaderFromBucket(t *testing.T) {
 	testutil.Ok(t, err)
 	defer func() { testutil.Ok(t, bkt.Close()) }()
 
-	// Create block.
-	blockID, err := e2eutil.CreateBlock(ctx, tmpDir, []labels.Labels{
-		{{Name: "a", Value: "1"}},
-		{{Name: "a", Value: "2"}},
-	}, 100, 0, 1000, labels.Labels{{Name: "ext1", Value: "1"}}, 124, metadata.NoneFunc)
-	testutil.Ok(t, err)
-	testutil.Ok(t, block.Upload(ctx, log.NewNopLogger(), bkt, filepath.Join(tmpDir, blockID.String()), metadata.NoneFunc))
-
-	for _, lazyDownload := range []bool{true} {
-		m := NewLazyBinaryReaderMetrics(nil)
-		bm := NewBinaryReaderMetrics(nil)
-		r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
-		testutil.Ok(t, err)
-		testutil.Assert(t, r.reader == nil)
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
-
-		_, err = os.Stat(filepath.Join(r.dir, blockID.String(), block.IndexHeaderFilename))
-		// Index file shouldn't exist.
-		if lazyDownload {
-			testutil.Equals(t, true, os.IsNotExist(err))
-		}
-		// Should lazy load the index upon first usage.
-		v, err := r.IndexVersion()
-		testutil.Ok(t, err)
-		if lazyDownload {
-			_, err = os.Stat(filepath.Join(r.dir, blockID.String(), block.IndexHeaderFilename))
+	for _, lazyDownload := range []bool{false, true} {
+		t.Run(fmt.Sprintf("lazyDownload=%v", lazyDownload), func(t *testing.T) {
+			// Create block.
+			blockID, err := e2eutil.CreateBlock(ctx, tmpDir, []labels.Labels{
+				{{Name: "a", Value: "1"}},
+				{{Name: "a", Value: "2"}},
+			}, 100, 0, 1000, labels.Labels{{Name: "ext1", Value: "1"}}, 124, metadata.NoneFunc)
 			testutil.Ok(t, err)
-		}
-		testutil.Equals(t, 2, v)
-		testutil.Assert(t, r.reader != nil)
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+			testutil.Ok(t, block.Upload(ctx, log.NewNopLogger(), bkt, filepath.Join(tmpDir, blockID.String()), metadata.NoneFunc))
 
-		labelNames, err := r.LabelNames()
-		testutil.Ok(t, err)
-		testutil.Equals(t, []string{"a"}, labelNames)
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+			m := NewLazyBinaryReaderMetrics(nil)
+			bm := NewBinaryReaderMetrics(nil)
+			r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
+			testutil.Ok(t, err)
+			testutil.Assert(t, r.reader == nil)
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+
+			_, err = os.Stat(filepath.Join(r.dir, blockID.String(), block.IndexHeaderFilename))
+			// Index file shouldn't exist.
+			if lazyDownload {
+				testutil.Equals(t, true, os.IsNotExist(err))
+			}
+			// Should lazy load the index upon first usage.
+			v, err := r.IndexVersion()
+			testutil.Ok(t, err)
+			if lazyDownload {
+				_, err = os.Stat(filepath.Join(r.dir, blockID.String(), block.IndexHeaderFilename))
+				testutil.Ok(t, err)
+			}
+			testutil.Equals(t, 2, v)
+			testutil.Assert(t, r.reader != nil)
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+
+			labelNames, err := r.LabelNames()
+			testutil.Ok(t, err)
+			testutil.Equals(t, []string{"a"}, labelNames)
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+		})
 	}
 }
 
@@ -120,22 +123,24 @@ func TestNewLazyBinaryReader_ShouldRebuildCorruptedIndexHeader(t *testing.T) {
 	testutil.Ok(t, os.WriteFile(headerFilename, []byte("xxx"), os.ModePerm))
 
 	for _, lazyDownload := range []bool{false, true} {
-		m := NewLazyBinaryReaderMetrics(nil)
-		bm := NewBinaryReaderMetrics(nil)
-		r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
-		testutil.Ok(t, err)
-		testutil.Assert(t, r.reader == nil)
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+		t.Run(fmt.Sprintf("lazyDownload=%v", lazyDownload), func(t *testing.T) {
+			m := NewLazyBinaryReaderMetrics(nil)
+			bm := NewBinaryReaderMetrics(nil)
+			r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
+			testutil.Ok(t, err)
+			testutil.Assert(t, r.reader == nil)
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
 
-		// Ensure it can read data.
-		labelNames, err := r.LabelNames()
-		testutil.Ok(t, err)
-		testutil.Equals(t, []string{"a"}, labelNames)
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+			// Ensure it can read data.
+			labelNames, err := r.LabelNames()
+			testutil.Ok(t, err)
+			testutil.Equals(t, []string{"a"}, labelNames)
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+		})
 	}
 }
 
@@ -157,38 +162,40 @@ func TestLazyBinaryReader_ShouldReopenOnUsageAfterClose(t *testing.T) {
 	testutil.Ok(t, block.Upload(ctx, log.NewNopLogger(), bkt, filepath.Join(tmpDir, blockID.String()), metadata.NoneFunc))
 
 	for _, lazyDownload := range []bool{false, true} {
-		m := NewLazyBinaryReaderMetrics(nil)
-		bm := NewBinaryReaderMetrics(nil)
-		r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
-		testutil.Ok(t, err)
-		testutil.Assert(t, r.reader == nil)
+		t.Run(fmt.Sprintf("lazyDownload=%v", lazyDownload), func(t *testing.T) {
+			m := NewLazyBinaryReaderMetrics(nil)
+			bm := NewBinaryReaderMetrics(nil)
+			r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
+			testutil.Ok(t, err)
+			testutil.Assert(t, r.reader == nil)
 
-		// Should lazy load the index upon first usage.
-		labelNames, err := r.LabelNames()
-		testutil.Ok(t, err)
-		testutil.Equals(t, []string{"a"}, labelNames)
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+			// Should lazy load the index upon first usage.
+			labelNames, err := r.LabelNames()
+			testutil.Ok(t, err)
+			testutil.Equals(t, []string{"a"}, labelNames)
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
 
-		// Close it.
-		testutil.Ok(t, r.Close())
-		testutil.Assert(t, r.reader == nil)
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.unloadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
-
-		// Should lazy load again upon next usage.
-		labelNames, err = r.LabelNames()
-		testutil.Ok(t, err)
-		testutil.Equals(t, []string{"a"}, labelNames)
-		testutil.Equals(t, float64(2), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
-
-		// Closing an already closed lazy reader should be a no-op.
-		for i := 0; i < 2; i++ {
+			// Close it.
 			testutil.Ok(t, r.Close())
-			testutil.Equals(t, float64(2), promtestutil.ToFloat64(m.unloadCount))
+			testutil.Assert(t, r.reader == nil)
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.unloadCount))
 			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
-		}
+
+			// Should lazy load again upon next usage.
+			labelNames, err = r.LabelNames()
+			testutil.Ok(t, err)
+			testutil.Equals(t, []string{"a"}, labelNames)
+			testutil.Equals(t, float64(2), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+
+			// Closing an already closed lazy reader should be a no-op.
+			for i := 0; i < 2; i++ {
+				testutil.Ok(t, r.Close())
+				testutil.Equals(t, float64(2), promtestutil.ToFloat64(m.unloadCount))
+				testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
+			}
+		})
 	}
 }
 
@@ -210,34 +217,36 @@ func TestLazyBinaryReader_unload_ShouldReturnErrorIfNotIdle(t *testing.T) {
 	testutil.Ok(t, block.Upload(ctx, log.NewNopLogger(), bkt, filepath.Join(tmpDir, blockID.String()), metadata.NoneFunc))
 
 	for _, lazyDownload := range []bool{false, true} {
-		m := NewLazyBinaryReaderMetrics(nil)
-		bm := NewBinaryReaderMetrics(nil)
-		r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
-		testutil.Ok(t, err)
-		testutil.Assert(t, r.reader == nil)
+		t.Run(fmt.Sprintf("lazyDownload=%v", lazyDownload), func(t *testing.T) {
+			m := NewLazyBinaryReaderMetrics(nil)
+			bm := NewBinaryReaderMetrics(nil)
+			r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
+			testutil.Ok(t, err)
+			testutil.Assert(t, r.reader == nil)
 
-		// Should lazy load the index upon first usage.
-		labelNames, err := r.LabelNames()
-		testutil.Ok(t, err)
-		testutil.Equals(t, []string{"a"}, labelNames)
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
+			// Should lazy load the index upon first usage.
+			labelNames, err := r.LabelNames()
+			testutil.Ok(t, err)
+			testutil.Equals(t, []string{"a"}, labelNames)
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
 
-		// Try to unload but not idle since enough time.
-		testutil.Equals(t, errNotIdle, r.unloadIfIdleSince(time.Now().Add(-time.Minute).UnixNano()))
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
+			// Try to unload but not idle since enough time.
+			testutil.Equals(t, errNotIdle, r.unloadIfIdleSince(time.Now().Add(-time.Minute).UnixNano()))
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
 
-		// Try to unload and idle since enough time.
-		testutil.Ok(t, r.unloadIfIdleSince(time.Now().UnixNano()))
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
-		testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.unloadCount))
-		testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
+			// Try to unload and idle since enough time.
+			testutil.Ok(t, r.unloadIfIdleSince(time.Now().UnixNano()))
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.loadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.loadFailedCount))
+			testutil.Equals(t, float64(1), promtestutil.ToFloat64(m.unloadCount))
+			testutil.Equals(t, float64(0), promtestutil.ToFloat64(m.unloadFailedCount))
+		})
 	}
 }
 
@@ -262,50 +271,52 @@ func TestLazyBinaryReader_LoadUnloadRaceCondition(t *testing.T) {
 	testutil.Ok(t, block.Upload(ctx, log.NewNopLogger(), bkt, filepath.Join(tmpDir, blockID.String()), metadata.NoneFunc))
 
 	for _, lazyDownload := range []bool{false, true} {
-		m := NewLazyBinaryReaderMetrics(nil)
-		bm := NewBinaryReaderMetrics(nil)
-		r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
-		testutil.Ok(t, err)
-		testutil.Assert(t, r.reader == nil)
-		t.Cleanup(func() {
-			testutil.Ok(t, r.Close())
+		t.Run(fmt.Sprintf("lazyDownload=%v", lazyDownload), func(t *testing.T) {
+			m := NewLazyBinaryReaderMetrics(nil)
+			bm := NewBinaryReaderMetrics(nil)
+			r, err := NewLazyBinaryReader(ctx, log.NewNopLogger(), bkt, tmpDir, blockID, 3, m, bm, nil, lazyDownload)
+			testutil.Ok(t, err)
+			testutil.Assert(t, r.reader == nil)
+			t.Cleanup(func() {
+				testutil.Ok(t, r.Close())
+			})
+
+			done := make(chan struct{})
+			time.AfterFunc(runDuration, func() { close(done) })
+			wg := sync.WaitGroup{}
+			wg.Add(2)
+
+			// Start a goroutine which continuously try to unload the reader.
+			go func() {
+				defer wg.Done()
+
+				for {
+					select {
+					case <-done:
+						return
+					default:
+						testutil.Ok(t, r.unloadIfIdleSince(0))
+					}
+				}
+			}()
+
+			// Try to read multiple times, while the other goroutine continuously try to unload it.
+			go func() {
+				defer wg.Done()
+
+				for {
+					select {
+					case <-done:
+						return
+					default:
+						_, err := r.PostingsOffset("a", "1")
+						testutil.Assert(t, err == nil || err == errUnloadedWhileLoading)
+					}
+				}
+			}()
+
+			// Wait until both goroutines have done.
+			wg.Wait()
 		})
-
-		done := make(chan struct{})
-		time.AfterFunc(runDuration, func() { close(done) })
-		wg := sync.WaitGroup{}
-		wg.Add(2)
-
-		// Start a goroutine which continuously try to unload the reader.
-		go func() {
-			defer wg.Done()
-
-			for {
-				select {
-				case <-done:
-					return
-				default:
-					testutil.Ok(t, r.unloadIfIdleSince(0))
-				}
-			}
-		}()
-
-		// Try to read multiple times, while the other goroutine continuously try to unload it.
-		go func() {
-			defer wg.Done()
-
-			for {
-				select {
-				case <-done:
-					return
-				default:
-					_, err := r.PostingsOffset("a", "1")
-					testutil.Assert(t, err == nil || err == errUnloadedWhileLoading)
-				}
-			}
-		}()
-
-		// Wait until both goroutines have done.
-		wg.Wait()
 	}
 }
