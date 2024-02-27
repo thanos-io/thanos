360d24de10581493c349cf5d87c82648dae7a72d
Jacob Baungård Hansen <jacobbaungard@redhat.com>
2024-02-27 16:48:29 +0100

    Query UI: Add tenant box (#6867)
    * Query UI: Add tenant box
    
    With this commit as tenant box is added to the query UI. It can be used
    to specify which tenant to use when making a query.
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>
    
    * Re-compile static react app
    
    Recompiles the static react app as now needed following:
    https://github.com/thanos-io/thanos/pull/6900
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>
    
    * Move changelog item to appropiate future release
    
    After merging it was under the 0.34 release.
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>
    
    * Move query path tenancy proposal to done
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>
    
    ---------
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

--- pkg/ui/react-app/src/globals.ts
+++ pkg/ui/react-app/src/globals.ts
@@ -3,3 +3,5 @@ import jquery from 'jquery';
 window.jQuery = jquery;
 window.moment = require('moment');
 window.THANOS_QUERY_URL = '';
+window.THANOS_TENANT_HEADER = '';
+window.THANOS_DEFAULT_TENANT = '';
--- pkg/ui/react-app/src/thanos/config.ts
+++ pkg/ui/react-app/src/thanos/config.ts
@@ -1,6 +1,18 @@
 declare const THANOS_QUERY_URL: string;
+declare const THANOS_TENANT_HEADER: string;
+declare const THANOS_DEFAULT_TENANT: string;
 
 export let queryURL = THANOS_QUERY_URL;
 if (queryURL === '' || queryURL === '{{ .queryURL }}') {
   queryURL = 'http://localhost:10902';
+}
+
+export let defaultTenant = THANOS_DEFAULT_TENANT;
+if (defaultTenant === '' || defaultTenant === '{{ .defaultTenant }}') {
+  defaultTenant = 'default-tenant';
+}
+
+export let tenantHeader = THANOS_TENANT_HEADER;
+if (tenantHeader === '' || tenantHeader === '{{ .tenantHeader }}') {
+  tenantHeader = 'thanos-tenant';
 }
