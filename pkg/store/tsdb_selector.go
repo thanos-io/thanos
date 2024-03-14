972f1f85ba2705e2a1a5c14ebe2fbe146226e342
Filip Petkovski <filip.petkovsky@gmail.com>
2024-03-14 09:13:55 +0100

    Add support for TSDB selector in querier (#7200)
    * Add support for TSDB selector in querier
    
    This PR allows using the query distributed mode against a set of multi-tenant receivers
    as described in https://github.com/thanos-io/thanos/blob/main/docs/proposals-done/202301-distributed-query-execution.md#distributed-execution-against-receive-components.
    
    The feature is enabled by a selector.relabel-config flag in the Query component
    which allows it to select a subset of TSDBs to query based on their external labels.
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Add CHANGELOG entry and fix docs
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Add comments
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Add test case for MatchersForLabelSets
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Fix failing test
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Use an unbuffered channel
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Change flag description
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    * Remove parameter from ServerAsClient
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>
    
    ---------
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>

---
+++ pkg/store/tsdb_selector.go
@@ -0,0 +1,102 @@
+// Copyright (c) The Thanos Authors.
+// Licensed under the Apache License 2.0.
+
+package store
+
+import (
+	"sort"
+	"strings"
+
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/relabel"
+	"golang.org/x/exp/maps"
+
+	"github.com/thanos-io/thanos/pkg/store/storepb"
+)
+
+const reMatchEmpty = "^$"
+
+var DefaultSelector = noopSelector()
+
+type TSDBSelector struct {
+	relabelConfig []*relabel.Config
+}
+
+func noopSelector() *TSDBSelector {
+	return NewTSDBSelector(nil)
+}
+
+func NewTSDBSelector(relabelConfig []*relabel.Config) *TSDBSelector {
+	return &TSDBSelector{
+		relabelConfig: relabelConfig,
+	}
+}
+
+// MatchLabelSets returns true if the given label sets match the TSDBSelector.
+// As a second parameter, it returns the matched label sets if they are a subset of the given input.
+// Otherwise the second return value is nil.
+func (sr *TSDBSelector) MatchLabelSets(labelSets ...labels.Labels) (bool, []labels.Labels) {
+	if sr.relabelConfig == nil || len(labelSets) == 0 {
+		return true, nil
+	}
+	matchedLabelSets := sr.runRelabelRules(labelSets)
+	return len(matchedLabelSets) > 0, matchedLabelSets
+}
+
+func (sr *TSDBSelector) runRelabelRules(labelSets []labels.Labels) []labels.Labels {
+	result := make([]labels.Labels, 0)
+	for _, labelSet := range labelSets {
+		if _, keep := relabel.Process(labelSet, sr.relabelConfig...); !keep {
+			continue
+		}
+
+		result = append(result, labelSet)
+	}
+
+	return result
+}
+
+// MatchersForLabelSets generates a list of label matchers for the given label sets.
+func MatchersForLabelSets(labelSets []labels.Labels) []storepb.LabelMatcher {
+	var (
+		// labelNameCounts tracks how many times a label name appears in the given label
+		// sets. This is used to make sure that an explicit empty value matcher is
+		// generated when a label name is missing from a label set.
+		labelNameCounts = make(map[string]int)
+		// labelNameValues contains an entry for each label name and label value
+		// combination that is present in the given label sets. This map is used to build
+		// out the label matchers.
+		labelNameValues = make(map[string]map[string]struct{})
+	)
+	for _, labelSet := range labelSets {
+		for _, lbl := range labelSet {
+			if _, ok := labelNameValues[lbl.Name]; !ok {
+				labelNameValues[lbl.Name] = make(map[string]struct{})
+			}
+			labelNameCounts[lbl.Name]++
+			labelNameValues[lbl.Name][lbl.Value] = struct{}{}
+		}
+	}
+
+	// If a label name is missing from a label set, force an empty value matcher for
+	// that label name.
+	for labelName := range labelNameValues {
+		if labelNameCounts[labelName] < len(labelSets) {
+			labelNameValues[labelName][reMatchEmpty] = struct{}{}
+		}
+	}
+
+	matchers := make([]storepb.LabelMatcher, 0, len(labelNameValues))
+	for lblName, lblVals := range labelNameValues {
+		values := maps.Keys(lblVals)
+		sort.Strings(values)
+		matcher := storepb.LabelMatcher{
+			Name:  lblName,
+			Value: strings.Join(values, "|"),
+			Type:  storepb.LabelMatcher_RE,
+		}
+		matchers = append(matchers, matcher)
+	}
+
+	return matchers
+}
