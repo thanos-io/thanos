a67eec04fc9ef7a80a6be6cca24a8f31e421ee2b
Ben Ye <benye@amazon.com>
2024-04-01 08:01:10 -0700

    change shipper to not overwrite all external labels (#7247)
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/shipper/shipper.go
+++ pkg/shipper/shipper.go
@@ -383,7 +383,9 @@ func (s *Shipper) upload(ctx context.Context, meta *metadata.Meta) error {
 	}
 	// Attach current labels and write a new meta file with Thanos extensions.
 	if lset := s.getLabels(); !lset.IsEmpty() {
-		meta.Thanos.Labels = lset.Map()
+		lset.Range(func(l labels.Label) {
+			meta.Thanos.Labels[l.Name] = l.Value
+		})
 	}
 	meta.Thanos.Source = s.source
 	meta.Thanos.SegmentFiles = block.GetSegmentFiles(updir)
