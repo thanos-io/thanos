a1e3e57f68ee14a72bc4ad8c8b1748c24211459a
Saswata Mukherjee <saswataminsta@yahoo.com>
2024-05-23 09:48:34 +0100

    *: Ensure objstore flag values are masked & disable debug/pprof/cmdline
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- pkg/server/http/http.go
+++ pkg/server/http/http.go
@@ -117,7 +117,6 @@ func (s *Server) Handle(pattern string, handler http.Handler) {
 
 func registerProfiler(mux *http.ServeMux) {
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
