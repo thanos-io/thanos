5c5c36d7e97c3c1dcd88f8666a3cef404b3cd3aa
Tidhar Klein Orbach <tidhar.o@taboola.com>
2024-09-02 13:17:32 +0300

    store: added a log error print when proxy limit are violated
    Signed-off-by: Tidhar Klein Orbach <tidhar.o@taboola.com>

--- pkg/store/proxy.go
+++ pkg/store/proxy.go
@@ -371,6 +371,7 @@ func (s *ProxyStore) Series(originalRequest *storepb.SeriesRequest, srv storepb.
 		}
 
 		if err := srv.Send(resp); err != nil {
+			level.Error(reqLogger).Log("msg", errors.Wrap(err, "send series response").Error())
 			return status.Error(codes.Unknown, errors.Wrap(err, "send series response").Error())
 		}
 	}
