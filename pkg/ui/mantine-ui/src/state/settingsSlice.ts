9e9eb3541333b1c921362825cefbecae6dad59de
Saswata Mukherjee <saswataminsta@yahoo.com>
2024-09-11 22:43:47 +0100

    Port in page components as well
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- pkg/ui/mantine-ui/src/state/settingsSlice.ts
+++ pkg/ui/mantine-ui/src/state/settingsSlice.ts
@@ -3,10 +3,8 @@ import { useAppSelector } from "./hooks";
 import { initializeFromLocalStorage } from "./initializeFromLocalStorage";
 
 interface Settings {
-  consolesLink: string | null;
-  lookbackDelta: string,
-  agentMode: boolean;
-  ready: boolean;
+  // lookbackDelta: string,
+  // ready: boolean;
   pathPrefix: string;
   useLocalTime: boolean;
   enableQueryHistory: boolean;
@@ -17,10 +15,8 @@ interface Settings {
 }
 
 // Declared/defined in public/index.html, value replaced by Prometheus when serving bundle.
-declare const GLOBAL_CONSOLES_LINK: string;
-declare const GLOBAL_AGENT_MODE: string;
-declare const GLOBAL_READY: string;
-declare const GLOBAL_LOOKBACKDELTA: string;
+// declare const GLOBAL_READY: string;
+// declare const GLOBAL_LOOKBACKDELTA: string;
 
 export const localStorageKeyUseLocalTime = "settings.useLocalTime";
 export const localStorageKeyEnableQueryHistory = "settings.enableQueryHistory";
@@ -31,19 +27,12 @@ export const localStorageKeyEnableLinter = "settings.enableLinter";
 export const localStorageKeyShowAnnotations = "settings.showAnnotations";
 
 export const initialState: Settings = {
-  consolesLink:
-    GLOBAL_CONSOLES_LINK === "CONSOLES_LINK_PLACEHOLDER" ||
-    GLOBAL_CONSOLES_LINK === "" ||
-    GLOBAL_CONSOLES_LINK === null
-      ? null
-      : GLOBAL_CONSOLES_LINK,
-  agentMode: GLOBAL_AGENT_MODE === "true",
-  ready: GLOBAL_READY === "true",
-  lookbackDelta:
-    GLOBAL_LOOKBACKDELTA === "LOOKBACKDELTA_PLACEHOLDER" ||
-    GLOBAL_LOOKBACKDELTA === null
-      ? ""
-      : GLOBAL_LOOKBACKDELTA,
+  // ready: GLOBAL_READY === "true",
+  // lookbackDelta:
+  //   GLOBAL_LOOKBACKDELTA === "LOOKBACKDELTA_PLACEHOLDER" ||
+  //   GLOBAL_LOOKBACKDELTA === null
+  //     ? ""
+  //     : GLOBAL_LOOKBACKDELTA,
   pathPrefix: "",
   useLocalTime: initializeFromLocalStorage<boolean>(
     localStorageKeyUseLocalTime,
--- pkg/ui/mantine-ui/vite.config.ts
+++ pkg/ui/mantine-ui/vite.config.ts
@@ -6,20 +6,20 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:9090",
-      },
-      "/-/": {
-        target: "http://localhost:9090",
-      },
       // "/api": {
-      //   target: "https://prometheus.demo.do.prometheus.io/",
-      //   changeOrigin: true,
+      //   target: "http://localhost:9090",
       // },
       // "/-/": {
-      //   target: "https://prometheus.demo.do.prometheus.io/",
-      //   changeOrigin: true,
+      //   target: "http://localhost:9090",
       // },
+      "/api": {
+         target: "https://prometheus.demo.do.prometheus.io/",
+         changeOrigin: true,
+       },
+       "/-/": {
+         target: "https://prometheus.demo.do.prometheus.io/",
+         changeOrigin: true,
+       },
     },
   },
 });
