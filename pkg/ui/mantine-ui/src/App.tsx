07ccfd14047a8952d03c6fb296d45885612b1208
Saswata Mukherjee <saswataminsta@yahoo.com>
2024-09-11 21:39:23 +0100

    Copy over utilities from Prometheus
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- pkg/ui/mantine-ui/src/App.tsx
+++ pkg/ui/mantine-ui/src/App.tsx
@@ -1,17 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- pkg/ui/mantine-ui/src/main.tsx
+++ pkg/ui/mantine-ui/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import store from "./state/store.ts";
+import { Provider } from "react-redux";
+import "./fonts/codicon.ttf";
+import "./promql.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
