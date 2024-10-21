9d2e5e0b6926ff0a5d2b52a12ddcfb6d505365af
Pedro Tanaka <pedro.tanaka@shopify.com>
2024-10-21 14:54:35 +0200

    QFE: Create new stats middleware to force query statistics collection
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- cmd/thanos/query_frontend.go
+++ cmd/thanos/query_frontend.go
@@ -146,6 +146,8 @@ func registerQueryFrontend(app *extkingpin.App) {
 	cmd.Flag("query-frontend.log-queries-longer-than", "Log queries that are slower than the specified duration. "+
 		"Set to 0 to disable. Set to < 0 to enable on all queries.").Default("0").DurationVar(&cfg.CortexHandlerConfig.LogQueriesLongerThan)
 
+	cmd.Flag("query-frontend.force-query-stats", "Force Report query stats as metrics and in logs.").Default("false").BoolVar(&cfg.ForceQueryStats)
+
 	cmd.Flag("query-frontend.org-id-header", "Deprecation Warning - This flag will be soon deprecated in favor of query-frontend.tenant-header"+
 		" and both flags cannot be used at the same time. "+
 		"Request header names used to identify the source of slow queries (repeated flag). "+
