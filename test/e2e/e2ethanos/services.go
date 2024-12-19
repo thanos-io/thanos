5b4600b74893837e551d167512cf95a063d3f4fa
Nicolas Takashi <nicolas.tcs@hotmail.com>
2024-12-19 22:26:57 +0000

    [TEST] upgrade collector version
    Signed-off-by: Nicolas Takashi <nicolas.tcs@hotmail.com>

--- test/e2e/e2ethanos/services.go
+++ test/e2e/e2ethanos/services.go
@@ -69,7 +69,7 @@ func DefaultPrometheusImage() string {
 
 // DefaultOtelImage sets default Otel image used in e2e service.
 func DefaultOtelImage() string {
-	return "otel/opentelemetry-collector-contrib:0.98.0"
+	return "otel/opentelemetry-collector-contrib:v0.116.0"
 }
 
 // DefaultAlertmanagerImage sets default Alertmanager image used in e2e service.
