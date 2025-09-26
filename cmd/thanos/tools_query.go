63c6c53f242424bbadaf29f2aca01d666699c993
Craig Ringer <craig.ringer@enterprisedb.com>
2025-09-26 12:40:16 +1200

    tools: add output formatters to tools query subcommand

--- cmd/thanos/tools_query.go
+++ cmd/thanos/tools_query.go
@@ -6,12 +6,15 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/oklog/run"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -25,6 +28,7 @@ import (
 	"github.com/thanos-io/thanos/pkg/extkingpin"
 	"github.com/thanos-io/thanos/pkg/model"
 	"github.com/thanos-io/thanos/pkg/query"
+	"github.com/thanos-io/thanos/pkg/store/storepb/prompb"
 )
 
 type toolsQueryConfig struct {
@@ -33,12 +37,17 @@ type toolsQueryConfig struct {
 	timeout  time.Duration
 	insecure bool
 	// TODO flags for TLS config
-	tlsconfig           tls.Config
+	tlsconfig tls.Config
+	// Flags for all queries
 	maxResolution       time.Duration
 	promqlOpts          query.Opts
 	skipChunks          bool
 	maxLookbackDelta    time.Duration
 	defaultPromqlEngine string
+	// Promtool compatible output format option
+	outFormat string
+	// Debug output
+	verbose bool
 }
 
 type toolsQueryRangeConfig struct {
@@ -70,6 +79,9 @@ func (tqc *toolsQueryConfig) registerToolsQueryFlag(cmd extkingpin.FlagClause) {
 	cmd.Flag("query.promql-engine", "Default PromQL engine to use.").Default(string(apiv1.PromqlEnginePrometheus)).
 		EnumVar(&tqc.defaultPromqlEngine, string(apiv1.PromqlEnginePrometheus), string(apiv1.PromqlEngineThanos))
 	cmd.Flag("query.lookback-delta", "The maximum lookback duration for retrieving metrics during expression evaluations. PromQL always evaluates the query for the certain timestamp (query range timestamps are deduced by step). Since scrape intervals might be different, PromQL looks back for given amount of time to get latest sample. If it exceeds the maximum lookback delta it assumes series is stale and returns none (a gap). This is why lookback delta should be set to at least 2 times of the slowest scrape interval. If unset it will use the promql default of 5m.").DurationVar(&tqc.maxLookbackDelta)
+
+	cmd.Flag("format", "Output format: promql (text format), json, jsonstream, raw (go structs). Note that json output is all buffered in memory, consider using jsonstream. Json output format will change to be promtool compatible in future.").
+		Default("promql").EnumVar(&tqc.outFormat, "promql", "json", "jsonstream", "raw")
 }
 
 func (tqrc *toolsQueryRangeConfig) registerToolsQueryRangeFlag(cmd extkingpin.FlagClause) {
@@ -123,6 +135,53 @@ func (tqc *toolsQueryConfig) createClient() (querypb.QueryClient, *grpc.ClientCo
 	return querypb.NewQueryClient(conn), conn, nil
 }
 
+func (tqc *toolsQueryConfig) createPrinter(logger log.Logger, instant bool) (printer, error) {
+	switch tqc.outFormat {
+	case "promql":
+		return &promqlPrinter{
+			logger:  logger,
+			out:     os.Stdout,
+			instant: instant,
+		}, nil
+	case "json":
+		return &jsonPrinter{
+			logger:  logger,
+			out:     os.Stdout,
+			instant: instant,
+		}, nil
+	case "jsonstream":
+		return &jsonStreamPrinter{
+			logger:  logger,
+			out:     os.Stdout,
+			instant: instant,
+		}, nil
+	case "raw":
+		return &rawPrinter{
+			logger:  logger,
+			out:     os.Stdout,
+			instant: instant,
+		}, nil
+	default:
+		return nil, fmt.Errorf("unknown or unimplemented output format %q", tqc.outFormat)
+	}
+}
+
+func (tqc *toolsQueryConfig) printRangeResponse(logger log.Logger, p printer, msg *querypb.QueryRangeResponse) {
+	level.Debug(logger).Log("msg", "QueryRange response message",
+		"type", fmt.Sprintf("%T", msg), "msg", fmt.Sprintf("%+v", msg))
+	switch r := msg.Result.(type) {
+	case *querypb.QueryRangeResponse_Timeseries:
+		p.printSeries(*r.Timeseries)
+	case *querypb.QueryRangeResponse_Stats:
+		p.printStats(r.Stats)
+	case *querypb.QueryRangeResponse_Warnings:
+		p.printWarnings(r.Warnings)
+	default:
+		level.Warn(logger).Log("msg", "unknown QueryRangeResponse type",
+			"type", fmt.Sprintf("%T", r), "value", fmt.Sprintf("%+v", r))
+	}
+}
+
 // Run a range query via Thanos gRPC API; like 'promtool query range'
 func registerToolsQueryRange(app extkingpin.AppClause) {
 	cmd := app.Command("range", "Run an range query on a Thanos gRPC endpoint.")
@@ -148,6 +207,12 @@ func registerToolsQueryRange(app extkingpin.AppClause) {
 			return fmt.Errorf("start time must be before end time")
 		}
 
+		p, err := tqr.createPrinter(logger, false)
+		if err != nil {
+			return err
+		}
+		defer p.finish()
+
 		qc, _, err := tqr.createClient()
 		if err != nil {
 			return err
@@ -166,8 +231,7 @@ func registerToolsQueryRange(app extkingpin.AppClause) {
 			LookbackDeltaSeconds:  int64(tqr.maxLookbackDelta.Seconds()),
 			Engine:                tqr.engineType(),
 		}
-		fmt.Fprintf(os.Stderr, "QueryRangeRequest: %+v\n", req)
-		fmt.Fprintf(os.Stderr, "Issuing gRPC QueryRangeRequest: %+v\n", req)
+		level.Debug(logger).Log("QueryRangeRequest", req)
 		resp, err := qc.QueryRange(ctx, req)
 		if err != nil {
 			return fmt.Errorf("QueryRange gRPC error: %v\n", err)
@@ -178,15 +242,31 @@ func registerToolsQueryRange(app extkingpin.AppClause) {
 			msg, err := resp.Recv()
 			switch err {
 			case nil:
-				fmt.Fprintf(os.Stdout, "QueryRange response message: %+v\n", msg)
+				tqr.printRangeResponse(logger, p, msg)
 			case io.EOF:
-				fmt.Fprintf(os.Stderr, "QueryRange: EOF\n")
+				level.Debug(logger).Log("msg", "QueryRange: EOF")
 				return nil
 			default:
 				return fmt.Errorf("QueryRange RecvMsg error: %v\n", err)
 			}
 		}
 	})
+}
+
+func (tqc *toolsQueryConfig) printInstantResponse(logger log.Logger, p printer, msg *querypb.QueryResponse) {
+	level.Debug(logger).Log("msg", "QueryRange response message",
+		"type", fmt.Sprintf("%T", msg), "msg", fmt.Sprintf("%+v", msg))
+	switch r := msg.Result.(type) {
+	case *querypb.QueryResponse_Timeseries:
+		p.printSeries(*r.Timeseries)
+	case *querypb.QueryResponse_Stats:
+		p.printStats(r.Stats)
+	case *querypb.QueryResponse_Warnings:
+		p.printWarnings(r.Warnings)
+	default:
+		level.Warn(logger).Log("msg", "unknown QueryRangeResponse type",
+			"type", fmt.Sprintf("%T", r), "value", fmt.Sprintf("%+v", r))
+	}
 }
 
 // Run an instant query via Thanos gRPC API; like 'promtool query instant'
@@ -206,6 +286,12 @@ func registerToolsQueryInstant(app extkingpin.AppClause) {
 		defer cancel()
 
 		warnOnFutureTimestamp(tqi.time, "--time")
+
+		p, err := tqi.createPrinter(logger, true)
+		if err != nil {
+			return err
+		}
+		defer p.finish()
 
 		qc, _, err := tqi.createClient()
 		if err != nil {
@@ -236,9 +322,9 @@ func registerToolsQueryInstant(app extkingpin.AppClause) {
 			msg, err := resp.Recv()
 			switch err {
 			case nil:
-				fmt.Fprintf(os.Stdout, "QueryRange response message: %+v\n", msg)
+				tqi.printInstantResponse(logger, p, msg)
 			case io.EOF:
-				fmt.Fprintf(os.Stderr, "QueryRange: EOF\n")
+				level.Debug(logger).Log("msg", "QueryRange: EOF")
 				return nil
 			default:
 				return fmt.Errorf("QueryRange RecvMsg error: %v\n", err)
@@ -256,3 +342,200 @@ func warnOnFutureTimestamp(t model.TimeOrDurationValue, desc string) {
 		}
 	}
 }
+
+/* TODO should these formatters live somewhere else, e.g. in pkg/internal ? */
+
+type printer interface {
+	printSeries(v prompb.TimeSeries)
+	printStats(v *querypb.QueryStats)
+	printWarnings(v string)
+	finish()
+}
+
+type promqlPrinter struct {
+	logger  log.Logger
+	out     io.Writer
+	instant bool
+}
+
+// TODO proper escaping of label values
+func (p *promqlPrinter) printSeries(val prompb.TimeSeries) {
+	var nameLabel string
+	var seriesIdentifier strings.Builder
+	// Build the series identifier and extract any name label.
+	for _, l := range val.Labels {
+		if l.Name == "__name__" {
+			nameLabel = l.Value
+			continue
+		}
+		if seriesIdentifier.Len() > 0 {
+			seriesIdentifier.Write([]byte(", "))
+		} else {
+			seriesIdentifier.Write([]byte("{"))
+		}
+		seriesIdentifier.WriteString(l.Name)
+		seriesIdentifier.WriteString("=")
+		// TODO check if the quoting rules are exactly the same as Prometheus
+		seriesIdentifier.WriteString(fmt.Sprintf("%q", l.Value))
+	}
+	if seriesIdentifier.Len() > 0 {
+		seriesIdentifier.WriteByte('}')
+	}
+
+	fmt.Fprintf(p.out, "%s%s ", nameLabel, seriesIdentifier.String())
+	if p.instant {
+		// one sample
+		if len(val.Samples) > 1 {
+			level.Warn(p.logger).Log("msg", "instant query series has multiple samples, showing only first")
+		}
+		p.printSampleValue(val.Samples[0])
+		p.out.Write([]byte("\n"))
+	} else {
+		// range of samples, newline after each series
+		fmt.Fprint(p.out, "\n")
+		for _, s := range val.Samples {
+			p.printSampleValue(s)
+			p.out.Write([]byte("\n"))
+		}
+	}
+}
+
+// TODO histograms and exemplars
+func (p *promqlPrinter) printSampleValue(s prompb.Sample) {
+	t := time.Unix(s.Timestamp/1000, (s.Timestamp%1000)*1e6)
+	fmt.Fprintf(p.out, "%f %f", s.Value, float64(t.UnixMicro())/1000)
+}
+
+func (p *promqlPrinter) printStats(v *querypb.QueryStats) {
+	fmt.Fprintf(p.out, "# stats: peak_samples=%d, total_samples=%d\n", v.PeakSamples, v.SamplesTotal)
+}
+
+func (p *promqlPrinter) printWarnings(v string) {
+	fmt.Fprintf(p.out, "# warning: %s\n", v)
+}
+
+func (p *promqlPrinter) finish() {
+	fmt.Fprintf(p.out, "\n")
+}
+
+type rawPrinter struct {
+	logger  log.Logger
+	out     io.Writer
+	instant bool
+}
+
+func (p *rawPrinter) printSeries(v prompb.TimeSeries) {
+	fmt.Fprintf(p.out, "%+v\n", v)
+}
+
+func (p *rawPrinter) printStats(v *querypb.QueryStats) {
+	fmt.Fprintf(p.out, "%+v\n", v)
+}
+
+func (p *rawPrinter) printWarnings(v string) {
+	fmt.Fprintf(p.out, "warning: %s\n", v)
+}
+
+func (p *rawPrinter) finish() {}
+
+// the json printer must accumulate in memory, unless we use a separate tool that
+// can generate a stream of json, or use text processing hacks. For now, just
+// build it in memory.
+type jsonPrinter struct {
+	logger   log.Logger
+	out      io.Writer
+	series   []prompb.TimeSeries
+	stats    []*querypb.QueryStats
+	warnings []string
+	instant  bool
+}
+
+func (p *jsonPrinter) printSeries(v prompb.TimeSeries) {
+	p.series = append(p.series, v)
+}
+
+func (p *jsonPrinter) printStats(v *querypb.QueryStats) {
+	p.stats = append(p.stats, v)
+}
+
+func (p *jsonPrinter) printWarnings(v string) {
+	p.warnings = append(p.warnings, v)
+}
+
+func (p *jsonPrinter) finish() {
+	// TODO create promtool-compatible JSON output
+	type jsonOutput struct {
+		Series   []prompb.TimeSeries   `json:"series,omitempty"`
+		Stats    []*querypb.QueryStats `json:"stats,omitempty"`
+		Warnings []string              `json:"warnings,omitempty"`
+	}
+	out := jsonOutput{
+		Series:   p.series,
+		Stats:    p.stats,
+		Warnings: p.warnings,
+	}
+	b, err := json.MarshalIndent(out, "", "  ")
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot marshal output to JSON", "error", err)
+		return
+	}
+	_, err = p.out.Write(b)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot write JSON to output", "error", err)
+		return
+	}
+}
+
+type jsonStreamPrinter struct {
+	logger  log.Logger
+	out     io.Writer
+	instant bool
+}
+
+type jsonMessage struct {
+	Type string      `json:"type"`
+	Data interface{} `json:"data"`
+}
+
+func (p *jsonStreamPrinter) printSeries(v prompb.TimeSeries) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot marshal series to JSON", "error", err)
+		return
+	}
+	_, err = p.out.Write(b)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot write JSON to output", "error", err)
+		return
+	}
+}
+
+func (p *jsonStreamPrinter) printStats(v *querypb.QueryStats) {
+	msg := jsonMessage{Type: "stats", Data: v}
+	b, err := json.Marshal(msg)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot marshal stats to JSON", "error", err)
+		return
+	}
+	_, err = p.out.Write(b)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot write JSON to output", "error", err)
+		return
+	}
+}
+
+func (p *jsonStreamPrinter) printWarnings(v string) {
+	msg := jsonMessage{Type: "warning", Data: v}
+	b, err := json.Marshal(msg)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot marshal warnings to JSON", "error", err)
+		return
+	}
+	_, err = p.out.Write(b)
+	if err != nil {
+		level.Error(p.logger).Log("msg", "cannot write JSON to output", "error", err)
+		return
+	}
+}
+
+func (p *jsonStreamPrinter) finish() {}
