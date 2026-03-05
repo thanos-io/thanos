002795936fe17dc742be633c92d7877a385dd141
Craig Ringer <craig.ringer@enterprisedb.com>
2026-03-06 12:20:05 +1300

    tracing: add series and sample counts to Query spans
    Add result.series and result.samples attributes to traces for the thanos
    Query gRPC response.
    
    Signed-off-by: Craig Ringer <craig.ringer@enterprisedb.com>

--- pkg/api/query/grpc.go
+++ pkg/api/query/grpc.go
@@ -139,9 +139,12 @@ func (g *GRPCAPI) Query(request *querypb.QueryRequest, server querypb.Query_Quer
 		}
 	}
 
+	var numSeries, numSamples int64
 	batchSize := request.ResponseBatchSize
 	switch vector := result.Value.(type) {
 	case promql.Scalar:
+		numSeries = 1
+		numSamples = 1
 		series := &prompb.TimeSeries{
 			Samples: []prompb.Sample{{Value: vector.V, Timestamp: vector.T}},
 		}
@@ -149,6 +152,8 @@ func (g *GRPCAPI) Query(request *querypb.QueryRequest, server querypb.Query_Quer
 			return err
 		}
 	case promql.Vector:
+		numSeries = int64(len(vector))
+		numSamples = int64(len(vector))
 		if batchSize <= 1 {
 			for _, sample := range vector {
 				floats, histograms := prompb.SamplesFromPromqlSamples(sample)
@@ -187,6 +192,11 @@ func (g *GRPCAPI) Query(request *querypb.QueryRequest, server querypb.Query_Quer
 	}
 	if err := server.Send(querypb.NewQueryStatsResponse(extractQueryStats(qry))); err != nil {
 		return err
+	}
+
+	if span := opentracing.SpanFromContext(ctx); span != nil {
+		span.SetTag("result.series", numSeries)
+		span.SetTag("result.samples", numSamples)
 	}
 
 	return nil
@@ -261,9 +271,14 @@ func (g *GRPCAPI) QueryRange(request *querypb.QueryRangeRequest, srv querypb.Que
 		}
 	}
 
+	var numSeries, numSamples int64
 	batchSize := request.ResponseBatchSize
 	switch value := result.Value.(type) {
 	case promql.Matrix:
+		numSeries = int64(len(value))
+		for _, s := range value {
+			numSamples += int64(len(s.Floats) + len(s.Histograms))
+		}
 		if batchSize <= 1 {
 			for _, series := range value {
 				floats, histograms := prompb.SamplesFromPromqlSeries(series)
@@ -300,6 +315,8 @@ func (g *GRPCAPI) QueryRange(request *querypb.QueryRangeRequest, srv querypb.Que
 			}
 		}
 	case promql.Vector:
+		numSeries = int64(len(value))
+		numSamples = int64(len(value))
 		if batchSize <= 1 {
 			for _, sample := range value {
 				floats, histograms := prompb.SamplesFromPromqlSamples(sample)
@@ -336,6 +353,8 @@ func (g *GRPCAPI) QueryRange(request *querypb.QueryRangeRequest, srv querypb.Que
 			}
 		}
 	case promql.Scalar:
+		numSeries = 1
+		numSamples = 1
 		series := &prompb.TimeSeries{
 			Samples: []prompb.Sample{{Value: value.V, Timestamp: value.T}},
 		}
@@ -345,6 +364,11 @@ func (g *GRPCAPI) QueryRange(request *querypb.QueryRangeRequest, srv querypb.Que
 	}
 	if err := srv.Send(querypb.NewQueryRangeStatsResponse(extractQueryStats(qry))); err != nil {
 		return err
+	}
+
+	if span := opentracing.SpanFromContext(ctx); span != nil {
+		span.SetTag("result.series", numSeries)
+		span.SetTag("result.samples", numSamples)
 	}
 
 	return nil
